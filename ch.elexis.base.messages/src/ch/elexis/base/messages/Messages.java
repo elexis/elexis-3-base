a87775dc677f52a4432f060f1e86935e0bf5ffb8
Marlovits Harald <marlovitsh@gmail.com>
2013-12-09 17:42:03 +0100

    converted to 3.0, named from ch.elexis.messages to ch.elexis.base.messages, update layout in MsgDetailDialog

---
+++ ch.elexis.base.messages/src/ch/elexis/base/messages/Messages.java
@@ -0,0 +1,24 @@
+package ch.elexis.base.messages;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "ch.elexis.base.messages.messages"; //$NON-NLS-1$
+	public static String MsgDetailDialog_asReminder;
+	public static String MsgDetailDialog_cancel;
+	public static String MsgDetailDialog_createMessage;
+	public static String MsgDetailDialog_delete;
+	public static String MsgDetailDialog_from;
+	public static String MsgDetailDialog_message;
+	public static String MsgDetailDialog_messageDated;
+	public static String MsgDetailDialog_readMessage;
+	public static String MsgDetailDialog_reply;
+	public static String MsgDetailDialog_send;
+	public static String MsgDetailDialog_to;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+	
+	private Messages(){}
+}

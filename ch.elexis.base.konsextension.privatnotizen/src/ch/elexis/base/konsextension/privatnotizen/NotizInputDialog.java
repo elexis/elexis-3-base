318c634c527bfe3938a7176d309761d43a38b70a
Marlovits Harald <marlovitsh@gmail.com>
2013-12-23 19:05:06 +0100

    converted ch.elexis.privatnotizen to 3.0.0, renamed to ch.elexis.base.konsextension.privatnotizen, added feedback if wrong mandant tries to open note corrected bug testing user against mandator

---
+++ ch.elexis.base.konsextension.privatnotizen/src/ch/elexis/base/konsextension/privatnotizen/NotizInputDialog.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2008, G. Weirich and Elexis
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    G. Weirich - initial implementation
+ *******************************************************************************/
+
+package ch.elexis.base.konsextension.privatnotizen;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.*;
+
+import ch.elexis.core.ui.icons.ImageSize;
+import ch.elexis.core.ui.icons.Images;
+import ch.elexis.core.ui.util.SWTHelper;
+import ch.elexis.privatnotizen.Privatnotiz;
+
+public class NotizInputDialog extends TitleAreaDialog {
+	private Privatnotiz mine;
+	private Text textField;
+	
+	public NotizInputDialog(Shell shell, Privatnotiz note){
+		super(shell);
+		mine = note;
+	}
+	
+	@Override
+	protected Control createDialogArea(Composite parent){
+		textField = new Text(parent, SWT.MULTI | SWT.BORDER);
+		textField.setLayoutData(SWTHelper.getFillGridData(1, true, 1, true));
+		textField.setText(mine.getText());
+		return textField;
+	}
+	
+	@Override
+	public void create(){
+		super.create();
+		setTitle(Messages.NotizInputDialog_noteDlgTitle);
+		getShell().setText(Messages.NotizInputDialog_noteDlgText);
+		setTitleImage(Images.IMG_LOGO.getImage(ImageSize._75x66_TitleDialogIconSize));
+		setMessage(Messages.NotizInputDialog_noteDlgMessage);
+	}
+	
+	@Override
+	protected void okPressed(){
+		mine.setText(textField.getText());
+		super.okPressed();
+	}
+	
+}
---
+++ ch.elexis.base.konsextension.privatnotizen/src/ch/elexis/privatnotizen/Privatnotiz.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2006, G. Weirich and Elexis
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    G. Weirich - initial implementation
+ *    
+ *  $Id: Privatnotiz.java 814 2006-08-28 20:00:33Z rgw_ch $
+ *******************************************************************************/
+
+package ch.elexis.privatnotizen;
+
+import ch.elexis.data.Mandant;
+import ch.elexis.data.PersistentObject;
+import ch.rgw.tools.StringTool;
+
+/**
+ * Die Datenklasse für Privatnotizen. Eine Privatnotiz besteht aus einer ID und einem Text. Die ID
+ * wiederum ist aus der ID des "Besitzers" und einer UID zusammengesetzt. Zum Speichern wird der
+ * Einfachheit halber die Elexis-Standardtabelle HEAP2 verwendet.
+ * 
+ * @author Gerry
+ * 
+ */
+public class Privatnotiz extends PersistentObject {
+	
+	static {
+		addMapping("HEAP2", "text=S:C:Contents"); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+	
+	public void setText(String tx){
+		set("text", tx); //$NON-NLS-1$
+	}
+	
+	public String getText(){
+		return get("text"); //$NON-NLS-1$
+	}
+	
+	@Override
+	public String getLabel(){
+		return Mandant.load(getMandantID()).getLabel();
+	}
+	
+	public String getMandantID(){
+		return getId().split(":")[0]; //$NON-NLS-1$
+	}
+	
+	public Privatnotiz(Mandant mandant){
+		String id = mandant.getId() + ":" + StringTool.unique("pnotes"); //$NON-NLS-1$ //$NON-NLS-2$
+		create(id);
+	}
+	
+	public static Privatnotiz load(String id){
+		return new Privatnotiz(id);
+	}
+	
+	@Override
+	protected String getTableName(){
+		return "HEAP2"; //$NON-NLS-1$
+	}
+	
+	protected Privatnotiz(String id){
+		super(id);
+	}
+	
+	protected Privatnotiz(){}
+}

ffe99559bfcae46359251f1e1aedd4bfa9de75ca
Niklaus Giger <niklaus.giger@member.fsf.org>
2014-02-09 16:40:29 +0100

    Fix Einstellungen Abrechnung Fachspezialitäten Liste ist leer

--- ch.elexis.base.ch.labortarif_2009/src/ch/elexis/labortarif2009/data/Constants.java
+++ ch.elexis.base.ch.labortarif_2009/src/ch/elexis/labortarif2009/data/Constants.java
@@ -12,5 +12,5 @@
 package ch.elexis.labortarif2009.data;
 
 public class Constants {
-	public static final String pluginID = "ch.elexis.labortarif.ch2009"; //$NON-NLS-1$
+	public static final String pluginID = "ch.elexis.base.ch.labortarif_2009"; //$NON-NLS-1$
 }

0383be1dbb4d87120197d293f15f7219f3ae8b7e
huthomas <huster@medevit.at>
2014-03-11 16:10:06 +0100

    [2108] fixed com.hilotec.elexis.kgview FavMedikament loading

--- com.hilotec.elexis.kgview/src/com/hilotec/elexis/kgview/data/FavMedikament.java
+++ com.hilotec.elexis.kgview/src/com/hilotec/elexis/kgview/data/FavMedikament.java
@@ -12,45 +12,34 @@ public class FavMedikament extends PersistentObject {
 	
 	private static final String TABLENAME = "COM_HILOTEC_ELEXIS_KGVIEW_FAVMEDIKAMENT";
 	
-	public static final String FLD_BEZEICHNUNG	= "Bezeichnung";
-	public static final String FLD_ZWECK		= "Zweck";
-	public static final String FLD_EINHEIT		= "Einheit";
-	public static final String FLD_ORDNUNGSZAHL	= "Ordnungszahl";
+	public static final String FLD_BEZEICHNUNG = "Bezeichnung";
+	public static final String FLD_ZWECK = "Zweck";
+	public static final String FLD_EINHEIT = "Einheit";
+	public static final String FLD_ORDNUNGSZAHL = "Ordnungszahl";
 	
 	static {
-		addMapping(TABLENAME,
-			"Bezeichnung",
-			"Zweck",
-			"Einheit",
-			"Ordnungszahl");
+		addMapping(TABLENAME, "Bezeichnung", "Zweck", "Einheit", "Ordnungszahl");
 		checkTable();
 	}
-
-	private static final String create =
-		"CREATE TABLE " + TABLENAME + " ("
-			+ "  ID				VARCHAR(25) PRIMARY KEY, "
-			+ "  lastupdate 	BIGINT, "
-			+ "  deleted		CHAR(1) DEFAULT '0', "
-			+ "  Artikel	 	VARCHAR(25), "
-			+ "  Ordnungszahl	INT DEFAULT 0,  "
-			+ "  Bezeichnung	TEXT, "
-			+ "  Zweck			TEXT, "
-			+ "  Einheit		TEXT  "
-			+ ");"
-			+ "INSERT INTO " + TABLENAME + " (ID, Bezeichnung) VALUES "
-			+ "	('VERSION', '" + VERSION + "');";
 	
-	private static final String up_1to2 =
-		"ALTER TABLE " + TABLENAME
-			+ "  ADD Ordnungszahl 	INT DEFAULT 0 AFTER Artikel;"
-			+ "UPDATE " + TABLENAME + " SET Bezeichnung = '2' WHERE"
-			+ "  ID LIKE 'VERSION';";
+	private static final String create = "CREATE TABLE " + TABLENAME + " ("
+		+ "  ID				VARCHAR(25) PRIMARY KEY, " + "  lastupdate 	BIGINT, "
+		+ "  deleted		CHAR(1) DEFAULT '0', " + "  Artikel	 	VARCHAR(25), "
+		+ "  Ordnungszahl	INT DEFAULT 0,  " + "  Bezeichnung	TEXT, " + "  Zweck			TEXT, "
+		+ "  Einheit		TEXT  " + ");" + "INSERT INTO " + TABLENAME + " (ID, Bezeichnung) VALUES "
+		+ "	('VERSION', '" + VERSION + "');";
 	
-	private static void checkTable() {
+	private static final String up_1to2 = "ALTER TABLE " + TABLENAME
+		+ "  ADD Ordnungszahl 	INT DEFAULT 0 AFTER Artikel;" + "UPDATE " + TABLENAME
+		+ " SET Bezeichnung = '2' WHERE" + "  ID LIKE 'VERSION';";
+	
+	private static void checkTable(){
 		
 		String fm = null;
-		try { fm = getConnection().queryString(
-				"SELECT Bezeichnung FROM " + TABLENAME + " WHERE ID='VERSION';");
+		try {
+			fm =
+				getConnection().queryString(
+					"SELECT Bezeichnung FROM " + TABLENAME + " WHERE ID='VERSION';");
 		} catch (Exception e) {}
 		
 		if (fm == null) {
@@ -60,8 +49,8 @@ public class FavMedikament extends PersistentObject {
 				createOrModifyTable(up_1to2);
 		}
 	}
-
-	public FavMedikament(Artikel a, int o, String b, String z, String e) {
+	
+	public FavMedikament(Artikel a, int o, String b, String z, String e){
 		super(a.getId());
 		if (!exists()) {
 			create(a.getId());
@@ -73,27 +62,30 @@ public class FavMedikament extends PersistentObject {
 		setEinheit(e);
 	}
 	
-	public static FavMedikament load(final String id) {
+	public static FavMedikament load(final String id){
 		// FIXME: Dirty hack um zu pruefen ob ein Medikament schon
-		//        existiert, damit nicht versehentlich leere angelegt
-		//        werden.
-		String fm =	getConnection().queryString(
-				"SELECT Bezeichnung FROM " + TABLENAME +
-				" WHERE ID='"+id+"';");
+		// existiert, damit nicht versehentlich leere angelegt
+		// werden.
+		String fm =
+			getConnection().queryString(
+				"SELECT Bezeichnung FROM " + TABLENAME + " WHERE ID='" + id + "';");
 		
-		if (fm == null) return null;
+		if (fm == null)
+			return null;
 		return new FavMedikament(id);
 	}
 	
-	public static FavMedikament load(Artikel art) {
+	public static FavMedikament load(Artikel art){
+		if (art == null) {
+			return null;
+		}
 		return load(art.getId());
 	}
 	
-	public static List<FavMedikament> getAll() {
-		Query<FavMedikament> q =
-			new Query<FavMedikament>(FavMedikament.class);
+	public static List<FavMedikament> getAll(){
+		Query<FavMedikament> q = new Query<FavMedikament>(FavMedikament.class);
 		List<FavMedikament> fm = q.execute();
-		for (FavMedikament f: fm) {
+		for (FavMedikament f : fm) {
 			if (f.getId().equals("VERSION")) {
 				fm.remove(f);
 				break;
@@ -102,66 +94,65 @@ public class FavMedikament extends PersistentObject {
 		return fm;
 	}
 	
-	protected FavMedikament() {}
+	protected FavMedikament(){}
 	
-	protected FavMedikament(final String id) {
+	protected FavMedikament(final String id){
 		super(id);
 	}
-
+	
 	@Override
-	protected String getTableName() {
+	protected String getTableName(){
 		return TABLENAME;
 	}
-
+	
 	@Override
-	public String getLabel() {
+	public String getLabel(){
 		return getBezeichnung();
 	}
 	
-	public Artikel getArtikel() {
+	public Artikel getArtikel(){
 		return Artikel.load(getId());
 	}
-
-	public int getOrdnungszahl() {
+	
+	public int getOrdnungszahl(){
 		return getInt(FLD_ORDNUNGSZAHL);
 	}
-
-	public String getBezeichnung() {
+	
+	public String getBezeichnung(){
 		return get(FLD_BEZEICHNUNG);
 	}
 	
-	public String getZweck() {
+	public String getZweck(){
 		return get(FLD_ZWECK);
 	}
 	
-	public String getEinheit() {
+	public String getEinheit(){
 		return get(FLD_EINHEIT);
 	}
 	
-	
-	public void setOrdnungszahl(int ord) {
+	public void setOrdnungszahl(int ord){
 		setInt(FLD_ORDNUNGSZAHL, ord);
 	}
 	
-	public void setBezeichnung(String txt) {
+	public void setBezeichnung(String txt){
 		set(FLD_BEZEICHNUNG, txt);
 	}
-
-	public void setZweck(String txt) {
+	
+	public void setZweck(String txt){
 		set(FLD_ZWECK, txt);
 	}
 	
-	public void setEinheit(String txt) {
+	public void setEinheit(String txt){
 		set(FLD_EINHEIT, txt);
 	}
 	
 	/**
-	 * Erstellt neues FM mit selben Eigenschaften aber neuem verknuepftem
-	 * Artikel, das original FM wird geloescht.
+	 * Erstellt neues FM mit selben Eigenschaften aber neuem verknuepftem Artikel, das original FM
+	 * wird geloescht.
 	 */
-	public FavMedikament relinkTo(Artikel a) {
-		FavMedikament fm = new FavMedikament(a, getOrdnungszahl(),
-				getBezeichnung(), getZweck(), getEinheit());
+	public FavMedikament relinkTo(Artikel a){
+		FavMedikament fm =
+			new FavMedikament(a, getOrdnungszahl(), getBezeichnung(), getZweck(), getEinheit());
 		delete();
 		return fm;
 	}

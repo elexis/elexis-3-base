a1512753bb5fc2ac7121be1e9405274102c363a4
lamann <amann@medevit.at>
2014-08-28 16:48:17 +0200

    [2333] populate CDA Rezept with selected Rezept

--- at.medevit.elexis.ehc.core/src/at/medevit/elexis/ehc/core/internal/EhcCoreServiceImpl.java
+++ at.medevit.elexis.ehc.core/src/at/medevit/elexis/ehc/core/internal/EhcCoreServiceImpl.java
@@ -11,6 +11,7 @@
 package at.medevit.elexis.ehc.core.internal;
 
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.openhealthtools.mdht.uml.cda.CDAFactory;
@@ -27,6 +28,7 @@ import org.openhealthtools.mdht.uml.cda.ihe.MedicationsSection;
 import org.openhealthtools.mdht.uml.cda.util.CDAUtil;
 import org.openhealthtools.mdht.uml.hl7.datatypes.CE;
 import org.openhealthtools.mdht.uml.hl7.datatypes.DatatypesFactory;
+import org.openhealthtools.mdht.uml.hl7.datatypes.ED;
 import org.openhealthtools.mdht.uml.hl7.datatypes.EN;
 import org.openhealthtools.mdht.uml.hl7.datatypes.IVL_PQ;
 import org.openhealthtools.mdht.uml.hl7.vocab.NullFlavor;
@@ -36,7 +38,9 @@ import org.slf4j.LoggerFactory;
 import at.medevit.elexis.ehc.core.EhcCoreService;
 import at.medevit.elexis.ehc.core.internal.document.CdaChImpl;
 import ch.elexis.data.Patient;
+import ch.elexis.data.Prescription;
 import ch.elexis.data.Rezept;
+import ch.rgw.tools.TimeTool;
 import ehealthconnector.cda.documents.ch.CdaCh;
 import ehealthconnector.cda.documents.ch.Phone;
 
@@ -88,69 +92,88 @@ public class EhcCoreServiceImpl implements EhcCoreService {
 		CDACH clinicalDocument = CHFactory.eINSTANCE.createCDACH().init();
 		CdaChImpl ret = new CdaChImpl(clinicalDocument);
 		
-		ret.cSetPatient(EhcCoreMapper.getEhcPatient(rezept.getPatient()));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+		Patient patient = rezept.getPatient();
+		List<Prescription> prescriptions = rezept.getLines();
 		
-		// TODO OID muss richtig sein
-		// create medication section
-		MedicationsSection medicationsSection = IHEFactory.eINSTANCE.createMedicationsSection();
-		medicationsSection.setTitle(DatatypesFactory.eINSTANCE.createST("Aktuelle Medikation"));
-		StrucDocText sdText = CDAFactory.eINSTANCE.createStrucDocText();
-		sdText.addText("Lisinopril HCT Streuli Tbl. 10/12.5 mg: 1-0-0");
-		medicationsSection.setText(sdText);
-		medicationsSection.setCode(DatatypesFactory.eINSTANCE.createCE("10160-0",
-			"2.16.840.1.113883.6.1", "LOINC", "HISTORY OF MEDICATION USE"));
-		medicationsSection.setId(DatatypesFactory.eINSTANCE.createII("2.16.756.5.30.1.105.1.6"));
+		TimeTool tt = new TimeTool(rezept.getDate());
+		String rezeptTitle = "Rezept " + sdf.format(tt.getTime());
 		
-		// define substance administration
-		SubstanceAdministration substanceAdministration =
-			CDAFactory.eINSTANCE.createSubstanceAdministration();
-		substanceAdministration.getTemplateIds().add(
-			DatatypesFactory.eINSTANCE.createII("2.16.756.5.30.1.1.1.1.1"));
-		substanceAdministration.getIds().add(
-			DatatypesFactory.eINSTANCE.createII("2.16.756.5.30.2.6.1"));
-		// code, system, systemname, displayname
-		substanceAdministration.setCode(DatatypesFactory.eINSTANCE.createCD("DRUG",
-			"2.16.840.1.113883.5.4", "", "Medikamentöse Therapie"));
-		substanceAdministration.setStatusCode(DatatypesFactory.eINSTANCE.createCS("completed"));
-		substanceAdministration.getEffectiveTimes().add(
-			DatatypesFactory.eINSTANCE.createIVL_TS("20140807"));
-		substanceAdministration.setPriorityCode(DatatypesFactory.eINSTANCE.createCE("R",
-			"2.16.840.1.113883.5.7", "ActPriority", "Routine"));
-		substanceAdministration.setRouteCode(DatatypesFactory.eINSTANCE.createCE("PO",
-			"2.16.840.1.113883.5.112", "RouteOfAdministration", "schlucken, oral"));
+		// set the patient
+		ret.cSetPatient(EhcCoreMapper.getEhcPatient(patient));
 		
-		// set quantities
-		IVL_PQ ivlQuantity = DatatypesFactory.eINSTANCE.createIVL_PQ();
-		ivlQuantity.setCenter(DatatypesFactory.eINSTANCE.createPQ(1, "1"));
-		substanceAdministration.setDoseQuantity(ivlQuantity);
+		// create a medication section
+		MedicationsSection mediSection = IHEFactory.eINSTANCE.createMedicationsSection();
+		mediSection.setTitle(DatatypesFactory.eINSTANCE.createST(rezeptTitle));
+		// set ID
+		mediSection.setId(DatatypesFactory.eINSTANCE.createII("2.16.756.5.30.1.105.1.6"));
 		
-		IVL_PQ ivlRateQuantity = DatatypesFactory.eINSTANCE.createIVL_PQ();
-		ivlRateQuantity.setNullFlavor(NullFlavor.UNK);
-		substanceAdministration.setRateQuantity(ivlQuantity);
+		StrucDocText line = CDAFactory.eINSTANCE.createStrucDocText();
 		
-		// set consumable medication
-		Consumable consumable = CDAFactory.eINSTANCE.createConsumable();
-		ManufacturedProduct manufacturedProduct = CDAFactory.eINSTANCE.createManufacturedProduct();
-		Material material = CDAFactory.eINSTANCE.createMaterial();
-		CE materialCode =
-			DatatypesFactory.eINSTANCE.createCE("C09BA03SETN000000010TABL", "2.16.756.5.30.2.6.2");
+		// add prescription lines
+		for (Prescription p : prescriptions) {
+			line.addText(p.getLabel() + "   ");
+			String pharmaCode = p.getArtikel().getPharmaCode();
+			mediSection.setCode(DatatypesFactory.eINSTANCE.createCE(pharmaCode,
+				"2.16.840.1.113883.6.1", "LOINC", "HISTORY OF MEDICATION USE"));
+			
+			// TODO not connected to the rezept or prescription any more
+			// define substance administration
+			SubstanceAdministration substanceAdministration =
+				CDAFactory.eINSTANCE.createSubstanceAdministration();
+			ED prescriptionLabel = DatatypesFactory.eINSTANCE.createED(p.getLabel());
+			substanceAdministration.setText(prescriptionLabel);
+			substanceAdministration.getTemplateIds().add(
+				DatatypesFactory.eINSTANCE.createII("2.16.756.5.30.1.1.1.1.1"));
+			substanceAdministration.getIds().add(
+				DatatypesFactory.eINSTANCE.createII("2.16.756.5.30.2.6.1"));
+			
+			// code, system, systemname, displayname
+			substanceAdministration.setCode(DatatypesFactory.eINSTANCE.createCD("DRUG",
+				"2.16.840.1.113883.5.4", "", "Medikamentöse Therapie"));
+			substanceAdministration.setStatusCode(DatatypesFactory.eINSTANCE.createCS("completed"));
+			substanceAdministration.getEffectiveTimes().add(
+				DatatypesFactory.eINSTANCE.createIVL_TS("20140807"));
+			substanceAdministration.setPriorityCode(DatatypesFactory.eINSTANCE.createCE("R",
+				"2.16.840.1.113883.5.7", "ActPriority", "Routine"));
+			substanceAdministration.setRouteCode(DatatypesFactory.eINSTANCE.createCE("PO",
+				"2.16.840.1.113883.5.112", "RouteOfAdministration", "schlucken, oral"));
+			
+			// set quantities
+			IVL_PQ ivlQuantity = DatatypesFactory.eINSTANCE.createIVL_PQ();
+			ivlQuantity.setCenter(DatatypesFactory.eINSTANCE.createPQ(1, p.getDosis()));
+			substanceAdministration.setDoseQuantity(ivlQuantity);
+			
+			IVL_PQ ivlRateQuantity = DatatypesFactory.eINSTANCE.createIVL_PQ();
+			ivlRateQuantity.setNullFlavor(NullFlavor.UNK);
+			substanceAdministration.setRateQuantity(ivlQuantity);
+			
+			// set consumable medication
+			Consumable consumable = CDAFactory.eINSTANCE.createConsumable();
+			ManufacturedProduct manufacturedProduct =
+				CDAFactory.eINSTANCE.createManufacturedProduct();
+			Material material = CDAFactory.eINSTANCE.createMaterial();
+			CE materialCode =
+				DatatypesFactory.eINSTANCE.createCE("C09BA03SETN000000010TABL",
+					"2.16.756.5.30.2.6.2");
+			
+			// set original text UNK
+			materialCode.getTranslations().add(
+				DatatypesFactory.eINSTANCE.createCD("C09BA03", "2.16.840.1.113883.6.73", "", ""));
+			material.setCode(DatatypesFactory.eINSTANCE.createCE("C09BA03SETN000000010TABL",
+				"2.16.756.5.30.2.6.2"));
+			EN name = DatatypesFactory.eINSTANCE.createEN();
+			name.addText(p.getLabel());
+			material.setName(name);
+			manufacturedProduct.setManufacturedMaterial(material);
+			consumable.setManufacturedProduct(manufacturedProduct);
+			
+			substanceAdministration.setConsumable(consumable);
+			mediSection.addSubstanceAdministration(substanceAdministration);
+		}
+		mediSection.setText(line);
 		
-		// set original text UNK
-		
-		materialCode.getTranslations().add(
-			DatatypesFactory.eINSTANCE.createCD("C09BA03", "2.16.840.1.113883.6.73", "", ""));
-		material.setCode(DatatypesFactory.eINSTANCE.createCE("C09BA03SETN000000010TABL",
-			"2.16.756.5.30.2.6.2"));
-		EN name = DatatypesFactory.eINSTANCE.createEN();
-		name.addText("LISINOPRIL HCT Streuli Tabl 10/12.5 30 Stk");
-		material.setName(name);
-		manufacturedProduct.setManufacturedMaterial(material);
-		consumable.setManufacturedProduct(manufacturedProduct);
-		
-		substanceAdministration.setConsumable(consumable);
-		medicationsSection.addSubstanceAdministration(substanceAdministration);
-		
-		clinicalDocument.addSection(medicationsSection);
+		clinicalDocument.addSection(mediSection);
 		
 		return ret;
 	}

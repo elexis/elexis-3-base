214fdb295d12c03d7fe34c1b308931dfb7321797
thomashu <huster@medevit.at>
2015-03-26 13:53:30 +0100

    [3512] fixed name strings for synlab

--- ch.elexis.laborimport.synlab/src/ch/elexis/laborimport/synlab/Importer.java
+++ ch.elexis.laborimport.synlab/src/ch/elexis/laborimport/synlab/Importer.java
@@ -43,8 +43,8 @@ import ch.rgw.tools.Result.SEVERITY;
 // import ch.elexis.util.Messages;
 
 public class Importer extends ImporterPage {
-	public static final String MY_LAB = "snylab";
-	public static final String PLUGIN_ID = "ch.elexis.laborimport_snylab";
+	public static final String MY_LAB = "Synlab";
+	public static final String PLUGIN_ID = "ch.elexis.laborimport.synlab";
 	
 	private static final String OPENMEDICAL_MAINCLASS = "ch.openmedical.JMedTransfer.JMedTransfer";
 	

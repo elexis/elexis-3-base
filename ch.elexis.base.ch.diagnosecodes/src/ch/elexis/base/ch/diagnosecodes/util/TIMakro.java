f8808eecc4e35003977ea9c5613c1a421f704ee1
thomashu <huster@medevit.at>
2015-05-02 10:52:34 +0200

    [3644] fix TIMakro handling non TI code makros

--- ch.elexis.base.ch.diagnosecodes/src/ch/elexis/base/ch/diagnosecodes/util/TIMakro.java
+++ ch.elexis.base.ch.diagnosecodes/src/ch/elexis/base/ch/diagnosecodes/util/TIMakro.java
@@ -1,11 +1,16 @@
 package ch.elexis.base.ch.diagnosecodes.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ch.elexis.core.data.events.ElexisEventDispatcher;
 import ch.elexis.core.ui.util.IKonsMakro;
 import ch.elexis.data.Konsultation;
 import ch.elexis.data.TICode;
 
 public class TIMakro implements IKonsMakro {
+	
+	private static Logger logger = LoggerFactory.getLogger(TIMakro.class);
 	
 	public TIMakro(){
 		// TODO Auto-generated constructor stub
@@ -15,9 +20,13 @@ public class TIMakro implements IKonsMakro {
 	public String executeMakro(String makro){
 		Konsultation actKons = (Konsultation) ElexisEventDispatcher.getSelected(Konsultation.class);
 		
-		TICode tiCode = TICode.getFromCode(makro);
-		if (tiCode != null) {
-			actKons.addDiagnose(tiCode);
+		try {
+			TICode tiCode = TICode.getFromCode(makro);
+			if (tiCode != null) {
+				actKons.addDiagnose(tiCode);
+			}
+		} catch (Exception e) {
+			logger.debug("Could not resolve TI Code [" + makro + "]");
 		}
 		
 		return "";

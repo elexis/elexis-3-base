591e66eec463f678741b031d89004efcd5ea8cad
thomashu <huster@medevit.at>
2016-02-27 07:43:18 +0100

    [4411] added download and initialization of tarmed text templates

--- ch.elexis.base.ch.arzttarife/src/ch/elexis/views/RnPrintView2.java
+++ ch.elexis.base.ch.arzttarife/src/ch/elexis/views/RnPrintView2.java
@@ -11,19 +11,32 @@
  *******************************************************************************/
 package ch.elexis.views;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 import org.jdom.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import at.medevit.elexis.tarmed.model.TarmedJaxbUtil;
 import ch.elexis.TarmedRechnung.XMLExporter;
+import ch.elexis.core.data.activator.CoreHub;
 import ch.elexis.core.data.interfaces.IRnOutputter;
 import ch.elexis.core.ui.text.ITextPlugin;
+import ch.elexis.core.ui.text.MimeTypeUtil;
 import ch.elexis.core.ui.text.TextContainer;
 import ch.elexis.core.ui.util.SWTHelper;
+import ch.elexis.data.Brief;
+import ch.elexis.data.Query;
 import ch.elexis.data.Rechnung;
 import ch.elexis.data.RnStatus;
+import ch.elexis.tarmed.printer.TarmedTemplateRequirement;
 import ch.elexis.tarmed.printer.XML40Printer;
 import ch.elexis.tarmed.printer.XML44Printer;
 
@@ -41,6 +54,8 @@ import ch.elexis.tarmed.printer.XML44Printer;
  */
 public class RnPrintView2 extends ViewPart {
 	public static final String ID = "ch.elexis.arzttarife_ch.printview2";
+	
+	private static Logger logger = LoggerFactory.getLogger(RnPrintView2.class);
 	
 	TextContainer text;
 	
@@ -95,6 +110,9 @@ public class RnPrintView2 extends ViewPart {
 		if (rn.getStatus() == RnStatus.FEHLERHAFT) {
 			return false;
 		}
+		// check if we have all req. text templates
+		initializeRequiredTemplates();
+		
 		// check if we are working with 4.0 or 4.4 tarmed xml
 		if (TarmedJaxbUtil.getXMLVersion(xmlRn).equals("4.0")) {
 			XML40Printer xmlPrinter = new XML40Printer(text);
@@ -112,5 +130,113 @@ public class RnPrintView2 extends ViewPart {
 			return false;
 		}
 		
+	}
+	
+	private void initializeRequiredTemplates(){
+		if(!testTemplate(TarmedTemplateRequirement.TT_TARMED_S1)) {
+			initializeTemplate(TarmedTemplateRequirement.TT_TARMED_S1);
+		}
+		if (!testTemplate(TarmedTemplateRequirement.TT_TARMED_S2)) {
+			initializeTemplate(TarmedTemplateRequirement.TT_TARMED_S2);
+		}
+		
+		if (!testTemplate(TarmedTemplateRequirement.TT_TARMED_44_S1)) {
+			initializeTemplate(TarmedTemplateRequirement.TT_TARMED_44_S1);
+		}
+		if (!testTemplate(TarmedTemplateRequirement.TT_TARMED_44_S2)) {
+			initializeTemplate(TarmedTemplateRequirement.TT_TARMED_44_S2);
+		}
+	}
+	
+	private void initializeTemplate(String name){
+		String templateUrl = getTemplateUrl(name);
+		if(templateUrl != null) {
+			byte[] content = downloadTempalte(templateUrl);
+			if (content != null && content.length > 0) {
+				Brief template = new Brief(name, null, CoreHub.actUser, null, null, Brief.TEMPLATE);
+				template.save(content, text.getPlugin().getMimeType());
+				// all tarmed templates are sys templates
+				template.set(Brief.FLD_KONSULTATION_ID, Brief.SYS_TEMPLATE);
+			}
+		}
+	}
+	
+	private byte[] downloadTempalte(String templateUrl){
+		BufferedInputStream in = null;
+		ByteArrayOutputStream bout = null;
+		try {
+			in = new BufferedInputStream(new URL(templateUrl).openStream());
+			bout = new ByteArrayOutputStream();
+			
+			final byte data[] = new byte[1024];
+			int count;
+			while ((count = in.read(data, 0, 1024)) != -1) {
+				bout.write(data, 0, count);
+			}
+		} catch (IOException e) {
+			logger.warn("Could not dowload template from [" + templateUrl + "]", e);
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					// ignore
+				}
+				try {
+					bout.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+		return bout != null ? bout.toByteArray() : null;
+	}
+	
+	private String getTemplateUrl(String name){
+		if (isWord()) {
+			switch (name) {
+			case TarmedTemplateRequirement.TT_TARMED_S1:
+				return "https://medelexis.ch/uploads/media/Tarmedrechnung_S1_01.docx";
+			case TarmedTemplateRequirement.TT_TARMED_S2:
+				return "https://medelexis.ch/uploads/media/Tarmedrechnung_S2_01.docx";
+			case TarmedTemplateRequirement.TT_TARMED_44_S1:
+				return "https://medelexis.ch/uploads/media/TR44_S1.docx";
+			case TarmedTemplateRequirement.TT_TARMED_44_S2:
+				return "https://medelexis.ch/uploads/media/TR44_S2.docx";
+			default:
+				break;
+			}
+		} else {
+			switch (name) {
+			case TarmedTemplateRequirement.TT_TARMED_S1:
+				return "https://medelexis.ch/uploads/media/Tarmedrechnung_S1.odt";
+			case TarmedTemplateRequirement.TT_TARMED_S2:
+				return "https://medelexis.ch/uploads/media/Tarmedrechnung_S2.odt";
+			case TarmedTemplateRequirement.TT_TARMED_44_S1:
+				return "https://medelexis.ch/uploads/media/TR44_S1.odt";
+			case TarmedTemplateRequirement.TT_TARMED_44_S2:
+				return "https://medelexis.ch/uploads/media/TR44_S2.odt";
+				
+			default:
+				break;
+			}
+		}
+		return null;
+	}
+	
+	private boolean isWord(){
+		return text.getPlugin().getMimeType().equals(MimeTypeUtil.MIME_TYPE_MSWORD);
+	}
+	
+	private boolean testTemplate(String name){
+		Query<Brief> qbe = new Query<Brief>(Brief.class);
+		qbe.add(Brief.FLD_TYPE, Query.EQUALS, Brief.TEMPLATE);
+		qbe.and();
+		qbe.add(Brief.FLD_SUBJECT, Query.EQUALS, name);
+		List<Brief> list = qbe.execute();
+		if ((list == null) || (list.size() == 0)) {
+			return false;
+		}
+		return true;
 	}
 }

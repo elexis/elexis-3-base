9dc644e84ac25ee8927a435eaf7c5f0b99bb0b78
thomashu <huster@medevit.at>
2016-02-29 09:57:47 +0100

    [3996] updated medi etiketten printing to handle new medication view

--- at.medevit.elexis.medietikette/src/at/medevit/elexis/medietikette/command/PrintMediEtiketteUi.java
+++ at.medevit.elexis.medietikette/src/at/medevit/elexis/medietikette/command/PrintMediEtiketteUi.java
@@ -12,6 +12,11 @@ package at.medevit.elexis.medietikette.command;
 
 import static ch.elexis.core.ui.text.TextTemplateRequirement.TT_MEDI_LABEL;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -19,6 +24,8 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.statushandlers.StatusManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import at.medevit.elexis.medietikette.Activator;
 import at.medevit.elexis.medietikette.Messages;
@@ -36,66 +43,103 @@ import ch.elexis.data.Verrechnet;
 
 public class PrintMediEtiketteUi extends AbstractHandler {
 	
+	private static final Logger logger = LoggerFactory.getLogger(PrintMediEtiketteUi.class);
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException{
 		
 		synchronized (PrintMediEtiketteUi.class) {
-			boolean valid = false;
 			// init the selection
 			ISelection selection =
 				HandlerUtil.getActiveWorkbenchWindow(event).getActivePage().getSelection();
 			if (selection instanceof IStructuredSelection) {
 				IStructuredSelection strucSelection = (IStructuredSelection) selection;
-				Object selected = strucSelection.getFirstElement();
-				if (selected instanceof Prescription) {
-					Prescription prescription = (Prescription) selected;
+				List<Prescription> prescriptions = getPrescriptionsFromSelection(strucSelection);
+				List<Artikel> artikels = getArtikelsFromSelection(strucSelection);
+				
+				for (Prescription prescription : prescriptions) {
 					DataAccessor.setSelectedPrescription(prescription);
 					DataAccessor.setSelectedArticel(prescription.getArtikel());
-					valid = true;
-				} else if (selected instanceof Verrechnet) {
-					Verrechnet verrechnet = (Verrechnet) selected;
-					IVerrechenbar verrechenbar = verrechnet.getVerrechenbar();
-					if (verrechenbar instanceof Artikel) {
-						Artikel articel = (Artikel) verrechenbar;
-						DataAccessor.setSelectedArticel(articel);
-						valid = true;
-					} else {
-						return null;
-					}
+					print(event);
+					// clear the selection
+					DataAccessor.setSelectedPrescription(null);
+					DataAccessor.setSelectedArticel(null);
+				}
+				
+				for (Artikel artikel : artikels) {
+					DataAccessor.setSelectedArticel(artikel);
+					print(event);
+					// clear the selection
+					DataAccessor.setSelectedPrescription(null);
+					DataAccessor.setSelectedArticel(null);
 				}
 			}
-			
-			if (valid) {
-				// start printing the etikette
-				Kontakt kontakt = (Kontakt) ElexisEventDispatcher.getSelected(Patient.class);
-				EtiketteDruckenDialog dlg =
-					new EtiketteDruckenDialog(HandlerUtil.getActiveShell(event), kontakt,
-						TT_MEDI_LABEL);
-				dlg.setTitle(Messages.PrintMediEtiketteUi_DialogTitel);
-				dlg.setMessage(Messages.PrintMediEtiketteUi_DialogMessage);
-				if (!CoreHub.localCfg.get("Drucker/Etiketten/Choose", true)) { //$NON-NLS-1$
-					dlg.setBlockOnOpen(false);
-					dlg.open();
-					if (dlg.doPrint()) {
-						dlg.close();
-					} else {
-						StatusManager.getManager().handle(
-							new ElexisStatus(ElexisStatus.ERROR, Activator.PLUGIN_ID,
-								ElexisStatus.CODE_NOFEEDBACK,
-								Messages.PrintMediEtiketteUi_PrintError, ElexisStatus.LOG_ERRORS),
-							StatusManager.BLOCK);
-						return null;
-					}
-				} else {
-					dlg.setBlockOnOpen(true);
-					dlg.open();
-				}
-			}
-			// clear the selection
-			DataAccessor.setSelectedPrescription(null);
-			DataAccessor.setSelectedArticel(null);
-			
 			return null;
 		}
+	}
+	
+	private void print(ExecutionEvent event){
+		// start printing the etikette
+		Kontakt kontakt = (Kontakt) ElexisEventDispatcher.getSelected(Patient.class);
+		EtiketteDruckenDialog dlg =
+			new EtiketteDruckenDialog(HandlerUtil.getActiveShell(event), kontakt, TT_MEDI_LABEL);
+		dlg.setTitle(Messages.PrintMediEtiketteUi_DialogTitel);
+		dlg.setMessage(Messages.PrintMediEtiketteUi_DialogMessage);
+		if (!CoreHub.localCfg.get("Drucker/Etiketten/Choose", true)) { //$NON-NLS-1$
+			dlg.setBlockOnOpen(false);
+			dlg.open();
+			if (dlg.doPrint()) {
+				dlg.close();
+			} else {
+				StatusManager.getManager()
+					.handle(new ElexisStatus(ElexisStatus.ERROR, Activator.PLUGIN_ID,
+						ElexisStatus.CODE_NOFEEDBACK, Messages.PrintMediEtiketteUi_PrintError,
+						ElexisStatus.LOG_ERRORS), StatusManager.BLOCK);
+				return;
+			}
+		} else {
+			dlg.setBlockOnOpen(true);
+			dlg.open();
+		}
+	}
+	
+	private List<Artikel> getArtikelsFromSelection(IStructuredSelection strucSelection){
+		List<?> selection = strucSelection.toList();
+		List<Artikel> ret = new ArrayList<Artikel>();
+		for (Object object : selection) {
+			if (object instanceof Verrechnet) {
+				Verrechnet verrechnet = (Verrechnet) object;
+				IVerrechenbar verrechenbar = verrechnet.getVerrechenbar();
+				if (verrechenbar instanceof Artikel) {
+					ret.add((Artikel) verrechenbar);
+				}
+			}
+		}
+		return ret;
+	}
+	
+	private List<Prescription> getPrescriptionsFromSelection(IStructuredSelection strucSelection){
+		List<?> selection = strucSelection.toList();
+		List<Prescription> ret = new ArrayList<Prescription>();
+		for (Object object : selection) {
+			if (object instanceof Prescription) {
+				ret.add((Prescription) object);
+			} else if (object.getClass().getName()
+				.equals("ch.elexis.core.ui.medication.views.MedicationTableViewerItem")) {
+				try {
+					Method method = object.getClass().getMethod("getPrescription", null);
+					if (method != null) {
+						Prescription pres = (Prescription) method.invoke(object, new Object[0]);
+						if (pres != null) {
+							ret.add(pres);
+						}
+					}
+				} catch (NoSuchMethodException | SecurityException | IllegalAccessException
+						| IllegalArgumentException | InvocationTargetException e) {
+					logger.warn("Could not get selected prescription.", e);
+				}
+			}
+		}
+		return ret;
 	}
 }

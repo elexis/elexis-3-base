b524211e74d762e8baae43e4bcaf4d6af1cfbe5f
Niklaus Giger <niklaus.giger@member.fsf.org>
2016-10-06 16:23:58 +0200

    [5131]Updated copyright

--- at.medevit.elexis.gdt/src/at/medevit/elexis/gdt/ui/dialog/DateTimeTargetToModelUVS.java
+++ at.medevit.elexis.gdt/src/at/medevit/elexis/gdt/ui/dialog/DateTimeTargetToModelUVS.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2016 Medevit OG, Medelexis AG
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marco Descher, initial API and implementaion
+ *     Lucia Amman, bug fixes and improvements
+ * Sponsors: M. + P. Richter
+ *******************************************************************************/
 package at.medevit.elexis.gdt.ui.dialog;
 
 import java.util.Date;

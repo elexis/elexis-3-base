55c9bb536599466a7455d521814f9c44133c2de1
thomashu <huster@medevit.at>
2017-03-05 14:39:36 +0100

    [5626] added fop service test fragment

---
+++ ch.elexis.fop.service.test/src/ch/elexis/fop/service/test/TestJaxbContacts.java
@@ -0,0 +1,19 @@
+package ch.elexis.fop.service.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "contacts")
+public class TestJaxbContacts {
+	private List<TestJaxbContact> contact = new ArrayList<>();
+	
+	public List<TestJaxbContact> getContact(){
+		return contact;
+	}
+	
+	public void setContact(List<TestJaxbContact> contacts){
+		this.contact = contacts;
+	}
+}

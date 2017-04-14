7eac3f158f987366d395f3f28aed43f0031f6c84
sazgin <azgin@medevit.at>
2017-04-14 15:35:27 +0200

    [5886] Bei der Agenda gross View wird noch die falsche Zeit dargestellt

--- ch.elexis.agenda/src/ch/elexis/agenda/ui/ProportionalSheet.java
+++ ch.elexis.agenda/src/ch/elexis/agenda/ui/ProportionalSheet.java
@@ -41,6 +41,7 @@ import org.eclipse.swt.widgets.ScrollBar;
 import ch.elexis.actions.Activator;
 import ch.elexis.agenda.data.Termin;
 import ch.elexis.agenda.preferences.PreferenceConstants;
+import ch.elexis.agenda.series.SerienTermin;
 import ch.elexis.core.data.activator.CoreHub;
 import ch.elexis.core.ui.UiDesk;
 import ch.elexis.core.ui.locks.AcquireLockBlockingUi;
@@ -159,7 +160,12 @@ public class ProportionalSheet extends Composite implements IAgendaLayout {
 						SWTHelper.showInfo("Termin Kollision", "Termine überschneiden sich");
 					} else {
 						if (ctrlKeyDown) { // copy 
+							ctrlKeyDown = false;
 							Termin tCopy = (Termin) t.clone();
+							if (t.isRecurringDate() && t.getKontakt() == null) {
+								// take kontakt from root termin
+								tCopy.setKontakt(new SerienTermin(t).getRootTermin().getKontakt());
+							}
 							AcquireLockBlockingUi.aquireAndRun(tCopy, new ILockHandler() {
 								
 								@Override
@@ -173,7 +179,6 @@ public class ProportionalSheet extends Composite implements IAgendaLayout {
 									tCopy.setBereich(Activator.getDefault().getActResource());
 								}
 							});
-							refresh();
 						} else { // move
 							AcquireLockBlockingUi.aquireAndRun(t, new ILockHandler() {
 								
@@ -188,9 +193,9 @@ public class ProportionalSheet extends Composite implements IAgendaLayout {
 									t.setBereich(Activator.getDefault().getActResource());
 								}
 							});
-							refresh();
 						}
 					}
+					refresh();
 				}
 			}
 		});
@@ -203,6 +208,12 @@ public class ProportionalSheet extends Composite implements IAgendaLayout {
 			return true;
 		}
 		return false;
+	}
+	
+	@Override
+	public boolean setFocus(){
+		ctrlKeyDown = false;
+		return super.setFocus();
 	}
 	
 	public MenuManager getContextMenuManager(){

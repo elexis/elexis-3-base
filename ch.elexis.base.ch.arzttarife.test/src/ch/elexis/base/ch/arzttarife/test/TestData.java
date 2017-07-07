dab381570398b058b48afd99d27c8f7926b7a85e
thomashu <huster@medevit.at>
2017-07-07 14:54:06 +0200

    [6624] try to fix existing bills without amount_prepaid

--- ch.elexis.base.ch.arzttarife.test/src/ch/elexis/base/ch/arzttarife/test/TestData.java
+++ ch.elexis.base.ch.arzttarife.test/src/ch/elexis/base/ch/arzttarife/test/TestData.java
@@ -40,6 +40,8 @@ public class TestData {
 	
 	public static final String EXISTING_44_2_RNR = "4402";
 	
+	public static final String ERRONEOUS_44_1_RNR = "4412";
+	
 	public static String EXISTING_4_RNR = "4000";
 	
 	public static String EXISTING_4_2_RNR = "4002";
@@ -133,6 +135,12 @@ public class TestData {
 			stringWriter = new StringWriter();
 			IOUtils.copy(xmlIn, stringWriter, "UTF-8");
 			blob = NamedBlob.load(XMLExporter.PREFIX + EXISTING_44_2_RNR);
+			blob.putString(stringWriter.toString());
+			
+			xmlIn = TestSzenario.class.getResourceAsStream("/rsc/erroneous44_1.xml");
+			stringWriter = new StringWriter();
+			IOUtils.copy(xmlIn, stringWriter, "UTF-8");
+			blob = NamedBlob.load(XMLExporter.PREFIX + ERRONEOUS_44_1_RNR);
 			blob.putString(stringWriter.toString());
 		}
 		
@@ -301,7 +309,8 @@ public class TestData {
 						}
 					}
 				}
-			} else if (rechnungNr.equals(EXISTING_44_RNR) || rechnungNr.equals(EXISTING_44_2_RNR)) {
+			} else if (rechnungNr.equals(EXISTING_44_RNR) || rechnungNr.equals(EXISTING_44_2_RNR)
+				|| rechnungNr.equals(ERRONEOUS_44_1_RNR)) {
 				for (IVerrechenbar leistung : leistungen) {
 					if (leistung instanceof TarmedLeistung
 						&& leistung.getCode().equals("00.0010")) {
@@ -325,7 +334,8 @@ public class TestData {
 			// add prepaid according to rsc/*.xml
 			if (rechnungNr.equals(EXISTING_4_2_RNR)) {
 				ret.addZahlung(new Money(10.00), "test", new TimeTool());
-			} else if (rechnungNr.equals(EXISTING_44_2_RNR)) {
+			} else if (rechnungNr.equals(EXISTING_44_2_RNR)
+				|| rechnungNr.equals(ERRONEOUS_44_1_RNR)) {
 				ret.addZahlung(new Money(4000.00), "test", new TimeTool());
 			}
 			

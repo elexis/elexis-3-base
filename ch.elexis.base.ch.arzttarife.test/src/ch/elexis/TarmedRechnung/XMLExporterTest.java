ddfba0eae1e2936faf3c27c59ebf0c080a874859
thomashu <huster@medevit.at>
2017-07-07 10:47:06 +0200

    [6624] fixed tarmed bill output for bills with prepaid amount

--- ch.elexis.base.ch.arzttarife.test/src/ch/elexis/TarmedRechnung/XMLExporterTest.java
+++ ch.elexis.base.ch.arzttarife.test/src/ch/elexis/TarmedRechnung/XMLExporterTest.java
@@ -160,6 +160,39 @@ public class XMLExporterTest {
 	}
 	
 	@Test
+	public void doExportPrepaidTest() throws IOException{
+		TestSzenario szenario = TestData.getTestSzenarioInstance();
+		assertNotNull(szenario.getRechnungen());
+		assertFalse(szenario.getRechnungen().isEmpty());
+		XMLExporter exporter = new XMLExporter();
+		List<Rechnung> rechnungen = szenario.getRechnungen();
+		for (Rechnung rechnung : rechnungen) {
+			Document result =
+				exporter.doExport(rechnung, getTempDestination(), IRnOutputter.TYPE.ORIG, true);
+			assertNotNull(result);
+			if (rechnung.getStatus() == RnStatus.FEHLERHAFT) {
+				printFaildDocument(result);
+				fail();
+			}
+			// check if the amount_prepaid amount of the balance element is correct
+			Element root = result.getRootElement();
+			Iterator<?> iter = root.getDescendants(new ElementFilter("balance"));
+			assertTrue(iter.hasNext());
+			while (iter.hasNext()) {
+				Element balance = (Element) iter.next();
+				Attribute amount = balance.getAttribute("amount");
+				Attribute prepaid = balance.getAttribute("amount_prepaid");
+				assertNotNull(amount);
+				assertNotNull(prepaid);
+				Double amountDouble = Double.parseDouble(amount.getValue());
+				Double prepaidDouble = Double.parseDouble(prepaid.getValue());
+				assertTrue(amountDouble > 0.0D);
+				assertTrue(prepaidDouble == 0.0D);
+			}
+		}
+	}
+	
+	@Test
 	public void doExportExisting4Test() throws IOException{
 		Namespace namespace = Namespace.getNamespace("http://www.xmlData.ch/xmlInvoice/XSD"); //$NON-NLS-1$
 		
@@ -189,6 +222,45 @@ public class XMLExporterTest {
 		balance = invoice.getChild("balance", namespace);//$NON-NLS-1$
 		String due = balance.getAttributeValue("amount_due");//$NON-NLS-1$
 		assertEquals("0.00", due);
+	}
+	
+	@Test
+	public void doReExportExistingPrepaid4Test() throws IOException{
+		Namespace namespace = Namespace.getNamespace("http://www.xmlData.ch/xmlInvoice/XSD"); //$NON-NLS-1$
+		
+		TestSzenario szenario = TestData.getTestSzenarioInstance();
+		Rechnung existing = szenario.getExistingRechnung(TestData.EXISTING_4_2_RNR);
+		XMLExporter exporter = new XMLExporter();
+		Document result =
+			exporter.doExport(existing, getTempDestination(), IRnOutputter.TYPE.COPY, true);
+		assertNotNull(result);
+		if (existing.getStatus() == RnStatus.FEHLERHAFT) {
+			printFaildDocument(result);
+			fail();
+		}
+		Element invoice = result.getRootElement().getChild("invoice", namespace);//$NON-NLS-1$
+		Element balance = invoice.getChild("balance", namespace);//$NON-NLS-1$
+		String prepaid = balance.getAttributeValue("amount_prepaid");//$NON-NLS-1$
+		assertEquals("10.00", prepaid);
+		String amount = balance.getAttributeValue("amount");//$NON-NLS-1$
+		assertEquals("17.76", amount);
+		String due = balance.getAttributeValue("amount_due");//$NON-NLS-1$
+		assertEquals("7.75", due);
+		
+		result = exporter.doExport(existing, getTempDestination(), IRnOutputter.TYPE.ORIG, true);
+		assertNotNull(result);
+		if (existing.getStatus() == RnStatus.FEHLERHAFT) {
+			printFaildDocument(result);
+			fail();
+		}
+		invoice = result.getRootElement().getChild("invoice", namespace);//$NON-NLS-1$
+		balance = invoice.getChild("balance", namespace);//$NON-NLS-1$
+		prepaid = balance.getAttributeValue("amount_prepaid");//$NON-NLS-1$
+		assertEquals("10.00", prepaid);
+		amount = balance.getAttributeValue("amount");//$NON-NLS-1$
+		assertEquals("17.76", amount);
+		due = balance.getAttributeValue("amount_due");//$NON-NLS-1$
+		assertEquals("7.75", due);
 	}
 	
 	@Test
@@ -222,6 +294,45 @@ public class XMLExporterTest {
 		balance = body.getChild("balance", XMLExporter.nsinvoice);//$NON-NLS-1$
 		String due = balance.getAttributeValue("amount_due");//$NON-NLS-1$
 		assertEquals("0.00", due);
+	}
+	
+	@Test
+	public void doReExportExistingPrepaid44Test() throws IOException{
+		TestSzenario szenario = TestData.getTestSzenarioInstance();
+		Rechnung existing = szenario.getExistingRechnung(TestData.EXISTING_44_2_RNR);
+		XMLExporter exporter = new XMLExporter();
+		Document result =
+			exporter.doExport(existing, getTempDestination(), IRnOutputter.TYPE.ORIG, true);
+		assertNotNull(result);
+		if (existing.getStatus() == RnStatus.FEHLERHAFT) {
+			printFaildDocument(result);
+			fail();
+		}
+		Element payload = result.getRootElement().getChild("payload", XMLExporter.nsinvoice);//$NON-NLS-1$
+		Element body = payload.getChild("body", XMLExporter.nsinvoice);//$NON-NLS-1$
+		Element balance = body.getChild("balance", XMLExporter.nsinvoice);//$NON-NLS-1$	
+		String prepaid = balance.getAttributeValue("amount_prepaid");//$NON-NLS-1$
+		assertEquals("4000.00", prepaid);
+		String due = balance.getAttributeValue("amount_due");//$NON-NLS-1$
+		assertEquals("1217.75", due);
+		String amount = balance.getAttributeValue("amount");//$NON-NLS-1$
+		assertEquals("5217.76", amount);
+		
+		result = exporter.doExport(existing, getTempDestination(), IRnOutputter.TYPE.ORIG, true);
+		assertNotNull(result);
+		if (existing.getStatus() == RnStatus.FEHLERHAFT) {
+			printFaildDocument(result);
+			fail();
+		}
+		payload = result.getRootElement().getChild("payload", XMLExporter.nsinvoice);//$NON-NLS-1$
+		body = payload.getChild("body", XMLExporter.nsinvoice);//$NON-NLS-1$
+		balance = body.getChild("balance", XMLExporter.nsinvoice);//$NON-NLS-1$
+		prepaid = balance.getAttributeValue("amount_prepaid");//$NON-NLS-1$
+		assertEquals("4000.00", prepaid);
+		due = balance.getAttributeValue("amount_due");//$NON-NLS-1$
+		assertEquals("1217.75", due);
+		amount = balance.getAttributeValue("amount");//$NON-NLS-1$
+		assertEquals("5217.76", amount);
 	}
 	
 	private String getTempDestination(){

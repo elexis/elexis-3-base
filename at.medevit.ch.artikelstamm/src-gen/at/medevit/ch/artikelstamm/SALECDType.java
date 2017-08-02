e03c980e6cd00c9af5dc905043f9b7cac1298bd2
Marco Descher <marco@descher.at>
2017-08-02 15:13:41 +0200

    [6709] Update to artikelstamm_v5

---
+++ at.medevit.ch.artikelstamm/src-gen/at/medevit/ch/artikelstamm/SALECDType.java
@@ -0,0 +1,45 @@
+//
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
+// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2017.08.01 um 02:12:39 PM CEST 
+//
+
+
+package at.medevit.ch.artikelstamm;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java-Klasse für SALECDType.
+ * 
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="SALECDType">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="I"/>
+ *     &lt;enumeration value="A"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "SALECDType")
+@XmlEnum
+public enum SALECDType {
+
+    I,
+    A;
+
+    public String value() {
+        return name();
+    }
+
+    public static SALECDType fromValue(String v) {
+        return valueOf(v);
+    }
+
+}

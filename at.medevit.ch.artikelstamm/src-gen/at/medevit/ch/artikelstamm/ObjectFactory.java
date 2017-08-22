523530ea2326b92e04f40bb311ce914ae2fcc5e1
Marco Descher <marco@descher.at>
2017-08-22 10:02:24 +0200

    [6609] Pull Artikelstamm_v5 changes

--- at.medevit.ch.artikelstamm/src-gen/at/medevit/ch/artikelstamm/ObjectFactory.java
+++ at.medevit.ch.artikelstamm/src-gen/at/medevit/ch/artikelstamm/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2017.08.17 um 01:32:57 PM CEST 
+// Generiert: 2017.08.22 um 09:39:50 AM CEST 
 //
 
 

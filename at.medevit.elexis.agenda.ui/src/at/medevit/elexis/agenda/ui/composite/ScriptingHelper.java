2068e4c8e8bf8501bba1737ae7ba91e79d800d3d
sazgin <azgin@medevit.at>
2017-09-08 16:10:42 +0200

    [6859] Agenda neu kleine Anpassungen (#85)

--- at.medevit.elexis.agenda.ui/src/at/medevit/elexis/agenda/ui/composite/ScriptingHelper.java
+++ at.medevit.elexis.agenda.ui/src/at/medevit/elexis/agenda/ui/composite/ScriptingHelper.java
@@ -21,6 +21,42 @@ public class ScriptingHelper {
 		browser.execute(script);
 	}
 	
+	/**
+	 * parses a time of format 1900 to 19:00:00
+	 * 
+	 * @param time
+	 * @return
+	 */
+	private String parseTime(String time){
+		if (time.length() < 6 || time.lastIndexOf(":") != 5) {
+			StringBuilder builder = new StringBuilder(6);
+			time = time.replaceAll(":", "");
+			int length = time.length();
+			for (int i = 0; i < 6; i++) {
+				if (i > 0 && i % 2 == 0) {
+					builder.append(":");
+				}
+				if (i < length) {
+					char c = time.charAt(i);
+					builder.append(c);
+				} else {
+					builder.append("0");
+				}
+			}
+			return builder.toString();
+		}
+		return time;
+	}
+	
+	public void setCalenderTime(String dayStartsAt, String dayEndsAt){
+		
+		String endsAt = "$('#calendar').fullCalendar('option', 'maxTime', '%s');";
+		String startAt = "$('#calendar').fullCalendar('option', 'minTime', '%s');";
+		String script = String.format(endsAt, parseTime(dayEndsAt))
+			+ String.format(startAt, parseTime(dayStartsAt));
+		browser.execute(script);
+	}
+	
 	public void setSelectedDate(LocalDate date){
 		String gotoDate = "$('#calendar').fullCalendar('gotoDate', '%s');";
 		String script = String.format(gotoDate, date.toString());

c7bda9a747bdd11ac1b80c759bf72b673bad8a96
sazgin <azgin@medevit.at>
2017-10-06 17:26:23 +0200

    observation coding support loinc

--- at.medevit.elexis.loinc/src/at/medevit/elexis/loinc/model/LoincCode.java
+++ at.medevit.elexis.loinc/src/at/medevit/elexis/loinc/model/LoincCode.java
@@ -2,11 +2,14 @@ package at.medevit.elexis.loinc.model;
 
 import java.util.List;
 
+import ch.elexis.core.findings.ICoding;
+import ch.elexis.core.findings.codes.CodingSystem;
 import ch.elexis.data.PersistentObject;
 import ch.rgw.tools.JdbcLink;
 import ch.rgw.tools.VersionInfo;
 
-public class LoincCode extends PersistentObject implements ch.elexis.core.model.ICodeElement {
+public class LoincCode extends PersistentObject
+		implements ch.elexis.core.model.ICodeElement, ICoding {
 	public static final String TABLENAME = "at_medevit_elexis_loinc"; //$NON-NLS-1$
 	public static final String VERSION = "1.0.0"; //$NON-NLS-1$
 	
@@ -119,5 +122,15 @@ public class LoincCode extends PersistentObject implements ch.elexis.core.model.
 	public List<Object> getActions(Object context){
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	public String getSystem(){
+		return CodingSystem.LOINC_CODESYSTEM.getSystem();
+	}
+	
+	@Override
+	public String getDisplay(){
+		return getText();
 	}
 }

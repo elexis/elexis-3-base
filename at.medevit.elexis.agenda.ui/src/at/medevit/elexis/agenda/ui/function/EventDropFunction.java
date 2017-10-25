53000f8053ef3442afe572b9f4b129acee96dae2
sazgin <azgin@medevit.at>
2017-10-25 15:45:58 +0200

    [5902] Kopie Drag&Drop von Terminen in der neuen Agenda (#95)

--- at.medevit.elexis.agenda.ui/src/at/medevit/elexis/agenda/ui/function/EventDropFunction.java
+++ at.medevit.elexis.agenda.ui/src/at/medevit/elexis/agenda/ui/function/EventDropFunction.java
@@ -5,9 +5,12 @@ import java.time.LocalDateTime;
 import org.eclipse.swt.browser.Browser;
 
 import ch.elexis.agenda.data.Termin;
+import ch.elexis.agenda.series.SerienTermin;
+import ch.elexis.core.data.activator.CoreHub;
 import ch.elexis.core.data.events.ElexisEventDispatcher;
 import ch.elexis.core.ui.locks.AcquireLockBlockingUi;
 import ch.elexis.core.ui.locks.ILockHandler;
+import ch.elexis.data.Kontakt;
 import ch.rgw.tools.TimeTool;
 
 public class EventDropFunction extends AbstractBrowserFunction {
@@ -28,16 +31,42 @@ public class EventDropFunction extends AbstractBrowserFunction {
 				
 				@Override
 				public void lockAcquired(){
-					LocalDateTime startDate = getDateTimeArg(arguments[1]);
-					termin.setStartTime(new TimeTool(startDate));
-					LocalDateTime endDate = getDateTimeArg(arguments[2]);
-					termin.setEndTime(new TimeTool(endDate));
-					if (arguments.length >= 4) {
-						String bereich = (String) arguments[3];
-						if (bereich != null && !bereich.isEmpty()) {
-							termin.setBereich(bereich);
+					Termin current = termin;
+					
+					// do copy
+					if (arguments.length >= 5 && Boolean.TRUE.equals(arguments[4])) {
+						current = (Termin) termin.clone();
+						if (termin.isRecurringDate() && termin.getKontakt() == null) {
+							// take kontakt from root termin
+							Kontakt k = new SerienTermin(termin).getRootTermin().getKontakt();
+							if (k != null) {
+								current.setKontakt(k);
+							}
 						}
 					}
+					
+					// moving
+					LocalDateTime startDate = getDateTimeArg(arguments[1]);
+					current.setStartTime(new TimeTool(startDate));
+					LocalDateTime endDate = getDateTimeArg(arguments[2]);
+					current.setEndTime(new TimeTool(endDate));
+					if (arguments.length >= 4 && arguments[3] != null) {
+						String bereich = (String) arguments[3];
+						if (!bereich.isEmpty()) {
+							current.setBereich(bereich);
+						}
+					}
+					
+					// checks if that termin is copied
+					if (!current.equals(termin)) {
+						if (CoreHub.getLocalLockService().acquireLock(current).isOk()) {
+							CoreHub.getLocalLockService().releaseLock(current);
+						} else {
+							// should not happened - no lock - delete the copied termin
+							current.delete();
+						}
+					}
+					
 					ElexisEventDispatcher.reload(Termin.class);
 					redraw();
 				}
@@ -48,5 +77,4 @@ public class EventDropFunction extends AbstractBrowserFunction {
 		return null;
 	}
 	
-
 }

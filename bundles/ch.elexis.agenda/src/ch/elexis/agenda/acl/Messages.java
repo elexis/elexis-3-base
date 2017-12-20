441dbe31463aa5bbb17c73cfeefb23d3362f1cb8
Txomin Astrain <tastrain@hispeed.ch>
2017-12-20 19:30:48 +0100

    [9098] All Messages.java in agenda subdir

--- bundles/ch.elexis.agenda/src/ch/elexis/agenda/acl/Messages.java
+++ bundles/ch.elexis.agenda/src/ch/elexis/agenda/acl/Messages.java
@@ -3,7 +3,6 @@ package ch.elexis.agenda.acl;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "ch.elexis.base.l10n.messages";
 	public static String  ACLContributor_acl_administer = ch.elexis.base.l10n.Messages.ACLContributor_acl_administer;
 	public static String  ACLContributor_acl_changeAppointments = ch.elexis.base.l10n.Messages.ACLContributor_acl_changeAppointments;
 	public static String  ACLContributor_acl_daylimits = ch.elexis.base.l10n.Messages.ACLContributor_acl_daylimits;
@@ -11,8 +10,4 @@ public class Messages extends NLS {
 	public static String  ACLContributor_acl_lockappointments = ch.elexis.base.l10n.Messages.ACLContributor_acl_lockappointments;
 	public static String  ACLContributor_acl_showAppointments = ch.elexis.base.l10n.Messages.ACLContributor_acl_showAppointments;
 	public static String  ACLContributor_acl_use = ch.elexis.base.l10n.Messages.ACLContributor_acl_use;
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
 }
--- bundles/ch.elexis.agenda/src/ch/elexis/agenda/util/Messages.java
+++ bundles/ch.elexis.agenda/src/ch/elexis/agenda/util/Messages.java
@@ -14,18 +14,11 @@ package ch.elexis.agenda.util;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "ch.elexis.agenda.util.messages"; //$NON-NLS-1$
-	public static String Plannables_databaseError;
-	public static String Plannables_errorInAppointmentText;
-	public static String TermineLockedTimesUpdater_0;
-	public static String TermineLockedTimesUpdater_4;
-	public static String TermineLockedTimesUpdater_5;
-	public static String TermineLockedTimesUpdater_6;
-	public static String TermineLockedTimesUpdater_7;
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
-	
-	private Messages(){}
+	public static String  Plannables_databaseError = ch.elexis.base.l10n.Messages.Plannables_databaseError;
+	public static String  Plannables_errorInAppointmentText = ch.elexis.base.l10n.Messages.Plannables_errorInAppointmentText;
+	public static String  TermineLockedTimesUpdater_0 = ch.elexis.base.l10n.Messages.TermineLockedTimesUpdater_0;
+	public static String  TermineLockedTimesUpdater_4 = ch.elexis.base.l10n.Messages.TermineLockedTimesUpdater_4;
+	public static String  TermineLockedTimesUpdater_5 = ch.elexis.base.l10n.Messages.TermineLockedTimesUpdater_5;
+	public static String  TermineLockedTimesUpdater_6 = ch.elexis.base.l10n.Messages.TermineLockedTimesUpdater_6;
+	public static String  TermineLockedTimesUpdater_7 = ch.elexis.base.l10n.Messages.TermineLockedTimesUpdater_7;
 }

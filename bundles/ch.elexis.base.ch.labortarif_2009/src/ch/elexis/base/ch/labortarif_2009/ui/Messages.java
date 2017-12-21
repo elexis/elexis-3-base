2d2b4aeb7e8cdbd95897844277a4df834c7fc6fa
Txomin Astrain <tastrain@hispeed.ch>
2017-12-21 23:04:25 +0100

    More updates 2

--- bundles/ch.elexis.base.ch.labortarif_2009/src/ch/elexis/base/ch/labortarif_2009/ui/Messages.java
+++ bundles/ch.elexis.base.ch.labortarif_2009/src/ch/elexis/base/ch/labortarif_2009/ui/Messages.java
@@ -14,24 +14,17 @@ package ch.elexis.base.ch.labortarif_2009.ui;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "ch.elexis.base.ch.labortarif_2009.ui.messages"; //$NON-NLS-1$
-	public static String DetailDisplay_chapter;
-	public static String DetailDisplay_code;
-	public static String DetailDisplay_fachbereich;
-	public static String DetailDisplay_limitation;
-	public static String DetailDisplay_name;
-	public static String DetailDisplay_taxpoints;
-	public static String Labor2009Selector_code;
-	public static String Labor2009Selector_text;
-	public static String Preferences_automaticAdditionsGroup;
-	public static String Preferences_automaticAdditionsToLabel;
-	public static String Preferences_automaticallyCalculatioAdditions;
-	public static String Preferences_pleaseEnterMultiplier;
-	public static String Preferences_specialities;
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
-	
-	private Messages(){}
+	public static String  DetailDisplay_chapter = ch.elexis.base.l10n.Messages.DetailDisplay_chapter;
+	public static String  DetailDisplay_code = ch.elexis.base.l10n.Messages.DetailDisplay_code;
+	public static String  DetailDisplay_fachbereich = ch.elexis.base.l10n.Messages.DetailDisplay_fachbereich;
+	public static String  DetailDisplay_limitation = ch.elexis.base.l10n.Messages.DetailDisplay_limitation;
+	public static String  DetailDisplay_name = ch.elexis.base.l10n.Messages.DetailDisplay_name;
+	public static String  DetailDisplay_taxpoints = ch.elexis.base.l10n.Messages.DetailDisplay_taxpoints;
+	public static String  Labor2009Selector_code = ch.elexis.base.l10n.Messages.Labor2009Selector_code;
+	public static String  Labor2009Selector_text = ch.elexis.base.l10n.Messages.Labor2009Selector_text;
+	public static String  Preferences_automaticAdditionsGroup = ch.elexis.base.l10n.Messages.Preferences_automaticAdditionsGroup;
+	public static String  Preferences_automaticAdditionsToLabel = ch.elexis.base.l10n.Messages.Preferences_automaticAdditionsToLabel;
+	public static String  Preferences_automaticallyCalculatioAdditions = ch.elexis.base.l10n.Messages.Preferences_automaticallyCalculatioAdditions;
+	public static String  Preferences_pleaseEnterMultiplier = ch.elexis.base.l10n.Messages.Preferences_pleaseEnterMultiplier;
+	public static String  Preferences_specialities = ch.elexis.base.l10n.Messages.Preferences_specialities;
 }
--- bundles/ch.elexis.base.ch.ticode/src/ch/elexis/base/ch/ticode/Messages.java
+++ bundles/ch.elexis.base.ch.ticode/src/ch/elexis/base/ch/ticode/Messages.java
@@ -13,283 +13,141 @@ package ch.elexis.base.ch.ticode;
 
 import org.eclipse.osgi.util.NLS;
 
-public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "ch.elexis.base.ch.ticode.messages"; //$NON-NLS-1$
-	
-	public static String TICode_other;
-	
-	public static String ICDImporter_createTable;
-	
-	public static String ICDImporter_enterDirectory;
-	
-	public static String ICDImporter_icdImport;
-	
-	public static String ICDImporter_readCodes;
-	
-	public static String TICode_accessory;
-	
-	public static String TICode_accidentAbdomen;
-	
-	public static String TICode_accidentArm;
-	
-	public static String TICode_accidentHead;
-	
-	public static String TICode_accidentLeg;
-	
-	public static String TICode_AccidentOther;
-	
-	public static String TICode_accidents;
-	
-	public static String TICode_accisdentThorax;
-	
-	public static String TICode_acute;
-	
-	public static String TICode_adnexes;
-	
-	public static String TICode_allergic;
-	
-	public static String TICode_anemia;
-	
-	public static String TICode_arteries;
-	
-	public static String TICode_arthrosis;
-	
-	public static String TICode_arthtiris;
-	
-	public static String TICode_asthma;
-	
-	public static String TICode_blood;
-	
-	public static String TICode_bloodOther;
-	
-	public static String TICode_boneMarrow;
-	
-	public static String TICode_bowel;
-	
-	public static String TICode_brain;
-	
-	public static String TICode_chronic;
-	
-	public static String TICode_coagulo;
-	
-	public static String TICode_cornea;
-	
-	public static String TICode_coronaria;
-	
-	public static String TICode_cough;
-	
-	public static String TICode_cycle;
-	
-	public static String TICode_cyst;
-	
-	public static String TICode_diabetes;
-	
-	public static String TICode_diaphragm;
-	
-	public static String TICode_digestive;
-	
-	public static String TICode_digestiveOther;
-	
-	public static String TICode_docInformed;
-	
-	public static String TICode_earform;
-	
-	public static String TICode_ekcema;
-	
-	public static String TICode_embolism;
-	
-	public static String TICode_epilepsy;
-	
-	public static String TICode_esophagus;
-	
-	public static String TICode_eye;
-	
-	public static String TICode_eyemuscles;
-	
-	public static String TICode_eyeOther;
-	
-	public static String TICode_fibroma;
-	
-	public static String TICode_functional;
-	
-	public static String TICode_Heart;
-	
-	public static String TICode_heartOther;
-	
-	public static String TICode_hepatitis;
-	
-	public static String TICode_hernia;
-	
-	public static String TICode_hypertonia;
-	
-	public static String TICode_infectionOther;
-	
-	public static String TICode_infections;
-	
-	public static String TICode_infectiuous;
-	
-	public static String TICode_inflammation;
-	
-	public static String TICode_innerEar;
-	
-	public static String TICode_iris;
-	
-	public static String TICode_jaw;
-	
-	public static String TICode_jawOther;
-	
-	public static String TICode_joint;
-	
-	public static String TICode_kidney;
-	
-	public static String TICode_larynx;
-	
-	public static String TICode_left;
-	
-	public static String TICode_lid;
-	
-	public static String TICode_liver;
-	
-	public static String TICode_locomotion;
-	
-	public static String TICode_locoOther;
-	
-	public static String TICode_lung;
-	
-	public static String TICode_lungOther;
-	
-	public static String TICode_lungPleural;
-	
-	public static String TICode_lymphVessels;
-	
-	public static String TICode_male;
-	
-	public static String TICode_mammae;
-	
-	public static String TICode_metabolic;
-	
-	public static String TICode_metabolicOther;
-	
-	public static String TICode_middleEar;
-	
-	public static String TICode_migraine;
-	
-	public static String TICode_mouth;
-	
-	public static String TICode_muscle;
-	
-	public static String TICode_neoplastic;
-	
-	public static String TICode_nerves;
-	
-	public static String TICode_nervous;
-	
-	public static String TICode_nervousOther;
-	
-	public static String TICode_nonmust;
-	
-	public static String TICode_nose;
-	
-	public static String TICode_orl;
-	
-	public static String TICode_orlOther;
-	
-	public static String TICode_palsy;
-	
-	public static String TICode_pancreas;
-	
-	public static String TICode_pregnancyAbnormal;
-	
-	public static String TICode_pregnancyNormal;
-	
-	public static String TICode_prevention;
-	
-	public static String TICode_preventionCheck;
-	
-	public static String TICode_professional;
-	
-	public static String TICode_psoriasis;
-	
-	public static String TICode_psyche;
-	
-	public static String TICode_psycheOther;
-	
-	public static String TICode_psychic;
-	
-	public static String TICode_psychoorganic;
-	
-	public static String TICode_rectum;
-	
-	public static String TICode_reproduction;
-	
-	public static String TICode_reproductionOther;
-	
-	public static String TICode_retina;
-	
-	public static String TICode_rhythm;
-	
-	public static String TICode_right;
-	
-	public static String TICode_scars;
-	
-	public static String TICode_sexual;
-	
-	public static String TICode_sexualOther;
-	
-	public static String TICode_simpleInfection;
-	
-	public static String TICode_sinuses;
-	
-	public static String TICode_skin;
-	
-	public static String TICode_skinOther;
-	
-	public static String TICode_sleep;
-	
-	public static String TICode_spleen;
-	
-	public static String TICode_sterilisation;
-	
-	public static String TICode_sterility;
-	
-	public static String TICode_stones;
-	
-	public static String TICode_thyroid;
-	
-	public static String TICode_tonsil;
-	
-	public static String TICode_toothabscess;
-	
-	public static String TICode_tuberculosis;
-	
-	public static String TICode_ureters;
-	
-	public static String TICode_urinary;
-	
-	public static String TICode_urinaryOther;
-	
-	public static String TICode_uterus;
-	
-	public static String TICode_vaccination;
-	
-	public static String TICode_vaginal;
-	
-	public static String TICode_valves;
-	
-	public static String TICode_vaskular;
-	
-	public static String TICode_veins;
-	
-	public static String TICode_vertebral;
-	
-	public static String TICodeDetail_fulltext;
-	public static String ICDCodeSelectorFactory_errorLoading;
-	public static String ICDCodeSelectorFactory_couldntCreate;
-	
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
-	
-	private Messages(){}
+public class Messages extends NLS {	
+	public static String  TICode_other = ch.elexis.base.l10n.Messages.TICode_other;
+	public static String  ICDImporter_createTable = ch.elexis.base.l10n.Messages.ICDImporter_createTable;
+	public static String  ICDImporter_enterDirectory = ch.elexis.base.l10n.Messages.ICDImporter_enterDirectory;
+	public static String  ICDImporter_icdImport = ch.elexis.base.l10n.Messages.ICDImporter_icdImport;
+	public static String  ICDImporter_readCodes = ch.elexis.base.l10n.Messages.ICDImporter_readCodes;
+	public static String  TICode_accessory = ch.elexis.base.l10n.Messages.TICode_accessory;
+	public static String  TICode_accidentAbdomen = ch.elexis.base.l10n.Messages.TICode_accidentAbdomen;
+	public static String  TICode_accidentArm = ch.elexis.base.l10n.Messages.TICode_accidentArm;
+	public static String  TICode_accidentHead = ch.elexis.base.l10n.Messages.TICode_accidentHead;
+	public static String  TICode_accidentLeg = ch.elexis.base.l10n.Messages.TICode_accidentLeg;
+	public static String  TICode_AccidentOther = ch.elexis.base.l10n.Messages.TICode_AccidentOther;
+	public static String  TICode_accidents = ch.elexis.base.l10n.Messages.TICode_accidents;
+	public static String  TICode_accisdentThorax = ch.elexis.base.l10n.Messages.TICode_accisdentThorax;
+	public static String  TICode_acute = ch.elexis.base.l10n.Messages.TICode_acute;
+	public static String  TICode_adnexes = ch.elexis.base.l10n.Messages.TICode_adnexes;
+	public static String  TICode_allergic = ch.elexis.base.l10n.Messages.TICode_allergic;
+	public static String  TICode_anemia = ch.elexis.base.l10n.Messages.TICode_anemia;
+	public static String  TICode_arteries = ch.elexis.base.l10n.Messages.TICode_arteries;
+	public static String  TICode_arthrosis = ch.elexis.base.l10n.Messages.TICode_arthrosis;
+	public static String  TICode_arthtiris = ch.elexis.base.l10n.Messages.TICode_arthtiris;
+	public static String  TICode_asthma = ch.elexis.base.l10n.Messages.TICode_asthma;
+	public static String  TICode_blood = ch.elexis.base.l10n.Messages.TICode_blood;
+	public static String  TICode_bloodOther = ch.elexis.base.l10n.Messages.TICode_bloodOther;
+	public static String  TICode_boneMarrow = ch.elexis.base.l10n.Messages.TICode_boneMarrow;
+	public static String  TICode_bowel = ch.elexis.base.l10n.Messages.TICode_bowel;
+	public static String  TICode_brain = ch.elexis.base.l10n.Messages.TICode_brain;
+	public static String  TICode_chronic = ch.elexis.base.l10n.Messages.TICode_chronic;
+	public static String  TICode_coagulo = ch.elexis.base.l10n.Messages.TICode_coagulo;
+	public static String  TICode_cornea = ch.elexis.base.l10n.Messages.TICode_cornea;
+	public static String  TICode_coronaria = ch.elexis.base.l10n.Messages.TICode_coronaria;
+	public static String  TICode_cough = ch.elexis.base.l10n.Messages.TICode_cough;
+	public static String  TICode_cycle = ch.elexis.base.l10n.Messages.TICode_cycle;
+	public static String  TICode_cyst = ch.elexis.base.l10n.Messages.TICode_cyst;
+	public static String  TICode_diabetes = ch.elexis.base.l10n.Messages.TICode_diabetes;
+	public static String  TICode_diaphragm = ch.elexis.base.l10n.Messages.TICode_diaphragm;
+	public static String  TICode_digestive = ch.elexis.base.l10n.Messages.TICode_digestive;
+	public static String  TICode_digestiveOther = ch.elexis.base.l10n.Messages.TICode_digestiveOther;
+	public static String  TICode_docInformed = ch.elexis.base.l10n.Messages.TICode_docInformed;
+	public static String  TICode_earform = ch.elexis.base.l10n.Messages.TICode_earform;
+	public static String  TICode_ekcema = ch.elexis.base.l10n.Messages.TICode_ekcema;
+	public static String  TICode_embolism = ch.elexis.base.l10n.Messages.TICode_embolism;
+	public static String  TICode_epilepsy = ch.elexis.base.l10n.Messages.TICode_epilepsy;
+	public static String  TICode_esophagus = ch.elexis.base.l10n.Messages.TICode_esophagus;
+	public static String  TICode_eye = ch.elexis.base.l10n.Messages.TICode_eye;
+	public static String  TICode_eyemuscles = ch.elexis.base.l10n.Messages.TICode_eyemuscles;
+	public static String  TICode_eyeOther = ch.elexis.base.l10n.Messages.TICode_eyeOther;
+	public static String  TICode_fibroma = ch.elexis.base.l10n.Messages.TICode_fibroma;
+	public static String  TICode_functional = ch.elexis.base.l10n.Messages.TICode_functional;
+	public static String  TICode_Heart = ch.elexis.base.l10n.Messages.TICode_Heart;
+	public static String  TICode_heartOther = ch.elexis.base.l10n.Messages.TICode_heartOther;
+	public static String  TICode_hepatitis = ch.elexis.base.l10n.Messages.TICode_hepatitis;
+	public static String  TICode_hernia = ch.elexis.base.l10n.Messages.TICode_hernia;
+	public static String  TICode_hypertonia = ch.elexis.base.l10n.Messages.TICode_hypertonia;
+	public static String  TICode_infectionOther = ch.elexis.base.l10n.Messages.TICode_infectionOther;
+	public static String  TICode_infections = ch.elexis.base.l10n.Messages.TICode_infections;
+	public static String  TICode_infectiuous = ch.elexis.base.l10n.Messages.TICode_infectiuous;
+	public static String  TICode_inflammation = ch.elexis.base.l10n.Messages.TICode_inflammation;
+	public static String  TICode_innerEar = ch.elexis.base.l10n.Messages.TICode_innerEar;
+	public static String  TICode_iris = ch.elexis.base.l10n.Messages.TICode_iris;
+	public static String  TICode_jaw = ch.elexis.base.l10n.Messages.TICode_jaw;
+	public static String  TICode_jawOther = ch.elexis.base.l10n.Messages.TICode_jawOther;
+	public static String  TICode_joint = ch.elexis.base.l10n.Messages.TICode_joint;
+	public static String  TICode_kidney = ch.elexis.base.l10n.Messages.TICode_kidney;
+	public static String  TICode_larynx = ch.elexis.base.l10n.Messages.TICode_larynx;
+	public static String  TICode_left = ch.elexis.base.l10n.Messages.TICode_left;
+	public static String  TICode_lid = ch.elexis.base.l10n.Messages.TICode_lid;
+	public static String  TICode_liver = ch.elexis.base.l10n.Messages.TICode_liver;
+	public static String  TICode_locomotion = ch.elexis.base.l10n.Messages.TICode_locomotion;
+	public static String  TICode_locoOther = ch.elexis.base.l10n.Messages.TICode_locoOther;
+	public static String  TICode_lung = ch.elexis.base.l10n.Messages.TICode_lung;
+	public static String  TICode_lungOther = ch.elexis.base.l10n.Messages.TICode_lungOther;
+	public static String  TICode_lungPleural = ch.elexis.base.l10n.Messages.TICode_lungPleural;
+	public static String  TICode_lymphVessels = ch.elexis.base.l10n.Messages.TICode_lymphVessels;
+	public static String  TICode_male = ch.elexis.base.l10n.Messages.TICode_male;
+	public static String  TICode_mammae = ch.elexis.base.l10n.Messages.TICode_mammae;
+	public static String  TICode_metabolic = ch.elexis.base.l10n.Messages.TICode_metabolic;
+	public static String  TICode_metabolicOther = ch.elexis.base.l10n.Messages.TICode_metabolicOther;
+	public static String  TICode_middleEar = ch.elexis.base.l10n.Messages.TICode_middleEar;
+	public static String  TICode_migraine = ch.elexis.base.l10n.Messages.TICode_migraine;
+	public static String  TICode_mouth = ch.elexis.base.l10n.Messages.TICode_mouth;
+	public static String  TICode_muscle = ch.elexis.base.l10n.Messages.TICode_muscle;
+	public static String  TICode_neoplastic = ch.elexis.base.l10n.Messages.TICode_neoplastic;
+	public static String  TICode_nerves = ch.elexis.base.l10n.Messages.TICode_nerves;
+	public static String  TICode_nervous = ch.elexis.base.l10n.Messages.TICode_nervous;
+	public static String  TICode_nervousOther = ch.elexis.base.l10n.Messages.TICode_nervousOther;
+	public static String  TICode_nonmust = ch.elexis.base.l10n.Messages.TICode_nonmust;
+	public static String  TICode_nose = ch.elexis.base.l10n.Messages.TICode_nose;
+	public static String  TICode_orl = ch.elexis.base.l10n.Messages.TICode_orl;
+	public static String  TICode_orlOther = ch.elexis.base.l10n.Messages.TICode_orlOther;
+	public static String  TICode_palsy = ch.elexis.base.l10n.Messages.TICode_palsy;
+	public static String  TICode_pancreas = ch.elexis.base.l10n.Messages.TICode_pancreas;
+	public static String  TICode_pregnancyAbnormal = ch.elexis.base.l10n.Messages.TICode_pregnancyAbnormal;
+	public static String  TICode_pregnancyNormal = ch.elexis.base.l10n.Messages.TICode_pregnancyNormal;
+	public static String  TICode_prevention = ch.elexis.base.l10n.Messages.TICode_prevention;
+	public static String  TICode_preventionCheck = ch.elexis.base.l10n.Messages.TICode_preventionCheck;
+	public static String  TICode_professional = ch.elexis.base.l10n.Messages.TICode_professional;
+	public static String  TICode_psoriasis = ch.elexis.base.l10n.Messages.TICode_psoriasis;
+	public static String  TICode_psyche = ch.elexis.base.l10n.Messages.TICode_psyche;
+	public static String  TICode_psycheOther = ch.elexis.base.l10n.Messages.TICode_psycheOther;
+	public static String  TICode_psychic = ch.elexis.base.l10n.Messages.TICode_psychic;
+	public static String  TICode_psychoorganic = ch.elexis.base.l10n.Messages.TICode_psychoorganic;
+	public static String  TICode_rectum = ch.elexis.base.l10n.Messages.TICode_rectum;
+	public static String  TICode_reproduction = ch.elexis.base.l10n.Messages.TICode_reproduction;
+	public static String  TICode_reproductionOther = ch.elexis.base.l10n.Messages.TICode_reproductionOther;
+	public static String  TICode_retina = ch.elexis.base.l10n.Messages.TICode_retina;
+	public static String  TICode_rhythm = ch.elexis.base.l10n.Messages.TICode_rhythm;
+	public static String  TICode_right = ch.elexis.base.l10n.Messages.TICode_right;
+	public static String  TICode_scars = ch.elexis.base.l10n.Messages.TICode_scars;
+	public static String  TICode_sexual = ch.elexis.base.l10n.Messages.TICode_sexual;
+	public static String  TICode_sexualOther = ch.elexis.base.l10n.Messages.TICode_sexualOther;
+	public static String  TICode_simpleInfection = ch.elexis.base.l10n.Messages.TICode_simpleInfection;
+	public static String  TICode_sinuses = ch.elexis.base.l10n.Messages.TICode_sinuses;
+	public static String  TICode_skin = ch.elexis.base.l10n.Messages.TICode_skin;
+	public static String  TICode_skinOther = ch.elexis.base.l10n.Messages.TICode_skinOther;
+	public static String  TICode_sleep = ch.elexis.base.l10n.Messages.TICode_sleep;
+	public static String  TICode_spleen = ch.elexis.base.l10n.Messages.TICode_spleen;
+	public static String  TICode_sterilisation = ch.elexis.base.l10n.Messages.TICode_sterilisation;
+	public static String  TICode_sterility = ch.elexis.base.l10n.Messages.TICode_sterility;
+	public static String  TICode_stones = ch.elexis.base.l10n.Messages.TICode_stones;
+	public static String  TICode_thyroid = ch.elexis.base.l10n.Messages.TICode_thyroid;
+	public static String  TICode_tonsil = ch.elexis.base.l10n.Messages.TICode_tonsil;
+	public static String  TICode_toothabscess = ch.elexis.base.l10n.Messages.TICode_toothabscess;
+	public static String  TICode_tuberculosis = ch.elexis.base.l10n.Messages.TICode_tuberculosis;
+	public static String  TICode_ureters = ch.elexis.base.l10n.Messages.TICode_ureters;
+	public static String  TICode_urinary = ch.elexis.base.l10n.Messages.TICode_urinary;
+	public static String  TICode_urinaryOther = ch.elexis.base.l10n.Messages.TICode_urinaryOther;
+	public static String  TICode_uterus = ch.elexis.base.l10n.Messages.TICode_uterus;
+	public static String  TICode_vaccination = ch.elexis.base.l10n.Messages.TICode_vaccination;
+	public static String  TICode_vaginal = ch.elexis.base.l10n.Messages.TICode_vaginal;
+	public static String  TICode_valves = ch.elexis.base.l10n.Messages.TICode_valves;
+	public static String  TICode_vaskular = ch.elexis.base.l10n.Messages.TICode_vaskular;
+	public static String  TICode_veins = ch.elexis.base.l10n.Messages.TICode_veins;
+	public static String  TICode_vertebral = ch.elexis.base.l10n.Messages.TICode_vertebral;
+	public static String  TICodeDetail_fulltext = ch.elexis.base.l10n.Messages.TICodeDetail_fulltext;
+	public static String  ICDCodeSelectorFactory_errorLoading = ch.elexis.base.l10n.Messages.ICDCodeSelectorFactory_errorLoading;
+	public static String  ICDCodeSelectorFactory_couldntCreate = ch.elexis.base.l10n.Messages.ICDCodeSelectorFactory_couldntCreate;
 }

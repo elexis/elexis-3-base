8aebb319454854829fb6aab7ad88cb7ed5275758
Txomin Astrain <tastrain@hispeed.ch>
2017-12-21 23:04:25 +0100

    More updates 2

--- bundles/ch.elexis.base.ch.labortarif_2009/src/ch/elexis/labortarif2009/data/Messages.java
+++ bundles/ch.elexis.base.ch.labortarif_2009/src/ch/elexis/labortarif2009/data/Messages.java
@@ -14,13 +14,6 @@ package ch.elexis.labortarif2009.data;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "ch.elexis.labortarif2009.data.messages"; //$NON-NLS-1$
-	public static String Importer_importEAL;
-	public static String Importer_selectFile;
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
-	
-	private Messages(){}
+	public static String  Importer_importEAL = ch.elexis.base.l10n.Messages.Importer_importEAL;
+	public static String  Importer_selectFile = ch.elexis.base.l10n.Messages.Importer_selectFile;
 }
--- bundles/ch.elexis.base.konsextension.bildanzeige/src/ch/elexis/base/konsextension/bildanzeige/Messages.java
+++ bundles/ch.elexis.base.konsextension.bildanzeige/src/ch/elexis/base/konsextension/bildanzeige/Messages.java
@@ -3,33 +3,26 @@ package ch.elexis.base.konsextension.bildanzeige;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "ch.elexis.base.konsextension.bildanzeige.messages"; //$NON-NLS-1$
-	public static String Bild_BadVersionNUmber;
-	public static String Bild_NoPatientSelected;
-	public static String Bild_VersionConflict;
-	public static String Bild_YouShouldSelectAPatient;
-	public static String BildanzeigeFenster_Cannot;
-	public static String BildanzeigeFenster_Close;
-	public static String BildanzeigeFenster_Create;
-	public static String BildanzeigeFenster_Error;
-	public static String BildanzeigeFenster_ErrorWriting;
-	public static String BildanzeigeFenster_Export;
-	public static String BildImportDialog_DescriptionOfImage;
-	public static String BildImportDialog_ImportCaption;
-	public static String BildImportDialog_ImportMessage;
-	public static String BildImportDialog_ImportTitle;
-	public static String BildImportDialog_JPEG_Description;
-	public static String BildImportDialog_PNG_Description;
-	public static String BildImportDialog_StorageFormat;
-	public static String BildImportDialog_TitleOfImage;
-	public static String KonsExtension_ErrorLoading;
-	public static String KonsExtension_Image;
-	public static String KonsExtension_ImageCouldnotBeLoaded;
-	public static String KonsExtension_InsertImage;
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
-	
-	private Messages(){}
+	public static String  Bild_BadVersionNUmber = ch.elexis.base.l10n.Messages.Bild_BadVersionNUmber;
+	public static String  Bild_NoPatientSelected = ch.elexis.base.l10n.Messages.Bild_NoPatientSelected;
+	public static String  Bild_VersionConflict = ch.elexis.base.l10n.Messages.Bild_VersionConflict;
+	public static String  Bild_YouShouldSelectAPatient = ch.elexis.base.l10n.Messages.Bild_YouShouldSelectAPatient;
+	public static String  BildanzeigeFenster_Cannot = ch.elexis.base.l10n.Messages.BildanzeigeFenster_Cannot;
+	public static String  BildanzeigeFenster_Close = ch.elexis.base.l10n.Messages.BildanzeigeFenster_Close;
+	public static String  BildanzeigeFenster_Create = ch.elexis.base.l10n.Messages.BildanzeigeFenster_Create;
+	public static String  BildanzeigeFenster_Error = ch.elexis.base.l10n.Messages.BildanzeigeFenster_Error;
+	public static String  BildanzeigeFenster_ErrorWriting = ch.elexis.base.l10n.Messages.BildanzeigeFenster_ErrorWriting;
+	public static String  BildanzeigeFenster_Export = ch.elexis.base.l10n.Messages.BildanzeigeFenster_Export;
+	public static String  BildImportDialog_DescriptionOfImage = ch.elexis.base.l10n.Messages.BildImportDialog_DescriptionOfImage;
+	public static String  BildImportDialog_ImportCaption = ch.elexis.base.l10n.Messages.BildImportDialog_ImportCaption;
+	public static String  BildImportDialog_ImportMessage = ch.elexis.base.l10n.Messages.BildImportDialog_ImportMessage;
+	public static String  BildImportDialog_ImportTitle = ch.elexis.base.l10n.Messages.BildImportDialog_ImportTitle;
+	public static String  BildImportDialog_JPEG_Description = ch.elexis.base.l10n.Messages.BildImportDialog_JPEG_Description;
+	public static String  BildImportDialog_PNG_Description = ch.elexis.base.l10n.Messages.BildImportDialog_PNG_Description;
+	public static String  BildImportDialog_StorageFormat = ch.elexis.base.l10n.Messages.BildImportDialog_StorageFormat;
+	public static String  BildImportDialog_TitleOfImage = ch.elexis.base.l10n.Messages.BildImportDialog_TitleOfImage;
+	public static String  KonsExtension_ErrorLoading = ch.elexis.base.l10n.Messages.KonsExtension_ErrorLoading;
+	public static String  KonsExtension_Image = ch.elexis.base.l10n.Messages.KonsExtension_Image;
+	public static String  KonsExtension_ImageCouldnotBeLoaded = ch.elexis.base.l10n.Messages.KonsExtension_ImageCouldnotBeLoaded;
+	public static String  KonsExtension_InsertImage = ch.elexis.base.l10n.Messages.KonsExtension_InsertImage;
 }

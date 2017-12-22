f9445c43301a3c3eb748192f4c05f50e4837eeab
Txomin Astrain <tastrain@hispeed.ch>
2017-12-22 18:10:13 +0100

    [9098] Finished with the changes that were asked

--- bundles/ch.elexis.omnivore.ui/src/ch/elexis/omnivore/ui/preferences/Messages.java
+++ bundles/ch.elexis.omnivore.ui/src/ch/elexis/omnivore/ui/preferences/Messages.java
@@ -14,66 +14,102 @@ package ch.elexis.omnivore.ui.preferences;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "ch.elexis.omnivore.ui.preferences.messages";
-	
-	public static String FileImportDialog_categoryLabel;
-	public static String FileImportDialog_newCategoryText;
-	public static String FileImportDialog_newCategoryCaption;
-	public static String FileImportDialog_dateLabel;
-	public static String FileImportDialog_importCaption;
-	public static String Preferences_storeInFS;
-	public static String Preferences_pathForDocs;
-	public static String Preferences_dateModifiable;
-	public static String FileImportDialog_importFileCaption;
-	public static String FileImportDialog_importFileText;
-	public static String FileImportDialog_keywordsLabel;
-	public static String FileImportDialog_titleLabel;
-	public static String OmnivoreView_categoryColumn;
-	public static String OmnivoreView_configErrorCaption;
-	public static String OmnivoreView_configErrorText;
-	public static String OmnivoreView_dataSources;
-	public static String OmnivoreView_dateColumn;
-	public static String OmnivoreView_deleteActionCaption;
-	public static String OmnivoreView_deleteActionToolTip;
-	public static String OmnivoreView_editActionCaption;
-	public static String OmnivoreView_editActionTooltip;
-	public static String OmnivoreView_exportActionCaption;
-	public static String OmnivoreView_exportActionTooltip;
-	public static String OmnivoreView_flatActionCaption;
-	public static String OmnivoreView_flatActionTooltip;
-	public static String OmnivoreView_importActionCaption;
-	public static String OmnivoreView_importActionToolTip;
-	public static String OmnivoreView_keywordsColumn;
-	public static String OmnivoreView_reallyDeleteCaption;
-	public static String OmnivoreView_reallyDeleteContents;
-	public static String OmnivoreView_searchKeywordsLabel;
-	public static String OmnivoreView_searchTitleLabel;
-	public static String OmnivoreView_titleColumn;
-	public static String Preferences_DEST_DIR;
-	public static String Preferences_construction_of_temporary_filename;
-	public static String Preferences_cotf_add_trail_char;
-	public static String Preferences_cotf_constant1;
-	public static String Preferences_cotf_constant2;
-	public static String Preferences_cotf_dguid;
-	public static String Preferences_cotf_dk;
-	public static String Preferences_cotf_dob;
-	public static String Preferences_cotf_dt;
-	public static String Preferences_cotf_fill_lead_char;
-	public static String Preferences_cotf_fn;
-	public static String Preferences_cotf_gn;
-	public static String Preferences_cotf_num_digits;
-	public static String Preferences_cotf_pid;
-	public static String Preferences_cotf_random;
-	public static String Omnivore_ErrNoActivator;
-	public static String Preferences_MAX_FILENAME_LENGTH;
-	public static String Preferences_Rule;
-	public static String Preferences_SRC_PATTERN;
-	public static String Preferences_automatic_archiving_of_processed_files;
-	public static String Preferences_omnivore;
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
-	
-	private Messages(){}
+	public static String FileImportDialog_categoryLabel =
+		ch.elexis.base.l10n.Messages.FileImportDialog_categoryLabel;
+	public static String FileImportDialog_newCategoryText =
+		ch.elexis.base.l10n.Messages.FileImportDialog_newCategoryText;
+	public static String FileImportDialog_newCategoryCaption =
+		ch.elexis.base.l10n.Messages.FileImportDialog_newCategoryCaption;
+	public static String FileImportDialog_dateLabel =
+		ch.elexis.base.l10n.Messages.FileImportDialog_dateLabel;
+	public static String FileImportDialog_importCaption =
+		ch.elexis.base.l10n.Messages.FileImportDialog_importCaption;
+	public static String Preferences_storeInFS = ch.elexis.base.l10n.Messages.Preferences_storeInFS;
+	public static String Preferences_pathForDocs =
+		ch.elexis.base.l10n.Messages.Preferences_pathForDocs;
+	public static String Preferences_dateModifiable =
+		ch.elexis.base.l10n.Messages.Preferences_dateModifiable;
+	public static String FileImportDialog_importFileCaption =
+		ch.elexis.base.l10n.Messages.FileImportDialog_importFileCaption;
+	public static String FileImportDialog_importFileText =
+		ch.elexis.base.l10n.Messages.FileImportDialog_importFileText;
+	public static String FileImportDialog_keywordsLabel =
+		ch.elexis.base.l10n.Messages.FileImportDialog_keywordsLabel;
+	public static String FileImportDialog_titleLabel =
+		ch.elexis.base.l10n.Messages.FileImportDialog_titleLabel;
+	public static String OmnivoreView_categoryColumn =
+		ch.elexis.base.l10n.Messages.OmnivoreView_categoryColumn;
+	public static String OmnivoreView_configErrorCaption =
+		ch.elexis.base.l10n.Messages.OmnivoreView_configErrorCaption;
+	public static String OmnivoreView_configErrorText =
+		ch.elexis.base.l10n.Messages.OmnivoreView_configErrorText;
+	public static String OmnivoreView_dataSources =
+		ch.elexis.base.l10n.Messages.OmnivoreView_dataSources;
+	public static String OmnivoreView_dateColumn =
+		ch.elexis.base.l10n.Messages.OmnivoreView_dateColumn;
+	public static String OmnivoreView_deleteActionCaption =
+		ch.elexis.base.l10n.Messages.OmnivoreView_deleteActionCaption;
+	public static String OmnivoreView_deleteActionToolTip =
+		ch.elexis.base.l10n.Messages.OmnivoreView_deleteActionToolTip;
+	public static String OmnivoreView_editActionCaption =
+		ch.elexis.base.l10n.Messages.OmnivoreView_editActionCaption;
+	public static String OmnivoreView_editActionTooltip =
+		ch.elexis.base.l10n.Messages.OmnivoreView_editActionTooltip;
+	public static String OmnivoreView_exportActionCaption =
+		ch.elexis.base.l10n.Messages.OmnivoreView_exportActionCaption;
+	public static String OmnivoreView_exportActionTooltip =
+		ch.elexis.base.l10n.Messages.OmnivoreView_exportActionTooltip;
+	public static String OmnivoreView_flatActionCaption =
+		ch.elexis.base.l10n.Messages.OmnivoreView_flatActionCaption;
+	public static String OmnivoreView_flatActionTooltip =
+		ch.elexis.base.l10n.Messages.OmnivoreView_flatActionTooltip;
+	public static String OmnivoreView_importActionCaption =
+		ch.elexis.base.l10n.Messages.OmnivoreView_importActionCaption;
+	public static String OmnivoreView_importActionToolTip =
+		ch.elexis.base.l10n.Messages.OmnivoreView_importActionToolTip;
+	public static String OmnivoreView_keywordsColumn =
+		ch.elexis.base.l10n.Messages.OmnivoreView_keywordsColumn;
+	public static String OmnivoreView_reallyDeleteCaption =
+		ch.elexis.base.l10n.Messages.OmnivoreView_reallyDeleteCaption;
+	public static String OmnivoreView_reallyDeleteContents =
+		ch.elexis.base.l10n.Messages.OmnivoreView_reallyDeleteContents;
+	public static String OmnivoreView_searchKeywordsLabel =
+		ch.elexis.base.l10n.Messages.OmnivoreView_searchKeywordsLabel;
+	public static String OmnivoreView_searchTitleLabel =
+		ch.elexis.base.l10n.Messages.OmnivoreView_searchTitleLabel;
+	public static String OmnivoreView_titleColumn =
+		ch.elexis.base.l10n.Messages.OmnivoreView_titleColumn;
+	public static String Preferences_DEST_DIR = ch.elexis.base.l10n.Messages.Preferences_DEST_DIR;
+	public static String Preferences_construction_of_temporary_filename =
+		ch.elexis.base.l10n.Messages.Preferences_construction_of_temporary_filename;
+	public static String Preferences_cotf_add_trail_char =
+		ch.elexis.base.l10n.Messages.Preferences_cotf_add_trail_char;
+	public static String Preferences_cotf_constant1 =
+		ch.elexis.base.l10n.Messages.Preferences_cotf_constant1;
+	public static String Preferences_cotf_constant2 =
+		ch.elexis.base.l10n.Messages.Preferences_cotf_constant2;
+	public static String Preferences_cotf_dguid =
+		ch.elexis.base.l10n.Messages.Preferences_cotf_dguid;
+	public static String Preferences_cotf_dk = ch.elexis.base.l10n.Messages.Preferences_cotf_dk;
+	public static String Preferences_cotf_dob = ch.elexis.base.l10n.Messages.Preferences_cotf_dob;
+	public static String Preferences_cotf_dt = ch.elexis.base.l10n.Messages.Preferences_cotf_dt;
+	public static String Preferences_cotf_fill_lead_char =
+		ch.elexis.base.l10n.Messages.Preferences_cotf_fill_lead_char;
+	public static String Preferences_cotf_fn = ch.elexis.base.l10n.Messages.Preferences_cotf_fn;
+	public static String Preferences_cotf_gn = ch.elexis.base.l10n.Messages.Preferences_cotf_gn;
+	public static String Preferences_cotf_num_digits =
+		ch.elexis.base.l10n.Messages.Preferences_cotf_num_digits;
+	public static String Preferences_cotf_pid = ch.elexis.base.l10n.Messages.Preferences_cotf_pid;
+	public static String Preferences_cotf_random =
+		ch.elexis.base.l10n.Messages.Preferences_cotf_random;
+	public static String Omnivore_ErrNoActivator =
+		ch.elexis.base.l10n.Messages.Omnivore_ErrNoActivator;
+	public static String Preferences_MAX_FILENAME_LENGTH =
+		ch.elexis.base.l10n.Messages.Preferences_MAX_FILENAME_LENGTH;
+	public static String Preferences_Rule = ch.elexis.base.l10n.Messages.Preferences_Rule;
+	public static String Preferences_SRC_PATTERN =
+		ch.elexis.base.l10n.Messages.Preferences_SRC_PATTERN;
+	public static String Preferences_automatic_archiving_of_processed_files =
+		ch.elexis.base.l10n.Messages.Preferences_automatic_archiving_of_processed_files;
+	public static String Preferences_omnivore = ch.elexis.base.l10n.Messages.Preferences_omnivore;
 }
--- bundles/ch.elexis.stickynotes/src/ch/elexis/stickynotes/ui/Messages.java
+++ bundles/ch.elexis.stickynotes/src/ch/elexis/stickynotes/ui/Messages.java
@@ -14,16 +14,14 @@ package ch.elexis.stickynotes.ui;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "ch.elexis.stickynotes.ui.messages"; //$NON-NLS-1$
-	public static String Preferences_BackgroundColor;
-	public static String Preferences_ForegroundColor;
-	public static String StickyNotesView_NoPatientSelected;
-	public static String StickyNotesView_StickyNotesName;
-	public static String StickyNotesView_StickyNotesNameDash;
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
-	
-	private Messages(){}
+	public static String Preferences_BackgroundColor =
+		ch.elexis.base.l10n.Messages.Preferences_BackgroundColor;
+	public static String Preferences_ForegroundColor =
+		ch.elexis.base.l10n.Messages.Preferences_ForegroundColor;
+	public static String StickyNotesView_NoPatientSelected =
+		ch.elexis.base.l10n.Messages.StickyNotesView_NoPatientSelected;
+	public static String StickyNotesView_StickyNotesName =
+		ch.elexis.base.l10n.Messages.StickyNotesView_StickyNotesName;
+	public static String StickyNotesView_StickyNotesNameDash =
+		ch.elexis.base.l10n.Messages.StickyNotesView_StickyNotesNameDash;
 }

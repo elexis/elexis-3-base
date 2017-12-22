c61238f1a08bbb56b98ed138516b795d23b8b14e
Txomin Astrain <tastrain@hispeed.ch>
2017-12-22 15:47:05 +0100

    More updates...

--- bundles/ch.elexis.base.messages/src/ch/elexis/base/messages/Messages.java
+++ bundles/ch.elexis.base.messages/src/ch/elexis/base/messages/Messages.java
@@ -3,31 +3,23 @@ package ch.elexis.base.messages;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "ch.elexis.base.messages.messages"; //$NON-NLS-1$
-	public static String MsgDetailDialog_asReminder;
-	public static String MsgDetailDialog_cancel;
-	public static String MsgDetailDialog_createMessage;
-	public static String MsgDetailDialog_delete;
-	public static String MsgDetailDialog_from;
-	public static String MsgDetailDialog_message;
-	public static String MsgDetailDialog_answer;
-	public static String MsgDetailDialog_messageDated;
-	public static String MsgDetailDialog_readMessage;
-	public static String MsgDetailDialog_reply;
-	public static String MsgDetailDialog_send;
-	public static String MsgDetailDialog_to;
-	public static String Prefs_Messages;
-	public static String Prefs_SoundSettings;
-	public static String Prefs_TurnOnSound;
-	public static String Prefs_BrowseFS;
-	public static String Prefs_FS_Open;
-	public static String Prefs_DialogSettings;
-	public static String Prefs_btnAnswerAutoclear;
-	
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
-	
-	private Messages(){}
+	public static String  MsgDetailDialog_asReminder = ch.elexis.base.l10n.Messages.MsgDetailDialog_asReminder;
+	public static String  MsgDetailDialog_cancel = ch.elexis.base.l10n.Messages.MsgDetailDialog_cancel;
+	public static String  MsgDetailDialog_createMessage = ch.elexis.base.l10n.Messages.MsgDetailDialog_createMessage;
+	public static String  MsgDetailDialog_delete = ch.elexis.base.l10n.Messages.MsgDetailDialog_delete;
+	public static String  MsgDetailDialog_from = ch.elexis.base.l10n.Messages.MsgDetailDialog_from;
+	public static String  MsgDetailDialog_message = ch.elexis.base.l10n.Messages.MsgDetailDialog_message;
+	public static String  MsgDetailDialog_answer = ch.elexis.base.l10n.Messages.MsgDetailDialog_answer;
+	public static String  MsgDetailDialog_messageDated = ch.elexis.base.l10n.Messages.MsgDetailDialog_messageDated;
+	public static String  MsgDetailDialog_readMessage = ch.elexis.base.l10n.Messages.MsgDetailDialog_readMessage;
+	public static String  MsgDetailDialog_reply = ch.elexis.base.l10n.Messages.MsgDetailDialog_reply;
+	public static String  MsgDetailDialog_send = ch.elexis.base.l10n.Messages.MsgDetailDialog_send;
+	public static String  MsgDetailDialog_to = ch.elexis.base.l10n.Messages.MsgDetailDialog_to;
+	public static String  Prefs_Messages = ch.elexis.base.l10n.Messages.Prefs_Messages;
+	public static String  Prefs_SoundSettings = ch.elexis.base.l10n.Messages.Prefs_SoundSettings;
+	public static String  Prefs_TurnOnSound = ch.elexis.base.l10n.Messages.Prefs_TurnOnSound;
+	public static String  Prefs_BrowseFS = ch.elexis.base.l10n.Messages.Prefs_BrowseFS;
+	public static String  Prefs_FS_Open = ch.elexis.base.l10n.Messages.Prefs_FS_Open;
+	public static String  Prefs_DialogSettings = ch.elexis.base.l10n.Messages.Prefs_DialogSettings;
+	public static String  Prefs_btnAnswerAutoclear = ch.elexis.base.l10n.Messages.Prefs_btnAnswerAutoclear;
 }
--- bundles/ch.elexis.externe_dokumente/src/ch/elexis/extdoc/Messages.java
+++ bundles/ch.elexis.externe_dokumente/src/ch/elexis/extdoc/Messages.java
@@ -3,63 +3,102 @@ package ch.elexis.extdoc;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "ch.elexis.extdoc.messages"; //$NON-NLS-1$
-	public static String ExterneDokumente_pop_menu;
-	public static String ExterneDokumente_delete_files;
-	public static String ExterneDokumente_click_to_sort_by_date;
-	public static String ExterneDokumente_click_to_sort_by_name;
-	public static String ExterneDokumente_Concerns;
-	public static String ExterneDokumente_could_not_read_File;
-	public static String ExterneDokumente_delete;
-	public static String ExterneDokumente_delete_doc;
-	public static String ExterneDokumente_email_app;
-	public static String ExterneDokumente_exception_while_copying;
-	public static String ExterneDokumente_externe_dokumente;
-	public static String ExterneDokumente_file_date;
-	public static String ExterneDokumente_file_name;
-	public static String ExterneDokumente_import_failed;
-	public static String ExterneDokumente_imported;
-	public static String ExterneDokumente_loading;
-	public static String ExterneDokumente_modified_time;
-	public static String ExterneDokumente_move_into_subdir;
-	public static String ExterneDokumente_move_into_subdir_tooltip;
-	public static String ExterneDokumente_no_files_found;
-	public static String ExterneDokumente_no_patient_found;
-	public static String ExterneDokumente_not_defined_in_preferences;
-	public static String ExterneDokumente_open;
-	public static String ExterneDokumente_OpenFileTip;
-	public static String ExterneDokumente_openFolder;
-	public static String ExterneDokumente_openFolderTip;
-	public static String ExterneDokumente_path_name_preference;
-	public static String ExterneDokumente_propeties;
-	public static String ExterneDokumente_read_errpor;
-	public static String ExterneDokumente_rename_or_change_date;
-	public static String ExterneDokumente_renaming_file;
-	public static String ExterneDokumente_select_patient_first;
-	public static String ExterneDokumente_sendEmail;
-	public static String ExterneDokumente_sendEmailTip;
-	public static String ExterneDokumente_shold_doc_be_delted;
-	public static String ExterneDokumente_shorthand_for_path;
-	public static String ExterneDokumente_verify_files;
-	public static String ExterneDokumente_verify_files_Belong_to_patient;
-	public static String MoveIntoSubDirsDialog_finished;
-	public static String MoveIntoSubDirsDialog_no_old_Files_found;
-	public static String MoveIntoSubDirsDialog_sub_task;
-	public static String VerifierDialog_name;
-	public static String VerifierDialog_verify_job_name;
-	public static String FileEditDialog_attribute_to_new_patient;
-	public static String FileEditDialog_extension;
-	public static String FileEditDialog_file_name;
-	public static String FileEditDialog_file_date_and_explanation;
-	public static String FileEditDialog_file_properties;
-	public static String FileEditDialog_really_attribute_to_new_patient;
-	public static String FileEditDialog_17;
-	public static String FileEditDialog_18;
-	
-	static {
-		// initialize resource bundle
-		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
-	}
-	
-	private Messages(){}
+	public static String ExterneDokumente_pop_menu =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_pop_menu;
+	public static String ExterneDokumente_delete_files =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_delete_files;
+	public static String ExterneDokumente_click_to_sort_by_date =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_click_to_sort_by_date;
+	public static String ExterneDokumente_click_to_sort_by_name =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_click_to_sort_by_name;
+	public static String ExterneDokumente_Concerns =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_Concerns;
+	public static String ExterneDokumente_could_not_read_File =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_could_not_read_File;
+	public static String ExterneDokumente_delete =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_delete;
+	public static String ExterneDokumente_delete_doc =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_delete_doc;
+	public static String ExterneDokumente_email_app =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_email_app;
+	public static String ExterneDokumente_exception_while_copying =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_exception_while_copying;
+	public static String ExterneDokumente_externe_dokumente =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_externe_dokumente;
+	public static String ExterneDokumente_file_date =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_file_date;
+	public static String ExterneDokumente_file_name =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_file_name;
+	public static String ExterneDokumente_import_failed =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_import_failed;
+	public static String ExterneDokumente_imported =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_imported;
+	public static String ExterneDokumente_loading =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_loading;
+	public static String ExterneDokumente_modified_time =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_modified_time;
+	public static String ExterneDokumente_move_into_subdir =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_move_into_subdir;
+	public static String ExterneDokumente_move_into_subdir_tooltip =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_move_into_subdir_tooltip;
+	public static String ExterneDokumente_no_files_found =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_no_files_found;
+	public static String ExterneDokumente_no_patient_found =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_no_patient_found;
+	public static String ExterneDokumente_not_defined_in_preferences =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_not_defined_in_preferences;
+	public static String ExterneDokumente_open = ch.elexis.base.l10n.Messages.ExterneDokumente_open;
+	public static String ExterneDokumente_OpenFileTip =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_OpenFileTip;
+	public static String ExterneDokumente_openFolder =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_openFolder;
+	public static String ExterneDokumente_openFolderTip =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_openFolderTip;
+	public static String ExterneDokumente_path_name_preference =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_path_name_preference;
+	public static String ExterneDokumente_propeties =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_propeties;
+	public static String ExterneDokumente_read_errpor =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_read_errpor;
+	public static String ExterneDokumente_rename_or_change_date =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_rename_or_change_date;
+	public static String ExterneDokumente_renaming_file =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_renaming_file;
+	public static String ExterneDokumente_select_patient_first =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_select_patient_first;
+	public static String ExterneDokumente_sendEmail =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_sendEmail;
+	public static String ExterneDokumente_sendEmailTip =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_sendEmailTip;
+	public static String ExterneDokumente_shold_doc_be_delted =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_shold_doc_be_delted;
+	public static String ExterneDokumente_shorthand_for_path =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_shorthand_for_path;
+	public static String ExterneDokumente_verify_files =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_verify_files;
+	public static String ExterneDokumente_verify_files_Belong_to_patient =
+		ch.elexis.base.l10n.Messages.ExterneDokumente_verify_files_Belong_to_patient;
+	public static String MoveIntoSubDirsDialog_finished =
+		ch.elexis.base.l10n.Messages.MoveIntoSubDirsDialog_finished;
+	public static String MoveIntoSubDirsDialog_no_old_Files_found =
+		ch.elexis.base.l10n.Messages.MoveIntoSubDirsDialog_no_old_Files_found;
+	public static String MoveIntoSubDirsDialog_sub_task =
+		ch.elexis.base.l10n.Messages.MoveIntoSubDirsDialog_sub_task;
+	public static String VerifierDialog_name = ch.elexis.base.l10n.Messages.VerifierDialog_name;
+	public static String VerifierDialog_verify_job_name =
+		ch.elexis.base.l10n.Messages.VerifierDialog_verify_job_name;
+	public static String FileEditDialog_attribute_to_new_patient =
+		ch.elexis.base.l10n.Messages.FileEditDialog_attribute_to_new_patient;
+	public static String FileEditDialog_extension =
+		ch.elexis.base.l10n.Messages.FileEditDialog_extension;
+	public static String FileEditDialog_file_name =
+		ch.elexis.base.l10n.Messages.FileEditDialog_file_name;
+	public static String FileEditDialog_file_date_and_explanation =
+		ch.elexis.base.l10n.Messages.FileEditDialog_file_date_and_explanation;
+	public static String FileEditDialog_file_properties =
+		ch.elexis.base.l10n.Messages.FileEditDialog_file_properties;
+	public static String FileEditDialog_really_attribute_to_new_patient =
+		ch.elexis.base.l10n.Messages.FileEditDialog_really_attribute_to_new_patient;
+	public static String FileEditDialog_17 = ch.elexis.base.l10n.Messages.FileEditDialog_17;
+	public static String FileEditDialog_18 = ch.elexis.base.l10n.Messages.FileEditDialog_18;
 }

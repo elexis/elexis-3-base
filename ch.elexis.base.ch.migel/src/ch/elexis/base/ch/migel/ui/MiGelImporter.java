8a8927bed583276bc0c17ac9b1d69f3695b55658
thomashu <huster@medevit.at>
2018-01-25 18:35:27 +0100

    [10520] updated MiGelImporter, reading csv only

--- ch.elexis.base.ch.migel/src/ch/elexis/base/ch/migel/ui/MiGelImporter.java
+++ ch.elexis.base.ch.migel/src/ch/elexis/base/ch/migel/ui/MiGelImporter.java
@@ -12,14 +12,12 @@
  *******************************************************************************/
 package ch.elexis.base.ch.migel.ui;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.text.ParseException;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -36,12 +34,47 @@ import ch.elexis.core.ui.util.SWTHelper;
 import ch.elexis.data.PersistentObject;
 import ch.rgw.tools.ExHandler;
 import ch.rgw.tools.Money;
+import ch.rgw.tools.StringTool;
 
 public class MiGelImporter extends ImporterPage {
 	private static final String SRC_ENCODING = "iso-8859-1"; //$NON-NLS-1$
 	boolean bDelete = false;
 	Button bClear;
 	String mode;
+	
+	private enum ImportFields {
+			POSNUMER(0), NAME(1), UNIT(2), PRICE(3), CATEGORY(4), SUBCATEGORY(5), AMOUNT(6);
+		
+		private int index;
+		
+		ImportFields(int index){
+			this.index = index;
+		}
+		
+		private boolean exists(String[] line){
+			return line.length > index;
+		}
+		
+		public String getStringValue(String[] line){
+			if (exists(line)) {
+				return line[index];
+			} else {
+				return "";
+			}
+		}
+		
+		public Money getMoneyValue(String[] line){
+			if (exists(line)) {
+				try {
+					return new Money(getStringValue(line));
+				} catch (ParseException e) {
+					// ignore
+				}
+			}
+			return new Money();
+		}
+		
+	}
 	
 	public MiGelImporter(){}
 	
@@ -62,36 +95,12 @@ public class MiGelImporter extends ImporterPage {
 			PersistentObject.getConnection().exec("DELETE FROM ARTIKEL WHERE TYP='MiGeL'"); //$NON-NLS-1$
 			mode = Messages.MiGelImporter_ModeCreateNew;
 		}
-		final String line =
-			"([0-9][0-9]\\.[0-9][0-9]\\.[0-9][0-9]\\.[0-9][0-9]\\.[0-9]) +L? +(.+)  +(.+)  +(.+)  +.+"; //$NON-NLS-1$
 		try {
 			File file = new File(results[0]);
 			long l = file.length();
 			monitor.beginTask("MiGeL Import " + mode, (int) l / 100); //$NON-NLS-1$
 			if (file.getName().toLowerCase().endsWith("csv")) { //$NON-NLS-1$
 				return importCSV(file, monitor);
-			} else {
-				// long l=file.length();
-				InputStreamReader is =
-					new InputStreamReader(new FileInputStream(file), "iso-8859-1"); //$NON-NLS-1$
-				BufferedReader br = new BufferedReader(is);
-				
-				String in;
-				monitor.subTask("MiGel - Import"); //$NON-NLS-1$
-				Pattern pat = Pattern.compile(line);
-				// Query qbe=new Query(MiGelArtikel.class);
-				LineFeeder lf = new LineFeeder(br);
-				while ((in = lf.nextLine()) != null) {
-					Matcher match = pat.matcher(in);
-					if (match.matches()) {
-						String code = match.group(1);
-						String text = match.group(2);
-						String unit = match.group(3);
-						Money price = new Money(match.group(4));
-						/* MiGelArtikel migel= */new MiGelArtikel(code, text, unit, price);
-					}
-				}
-				return Status.OK_STATUS;
 			}
 		} catch (Exception ex) {
 			ExHandler.handle(ex);
@@ -116,70 +125,6 @@ public class MiGelImporter extends ImporterPage {
 		
 	}
 	
-	class LineFeeder {
-		static final String codeline =
-			"[0-9][0-9]\\.[0-9][0-9]\\.[0-9][0-9]\\.[0-9][0-9]\\.[0-9].+"; //$NON-NLS-1$
-		String prev;
-		BufferedReader br;
-		
-		LineFeeder(final BufferedReader b) throws Exception{
-			br = b;
-			prev = br.readLine();
-		}
-		
-		char peek(){
-			return prev.charAt(0);
-		}
-		
-		String nextl() throws Exception{
-			String r;
-			while ((r = br.readLine()) != null) {
-				if (r.matches(codeline)) {
-					break;
-				}
-			}
-			return r;
-		}
-		
-		String nextLine() throws Exception{
-			if (prev == null) {
-				return null;
-			}
-			if (!prev.matches(codeline)) {
-				prev = nextl();
-			}
-			String ret = prev;
-			prev = br.readLine();
-			if (prev == null) {
-				br.close();
-				return ret;
-			}
-			while (!prev.matches(codeline) && !prev.startsWith(" ")) { //$NON-NLS-1$
-				if (ret.matches(".*- +[CHIM]?$")) { //$NON-NLS-1$
-					ret = ret.replaceFirst("- +[CHIM]?$", prev.trim()); //$NON-NLS-1$
-				} else if (ret.matches(".* +[CHIM]$")) { //$NON-NLS-1$
-					ret = ret.replaceFirst("[CHIM]$", prev.trim()); //$NON-NLS-1$
-				} else {
-					ret += " " + prev.trim(); //$NON-NLS-1$
-				}
-				prev = br.readLine();
-				if (prev == null) {
-					br.close();
-					return ret;
-				}
-			}
-			return ret;
-		}
-		
-		boolean atEOF(){
-			return prev == null;
-		}
-		
-		public void close() throws Exception{
-			br.close();
-		}
-	}
-	
 	private IStatus importCSV(final File file, final IProgressMonitor monitor)
 		throws FileNotFoundException, IOException{
 		InputStreamReader isr = new InputStreamReader(new FileInputStream(file), SRC_ENCODING);
@@ -187,20 +132,49 @@ public class MiGelImporter extends ImporterPage {
 		String[] line;
 		monitor.subTask(Messages.MiGelImporter_ReadMigel);
 		while ((line = reader.readNext()) != null) {
-			if (line.length < 3) {
-				continue;
+			if (isFieldsLine(line) && line.length >= 3) {
+				StringBuilder sb = new StringBuilder();
+				String category = ImportFields.SUBCATEGORY.getStringValue(line);
+				if (category.isEmpty()) {
+					category = ImportFields.CATEGORY.getStringValue(line);
+				}
+				// category only 1 line and max 80 char
+				if(!category.isEmpty()) {
+					sb.append(StringTool.getFirstLine(category, 80)).append(" - ");
+				}
+				sb.append(ImportFields.NAME.getStringValue(line));
+				
+				MiGelArtikel artikel = new MiGelArtikel(ImportFields.POSNUMER.getStringValue(line),
+					sb.toString(), ImportFields.UNIT.getStringValue(line),
+					ImportFields.PRICE.getMoneyValue(line));
+				
+				String amount = ImportFields.AMOUNT.getStringValue(line);
+				if (!amount.isEmpty()) {
+					try {
+						double amountDbl = Double.parseDouble(amount);
+						artikel.setPackungsGroesse((int) amountDbl);
+					} catch (NumberFormatException e) {
+						// ignore
+					}
+				}
+				monitor.worked(1);
 			}
-			// line=StringTool.convertEncoding(line, SRC_ENCODING);
-			Money betrag;
-			try {
-				betrag = new Money(Double.parseDouble(line[3]));
-			} catch (Exception ex) {
-				betrag = new Money();
-			}
-			new MiGelArtikel(line[0], line[1], line[2], betrag);
-			monitor.worked(1);
 		}
 		monitor.done();
 		return Status.OK_STATUS;
+	}
+	
+	private boolean isFieldsLine(String[] line){
+		// line[0] contains the code, which always contains digits, so if not its is probably the description
+		return containsDigits(line[0]);
+	}
+	
+	private boolean containsDigits(String string){
+		for (char character : string.toCharArray()) {
+			if (Character.isDigit(character)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }

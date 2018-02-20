bb2b170bbbd310c0974f1f9a1d60fcc1ec2d3652
David Gutknecht <david.gutknecht@novcom.ch>
2018-02-21 00:16:52 +0100

    Import missing

--- ch.novcom.elexis.mednet.plugin/src/ch/novcom/elexis/mednet/plugin/MedNetSettings.java
+++ ch.novcom.elexis.mednet.plugin/src/ch/novcom/elexis/mednet/plugin/MedNetSettings.java
@@ -23,6 +23,10 @@ import org.slf4j.LoggerFactory;
 import ch.elexis.core.data.activator.CoreHub;
 import ch.rgw.io.Settings;
 
+import ch.novcom.elexis.mednet.plugin.MedNetConfigDocumentPath;
+import ch.novcom.elexis.mednet.plugin.MedNetConfigFormPath;
+import ch.novcom.elexis.mednet.plugin.MedNetConfigFormItem;
+
 /**
  * Object that manage and store the simple MedNet settings
  */

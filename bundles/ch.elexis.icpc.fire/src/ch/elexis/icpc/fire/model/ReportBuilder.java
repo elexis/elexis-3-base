9d814a484ed8d25ab1f6fdf1c9c23e6e2d120da2
Marco Descher <marco@descher.at>
2018-02-22 10:28:39 +0100

    [10716] FIRE Adaptations stickerToMC, insurer information

--- bundles/ch.elexis.icpc.fire/src/ch/elexis/icpc/fire/model/ReportBuilder.java
+++ bundles/ch.elexis.icpc.fire/src/ch/elexis/icpc/fire/model/ReportBuilder.java
@@ -2,6 +2,7 @@ package ch.elexis.icpc.fire.model;
 
 import java.math.BigInteger;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -9,10 +10,17 @@ import javax.xml.datatype.DatatypeConfigurationException;
 
 import org.slf4j.LoggerFactory;
 
+import ch.elexis.core.data.activator.CoreHub;
+import ch.elexis.core.model.ISticker;
 import ch.elexis.core.types.Gender;
+import ch.elexis.data.Fall;
 import ch.elexis.data.Konsultation;
+import ch.elexis.data.Kontakt;
 import ch.elexis.data.Mandant;
 import ch.elexis.data.Patient;
+import ch.elexis.data.Query;
+import ch.elexis.data.Sticker;
+import ch.elexis.icpc.fire.ui.Preferences;
 import ch.rgw.tools.TimeTool;
 
 public class ReportBuilder {
@@ -38,8 +46,8 @@ public class ReportBuilder {
 		report.setExportDate(XmlUtil.getXmlGregorianCalendar(new TimeTool()));
 	}
 	
-	public void addKonsultation(BigInteger patId, BigInteger docId,
-		Konsultation konsultation) throws DatatypeConfigurationException{
+	public void addKonsultation(BigInteger patId, BigInteger docId, Konsultation konsultation)
+		throws DatatypeConfigurationException{
 		if (fireConfig.isValid()) {
 			if (report.getConsultations() == null) {
 				report.setConsultations(fireConfig.getFactory().createReportConsultations());
@@ -71,7 +79,7 @@ public class ReportBuilder {
 			
 			TPatient tPatient = fireConfig.getFactory().createTPatient();
 			tPatient.setId(patId);
-
+			
 			Gender gender = patient.getGender();
 			if (gender == Gender.MALE) {
 				tPatient.setGender(true);
@@ -95,23 +103,44 @@ public class ReportBuilder {
 	}
 	
 	private TStatus createPatientTStatus(Patient patient){
-		//		TStatus tStatus = fireConfig.getFactory().createTStatus();
-		// TODO
-		//		Query<Fall> qbe = new Query<>(Fall.class, Fall.PATIENT_ID, patient.getId());
-		//		qbe.add(Fall.FLD_XGESETZ, Query.EQUALS, "KVG");
-		//		List<Fall> qre = qbe.execute();
-		//		for (Fall fall : qre) {
-		//			if(fall.isOpen()) {
-		//				Kontakt garant = fall.getGarant();
-		//				if (garant != null && !garant.getId().equals(patient.getId()) && garant.exists()) {
-		//					tStatus.setInsurer(garant.getLabel());
-		//					break;
-		//				}
-		//			}
-		//		}
+		String insurer = null;
+		Boolean mc = null;
+		Query<Fall> qbe = new Query<>(Fall.class, Fall.PATIENT_ID, patient.getId());
+		qbe.add(Fall.FLD_XGESETZ, Query.EQUALS, "KVG");
+		qbe.orderBy(true, Fall.FLD_DATUM_VON);
+		List<Fall> qre = qbe.execute();
+		for (Fall fall : qre) {
+			if (fall.isOpen()) {
+				String kostentraeger = (String) fall.getInfoElement("Kostenträger");
+				if(kostentraeger == null) {
+					kostentraeger = fall.get(Fall.FLD_KOSTENTRAEGER);
+				}
+				if (kostentraeger != null && !kostentraeger.equals(patient.getId())) {
+					Kontakt costBearer = Kontakt.load(kostentraeger);
+					if (costBearer.isValid() && costBearer.istOrganisation()) {
+						insurer = costBearer.getLabel();
+						break;
+					}
+				}
+			}
+		}
+		List<ISticker> stickers = patient.getStickers();
+		for (ISticker iSticker : stickers) {
+			String stickerId = ((Sticker) iSticker).getId();
+			if (CoreHub.globalCfg.get(Preferences.CFG_BASE_IS_HAM_STICKER + stickerId, false)) {
+				mc = true;
+				break;
+			}
+		}
+		if (insurer != null || mc != null) {
+			TStatus tStatus = fireConfig.getFactory().createTStatus();
+			tStatus.setInsurer(insurer);
+			tStatus.setMc(mc);
+			return tStatus;
+		}
 		return null;
 	}
-
+	
 	public BigInteger addMandant(Mandant mandant){
 		BigInteger docId = fireConfig.getDocId(mandant);
 		// only add a new doctor model object if no already in the list
@@ -146,8 +175,7 @@ public class ReportBuilder {
 	public Optional<Report> build(){
 		return Optional.ofNullable(report);
 	}
-
-
+	
 	/**
 	 * handle finishing tasks
 	 */

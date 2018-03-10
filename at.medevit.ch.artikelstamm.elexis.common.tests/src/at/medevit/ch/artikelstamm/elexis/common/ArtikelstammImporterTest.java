07619164bb7de1f6c62bd14d05acf622f22b9f9b
Marlovits Harald <marlovitsh@gmail.com>
2018-03-10 10:43:53 +0100

    Update ArtikelstammImporterTest.java

--- at.medevit.ch.artikelstamm.elexis.common.tests/src/at/medevit/ch/artikelstamm/elexis/common/ArtikelstammImporterTest.java
+++ at.medevit.ch.artikelstamm.elexis.common.tests/src/at/medevit/ch/artikelstamm/elexis/common/ArtikelstammImporterTest.java
@@ -7,7 +7,6 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;

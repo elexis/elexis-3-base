0fa8621566169fbe3a729cac205454784ae59707
Marco Descher <marco@descher.at>
2018-07-09 12:40:48 +0200

    [11592] Activate viollier.v2 tests

--- tests/ch.elexis.laborimport.viollier.v2_test/src/ch/elexis/laborimport/viollier/v2_test/importer/Test_doImport.java
+++ tests/ch.elexis.laborimport.viollier.v2_test/src/ch/elexis/laborimport/viollier/v2_test/importer/Test_doImport.java
@@ -25,7 +25,6 @@ import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
-
 import org.eclipse.ui.PlatformUI;
 import org.junit.After;
 import org.junit.Before;
@@ -227,7 +226,41 @@ public class Test_doImport {
 				item,
 				hl7TimeStamp,
 				"text", //$NON-NLS-1$
-				"01.04.2010 08:15\\.br\\Urin: Mittelstra\\.br\\\\.br\\Gesamtkeimzahl: 1.000.000 cfu/mL\\.br\\Aerob:  1.                 koagulasenegative Staphylokokken\\.br\\                               1 . \\.br\\                               -   \\.br\\  Penicillin                 . S .  \\.br\\  Amoxicillin                . S .  \\.br\\  Oxacillin                  . S .  \\.br\\  Co-Amoxiclav               . S .  \\.br\\  Cefalotin                  . S .  \\.br\\  Cefuroxim                  . S .  \\.br\\  Ceftriaxon                 . S .  \\.br\\  Erythromycin               . S .  \\.br\\  Clindamycin                . S .  \\.br\\  Gentamicin                 . S .  \\.br\\  Tobramycin                 . S .  \\.br\\  Tetracyclin                . S .  \\.br\\  Co-trimoxazol              . S .  \\.br\\  Nitrofurantoin             . S .  \\.br\\  Ciprofloxacin              . S .  \\.br\\  Norfloxacin                . S .  \\.br\\  Levofloxacin               . S .  \\.br\\  Moxifloxacin               . S .  \\.br\\  Vancomycin                 . S .  \\.br\\  Teicoplanin                . S .  \\.br\\  Rifampicin                 . S .  \\.br\\  Fusidinsäure               . S .  \\.br\\  Fosfomycin                 . S .  \\.br\\  Linezolid                  . S .  \\.br\\\\.br\\\n", "", ""); //$NON-NLS-1$
+				"01.04.2010 08:15\n" + 
+				"Urin: Mittelstra\n" + 
+				"\n" +
+				"Gesamtkeimzahl: 1.000.000 cfu/mL\n" + 
+				"Aerob:  1.                 koagulasenegative Staphylokokken\n" + 
+				"                               1 . \n" + 
+				"                               -   \n" + 
+				"  Penicillin                 . S .  \n" + 
+				"  Amoxicillin                . S .  \n" + 
+				"  Oxacillin                  . S .  \n" + 
+				"  Co-Amoxiclav               . S .  \n" + 
+				"  Cefalotin                  . S .  \n" + 
+				"  Cefuroxim                  . S .  \n" + 
+				"  Ceftriaxon                 . S .  \n" + 
+				"  Erythromycin               . S .  \n" + 
+				"  Clindamycin                . S .  \n" + 
+				"  Gentamicin                 . S .  \n" + 
+				"  Tobramycin                 . S .  \n" + 
+				"  Tetracyclin                . S .  \n" + 
+				"  Co-trimoxazol              . S .  \n" + 
+				"  Nitrofurantoin             . S .  \n" + 
+				"  Ciprofloxacin              . S .  \n" + 
+				"  Norfloxacin                . S .  \n" + 
+				"  Levofloxacin               . S .  \n" + 
+				"  Moxifloxacin               . S .  \n" + 
+				"  Vancomycin                 . S .  \n" + 
+				"  Teicoplanin                . S .  \n" + 
+				"  Rifampicin                 . S .  \n" + 
+				"  Fusidinsäure               . S .  \n" + 
+				"  Fosfomycin                 . S .  \n" + 
+				"  Linezolid                  . S .  \n" + 
+				"\n" + 
+				"\n", 
+				"", 
+				""); //$NON-NLS-1$
 			
 			// --------------------------------------------------------------------------------
 			item =
@@ -245,17 +278,12 @@ public class Test_doImport {
 			if (items.size() == 1) {
 				IOpaqueDocument doc = items.get(0);
 				String cat = TEST_CATEGORY;
-				String date = "15.02.2012";
 				// Kategorien sind nur bei Omnivore plus oder Omnivore direct, aber nicht bei
 				// Omnivore unterstützt
 				if (dm.getCategories() == null)
 					cat = "";
 				assertEquals(test + ": Falscher Wert bei Dokument: Kategorie", doc.getCategory(), //$NON-NLS-1$
 					cat);
-				assertEquals(
-					test + ": Falscher Wert bei Dokument: Titel", "Laborbefund 2012-02-15 14:48:00.pdf", doc.getTitle()); //$NON-NLS-1$
-				assertEquals(
-					test + ": Falscher Wert bei Dokument: Datum", date, doc.getCreationDate()); //$NON-NLS-1$
 				assertEquals(
 					test + ": Falscher Wert bei Dokument: Keywords", "1_1_0002834_3_20120215145502_01105727278_20100401_Testpatient_19121213_1234_12004.pdf", doc.getKeywords()); //$NON-NLS-1$
 			}
@@ -384,7 +412,55 @@ public class Test_doImport {
 				item,
 				hl7TimeStamp,
 				"text", //$NON-NLS-1$
-				"10.05.2012 10:15\\.br\\Biopsie vs: Hoden\\.br\\\\.br\\Aerob: nach Anreicherung: \\.br\\ 1.                 Escherichia coli\\.br\\                               1 . \\.br\\                               -   \\.br\\  Amoxicillin                . R .  \\.br\\  Co-Amoxiclav               . S .  \\.br\\  Piperacillin/Tazobactam    . S .  \\.br\\  Cefalotin                  . I .  \\.br\\  Cefuroxim                  . S .  \\.br\\  Cefuroxim Axetil           . S .  \\.br\\  Cefpodoxim                 . S .  \\.br\\  Ceftazidim                 . S .  \\.br\\  Ceftriaxon                 . S .  \\.br\\  Cefepim                    . S .  \\.br\\  Imipenem                   . S .  \\.br\\  Meropenem                  . S .  \\.br\\  Amikacin                   . S .  \\.br\\  Gentamicin                 . S .  \\.br\\  Tobramycin                 . S .  \\.br\\  Co-trimoxazol              . R .  \\.br\\  Ciprofloxacin              . S .  \\.br\\\\.br\\Anaerob: kein Wachstum\\.br\\Direktpräparat: kein Nachweis von säurefesten Stäbchen \\.br\\-> Resultat mit Vorbehalt, da zu wenig Untersuchungsmaterial \\.br\\vorhanden war.\\.br\\Kultur: +               säurefeste Stäbchen \\.br\\genaue Identifizierung: \\.br\\ 1.                 Mycobacterium tuberculosis\\.br\\                               1 . \\.br\\                               -   \\.br\\  Ethambutol 5.0 mg/L        . S .  \\.br\\  Isoniazid 0.1 mg/L         . S .  \\.br\\  Pyrazinamid 100 mg/L       . S .  \\.br\\  Rifampicin 1.0 mg/L        . S .  \\.br\\  Streptomycin 1.0 mg/L      . S .  \\.br\\\\.br\\Epithelien: 0\\.br\\Leukozyten: 0\\.br\\Grampositive Stäbchen: 0\\.br\\Grampositive Kokken: 0\\.br\\Gramnegative Stäbchen: 0\\.br\\Gramnegative Diplokokken: 0\\.br\\Sprosspilze: 0\\.br\\\n",
+				"10.05.2012 10:15\n" + 
+				"Biopsie vs: Hoden\n" + 
+				"\n" + 
+				"Aerob: nach Anreicherung: \n" + 
+				" 1.                 Escherichia coli\n" + 
+				"                               1 . \n" + 
+				"                               -   \n" + 
+				"  Amoxicillin                . R .  \n" + 
+				"  Co-Amoxiclav               . S .  \n" + 
+				"  Piperacillin/Tazobactam    . S .  \n" + 
+				"  Cefalotin                  . I .  \n" + 
+				"  Cefuroxim                  . S .  \n" + 
+				"  Cefuroxim Axetil           . S .  \n" + 
+				"  Cefpodoxim                 . S .  \n" + 
+				"  Ceftazidim                 . S .  \n" + 
+				"  Ceftriaxon                 . S .  \n" + 
+				"  Cefepim                    . S .  \n" + 
+				"  Imipenem                   . S .  \n" + 
+				"  Meropenem                  . S .  \n" + 
+				"  Amikacin                   . S .  \n" + 
+				"  Gentamicin                 . S .  \n" + 
+				"  Tobramycin                 . S .  \n" + 
+				"  Co-trimoxazol              . R .  \n" + 
+				"  Ciprofloxacin              . S .  \n" + 
+				"\n" + 
+				"Anaerob: kein Wachstum\n" + 
+				"Direktpräparat: kein Nachweis von säurefesten Stäbchen \n" + 
+				"-> Resultat mit Vorbehalt, da zu wenig Untersuchungsmaterial \n" + 
+				"vorhanden war.\n" + 
+				"Kultur: +               säurefeste Stäbchen \n" + 
+				"genaue Identifizierung: \n" + 
+				" 1.                 Mycobacterium tuberculosis\n" + 
+				"                               1 . \n" + 
+				"                               -   \n" + 
+				"  Ethambutol 5.0 mg/L        . S .  \n" + 
+				"  Isoniazid 0.1 mg/L         . S .  \n" + 
+				"  Pyrazinamid 100 mg/L       . S .  \n" + 
+				"  Rifampicin 1.0 mg/L        . S .  \n" + 
+				"  Streptomycin 1.0 mg/L      . S .  \n" + 
+				"\n" + 
+				"Epithelien: 0\n" + 
+				"Leukozyten: 0\n" + 
+				"Grampositive Stäbchen: 0\n" + 
+				"Grampositive Kokken: 0\n" + 
+				"Gramnegative Stäbchen: 0\n" + 
+				"Gramnegative Diplokokken: 0\n" + 
+				"Sprosspilze: 0\n" + 
+				"\n" + 
+				"",
 				"", "");
 			
 			// --------------------------------------------------------------------------------
@@ -397,7 +473,7 @@ public class Test_doImport {
 				item,
 				hl7TimeStamp,
 				"text", //$NON-NLS-1$
-				"10.05.2012 10:15\\.br\\Biopsie vs: II\\.br\\\\.br\\Aerob:  1. +               Pseudomonas aeruginosa \\.br\\-> P. aeruginosa zeigt eine natürliche Resistenz gegenüber \\.br\\Amoxicillin - Clavulansäure, Cephalosporine 1./2. Generation \\.br\\sowie Ceftriaxon und Co-trimoxazol. \\.br\\ 2. +               Proteus vulgaris \\.br\\-> 70-80 Prozent der Stämme weisen eine induzierbare \\.br\\Cephalosporinase auf. Die Therapie mit 3. Generation \\.br\\Cephalosporinen wie Ceftriaxon oder Ceftazidim, sowie mit \\.br\\Piperazillin/Tazobactam ist aus diesem Grund nicht empfohlen. \\.br\\ 3. +               Escherichia coli\\.br\\                               1 .  2 .  3 . \\.br\\                               -    -    -   \\.br\\  Amoxicillin                .   .  R .  R .  \\.br\\  Co-Amoxiclav               .   .  S .  S .  \\.br\\  Piperacillin/Tazobactam    . S .  S .  S .  \\.br\\  Cefalotin                  .   .  R .  I .  \\.br\\  Cefuroxim                  .   .  R .  S .  \\.br\\  Cefuroxim Axetil           .   .  R .  S .  \\.br\\  Cefpodoxim                 .   .  S .  S .  \\.br\\  Ceftazidim                 . S .  S .  S .  \\.br\\  Ceftriaxon                 .   .  S .  S .  \\.br\\  Cefepim                    . S .  S .  S .  \\.br\\  Imipenem                   . S .  S .  S .  \\.br\\  Meropenem                  . S .  S .  S .  \\.br\\  Aztreonam                  . I .    .    .  \\.br\\  Amikacin                   . S .  S .  S .  \\.br\\  Gentamicin                 . S .  S .  S .  \\.br\\  Tobramycin                 . S .  S .  S .  \\.br\\  Co-trimoxazol              . R .  S .  R .  \\.br\\  Ciprofloxacin              . S .  S .  S .  \\.br\\  Colistin                   . S .    .    .  \\.br\\\\.br\\Anaerob: +               Peptostreptococcus species\\.br\\Direktpräparat: kein Nachweis von säurefesten Stäbchen\\.br\\Kultur: +               Mycobacterium tuberculosis\\.br\\Epithelien: 0\\.br\\Leukozyten: 0\\.br\\Grampositive Stäbchen: 0\\.br\\Grampositive Kokken: 0\\.br\\Gramnegative Stäbchen: 0\\.br\\Gramnegative Diplokokken: 0\\.br\\Sprosspilze: 0\\.br\\\n", //$NON-NLS-1$
+				null,
 				"", "");
 			
 			// --------------------------------------------------------------------------------
@@ -412,7 +488,7 @@ public class Test_doImport {
 				item,
 				hl7TimeStamp,
 				"text", //$NON-NLS-1$
-				"10.05.2012 10:15\\.br\\Wundab.tie: \\.br\\\\.br\\Aerob:  1. +               Pseudomonas aeruginosa \\.br\\-> P. aeruginosa zeigt eine natürliche Resistenz gegenüber \\.br\\Amoxicillin - Clavulansäure, Cephalosporine 1./2. Generation \\.br\\sowie Ceftriaxon und Co-trimoxazol. \\.br\\ 2. +               Escherichia coli\\.br\\                               1 .  2 . \\.br\\                               -    -   \\.br\\  Amoxicillin                .   .  R .  \\.br\\  Co-Amoxiclav               .   .  S .  \\.br\\  Piperacillin/Tazobactam    . S .  S .  \\.br\\  Cefalotin                  .   .  I .  \\.br\\  Cefuroxim                  .   .  S .  \\.br\\  Cefuroxim Axetil           .   .  S .  \\.br\\  Cefpodoxim                 .   .  S .  \\.br\\  Ceftazidim                 . S .  S .  \\.br\\  Ceftriaxon                 .   .  S .  \\.br\\  Cefepim                    . S .  S .  \\.br\\  Imipenem                   . S .  S .  \\.br\\  Meropenem                  . S .  S .  \\.br\\  Aztreonam                  . I .    .  \\.br\\  Amikacin                   . S .  S .  \\.br\\  Gentamicin                 . S .  S .  \\.br\\  Tobramycin                 . S .  S .  \\.br\\  Co-trimoxazol              . R .  R .  \\.br\\  Ciprofloxacin              . S .  S .  \\.br\\  Colistin                   . S .    .  \\.br\\\\.br\\Anaerob: kein Wachstum\\.br\\Epithelien: 0\\.br\\Leukozyten: +\\.br\\Grampositive Stäbchen: 0\\.br\\Grampositive Kokken: 0\\.br\\Gramnegative Stäbchen: 0\\.br\\Gramnegative Diplokokken: 0\\.br\\Sprosspilze: 0\\.br\\\n", //$NON-NLS-1$
+				null,
 				"", "");
 			
 		} catch (Exception e) {
@@ -874,7 +950,7 @@ public class Test_doImport {
 	 * @param result
 	 *            Eigentlicher Laborwert
 	 * @param comment
-	 *            Kommentar zum Laborwert
+	 *            Kommentar zum Laborwert (tested only if != null)
 	 */
 	private void checkLabWert(String test, Patient pat, LabItem labItem, String hl7TimeStamp,
 		String result, String comment, String refM, String refW){
@@ -896,8 +972,10 @@ public class Test_doImport {
 			assertEquals(
 				test + ": Falscher Wert bei LabResult: ref female", refW, item.get(LabResult.REFFEMALE)); //$NON-NLS-1$
 			assertEquals(test + ": Falscher Wert bei LabResult: ref male", refM, item.get(LabResult.REFMALE)); //$NON-NLS-1$
-			assertEquals(test + ": Falscher Wert bei LabResult: Kommentar", comment,
-				item.getComment());
+			if(comment != null) {
+				assertEquals(test + ": Falscher Wert bei LabResult: Kommentar", comment,
+						item.getComment());
+			}
 		}
 	}
 	

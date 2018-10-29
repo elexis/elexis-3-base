f7974f4b2ada64c10526bb7dcb5d7fed626e6d85
thomashu <huster@medevit.at>
2018-10-29 12:09:13 +0100

    [14615] added labororder.lg1 plugin and feature

---
+++ bundles/ch.elexis.labororder.lg1/src/ch/elexis/labororder/lg1/messages/Messages.java
@@ -0,0 +1,32 @@
+/**********************************************************************
+ * Copyright (c) 2005 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM - Initial API and implementation
+ **********************************************************************/
+package ch.elexis.labororder.lg1.messages;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "ch.elexis.labororder.lg1.messages.messages";
+    public static String LabOrderAction_errorMessageNoFallSelected;
+    public static String LabOrderAction_errorMessageNoPatientSelected;
+    public static String LabOrderAction_errorTitleCannotCreateHL7;
+    public static String LabOrderAction_errorTitleCannotShowURL;
+    public static String LabOrderAction_errorTitleNoFallSelected;
+    public static String LabOrderAction_errorTitleNoPatientSelected;
+    public static String LabOrderAction_infoMessageLabOrderFinshed;
+    public static String LabOrderAction_infoTitleLabOrderFinshed;
+    public static String LabOrderAction_nameAction;
+    public static String LabOrderAction_receivingApplication;
+    public static String LabOrderAction_receivingFacility;
+	public static String Lg1PreferencePage_labelUploadDir;
+
+    static { // load message values from bundle file
+    NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+  }
+}

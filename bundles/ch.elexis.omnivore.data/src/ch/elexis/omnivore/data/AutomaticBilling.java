2e602cd81d6fbbd702953474ed59e2d4cb7a0136
thomashu <huster@medevit.at>
2018-11-28 09:50:05 +0100

    [14338] reworked automatic billing

--- bundles/ch.elexis.omnivore.data/src/ch/elexis/omnivore/data/AutomaticBilling.java
+++ bundles/ch.elexis.omnivore.data/src/ch/elexis/omnivore/data/AutomaticBilling.java
@@ -1,7 +1,7 @@
 package ch.elexis.omnivore.data;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -20,7 +20,6 @@ import ch.elexis.data.Konsultation;
 import ch.elexis.data.Leistungsblock;
 import ch.elexis.data.Patient;
 import ch.elexis.data.PersistentObject;
-import ch.elexis.data.Query;
 import ch.elexis.omnivore.PreferenceConstants;
 import ch.rgw.tools.Result;
 import ch.rgw.tools.TimeTool;
@@ -51,14 +50,16 @@ public class AutomaticBilling {
 				public void run(){
 					try {
 						Konsultation encounter = getEncounter();
-						Leistungsblock block = Leistungsblock
-							.load(CoreHub.localCfg.get(PreferenceConstants.AUTO_BILLING_BLOCK, ""));
-						if (encounter != null && encounter.isEditable(false)) {
-							addBlockToEncounter(block, encounter);
-						} else {
-							LoggerFactory.getLogger(getClass()).warn(String.format(
-								"Could not add block [%s] for document of patient [%s] because no valid kons found.",
-								block.getName(), patient.getLabel()));
+						if (encounter != null) {
+							Leistungsblock block = Leistungsblock.load(
+								CoreHub.localCfg.get(PreferenceConstants.AUTO_BILLING_BLOCK, ""));
+							if (encounter != null && encounter.isEditable(false)) {
+								addBlockToEncounter(block, encounter);
+							} else {
+								LoggerFactory.getLogger(getClass()).warn(String.format(
+									"Could not add block [%s] for document of patient [%s] because no valid kons found.",
+									block.getName(), patient.getLabel()));
+							}
 						}
 					} catch (Exception e) {
 						ElexisEventDispatcher.getInstance().fireMessageEvent(new MessageEvent(
@@ -72,7 +73,7 @@ public class AutomaticBilling {
 	}
 	
 	private void addBlockToEncounter(Leistungsblock block, Konsultation encounter){
-		List<ICodeElement> elements = block.getElements();
+		List<ICodeElement> elements = block.getElements(encounter);
 		for (ICodeElement element : elements) {
 			if (element instanceof PersistentObject) {
 				Result<IVerrechenbar> result = encounter.addLeistung((IVerrechenbar) element);
@@ -89,21 +90,27 @@ public class AutomaticBilling {
 	}
 	
 	private Konsultation getEncounter(){
-		Konsultation encounter = getLatestEncounter();
-		if (encounter == null || !encounter.isEditable(false) || !isOnlyOneKonsToday()) {
+		Konsultation encounter = patient.getLastKonsultation();
+		if (encounter == null || !encounter.isEditable(false)) {
 			encounter = createEncounter();
 		}
 		return encounter;
 	}
 	
 	private Konsultation createEncounter(){
+		Konsultation lastEncounter = patient.getLastKonsultation();
 		Fall fall = null;
-		List<Fall> openFall = getOpenFall();
-		if (openFall.isEmpty()) {
-			fall = patient.neuerFall(Fall.getDefaultCaseLabel(), Fall.getDefaultCaseReason(),
-				Fall.getDefaultCaseLaw());
-		} else {
-			fall = openFall.get(0);
+		if(lastEncounter != null) {
+			fall = lastEncounter.getFall();
+		}
+		if (fall == null || !fall.isOpen()) {
+			List<Fall> openFall = getOpenFall();
+			if (openFall.isEmpty()) {
+				fall = patient.neuerFall(Fall.getDefaultCaseLabel(), Fall.getDefaultCaseReason(),
+					Fall.getDefaultCaseLaw());
+			} else {
+				fall = openFall.get(0);
+			}
 		}
 		if (fall != null) {
 			return fall.neueKonsultation();
@@ -119,68 +126,19 @@ public class AutomaticBilling {
 				ret.add(f);
 			}
 		}
-		return ret;
-	}
-	
-	private Konsultation getLatestEncounter(){
-		List<Konsultation> list = getOpenKons();
-		
-		if ((list == null) || list.isEmpty()) {
-			return null;
-		} else {
-			return list.get(0);
-		}
-	}
-	
-	private List<Konsultation> getOpenKons(){
-		Query<Konsultation> qbe = new Query<Konsultation>(Konsultation.class);
-		Fall[] faelle = patient.getFaelle();
-		if ((faelle == null) || (faelle.length == 0)) {
-			return null;
-		}
-		qbe.startGroup();
-		
-		boolean termInserted = false;
-		for (Fall fall : faelle) {
-			if (fall.isOpen()) {
-				qbe.add(Konsultation.FLD_CASE_ID, Query.EQUALS, fall.getId());
-				qbe.or();
-				termInserted = true;
+		ret.sort(new Comparator<Fall>() {
+			TimeTool o1Begin = new TimeTool();
+			TimeTool o2Begin = new TimeTool();
+			@Override
+			public int compare(Fall o1, Fall o2){
+				if (o1.getBeginnDatum() != null && o2.getBeginnDatum() != null) {
+					o1Begin.set(o1.getBeginnDatum());
+					o2Begin.set(o2.getBeginnDatum());
+					return o1Begin.compareTo(o2Begin);
+				}
+				return Long.compare(o1.getLastUpdate(), o2.getLastUpdate());
 			}
-		}
-		if (!termInserted) {
-			return null;
-		}
-		qbe.endGroup();
-		qbe.orderBy(true, Konsultation.DATE);
-		return qbe.execute();
-	}
-	
-	private boolean isOnlyOneKonsToday(){
-		// relevant is not the kons but the fall
-		// 2 kons of the same fall are ok
-		HashSet<String> set = new HashSet<String>();
-		List<Konsultation> list = getTodaysOpenKons();
-		for (Konsultation konsultation : list) {
-			Fall fall = konsultation.getFall();
-			set.add(fall.getId());
-		}
-		if (set.size() == 1) {
-			return true;
-		}
-		return false;
-	}
-	
-	private List<Konsultation> getTodaysOpenKons(){
-		List<Konsultation> ret = new ArrayList<Konsultation>();
-		List<Konsultation> list = getOpenKons();
-		
-		for (Konsultation konsultation : list) {
-			TimeTool konsDate = new TimeTool(konsultation.getDatum());
-			if (konsDate.isSameDay(new TimeTool())) {
-				ret.add(konsultation);
-			}
-		}
+		});
 		return ret;
 	}
 }

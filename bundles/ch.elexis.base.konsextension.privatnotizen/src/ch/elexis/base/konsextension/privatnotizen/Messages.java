e5d70ab15dab5818001bf06eda8ded541b5925c5
thomashu <huster@medevit.at>
2018-11-30 14:43:58 +0100

    [14725] fixed messages of privatnotizen

--- bundles/ch.elexis.base.konsextension.privatnotizen/src/ch/elexis/base/konsextension/privatnotizen/Messages.java
+++ bundles/ch.elexis.base.konsextension.privatnotizen/src/ch/elexis/base/konsextension/privatnotizen/Messages.java
@@ -3,11 +3,18 @@ package ch.elexis.base.konsextension.privatnotizen;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	public static String  KonsExtension_noteActionLabel;
-	public static String  KonsExtension_noteActionXREFText;
-	public static String  NotizInputDialog_noteDlgMessage;
-	public static String  NotizInputDialog_noteDlgText;
-	public static String  NotizInputDialog_noteDlgTitle;
-	public static String  NotizInputDialog_notYourNoteTitle;
-	public static String  NotizInputDialog_notYourNoteMessage;
+	private static final String BUNDLE_NAME = "ch.elexis.base.konsextension.privatnotizen.messages"; //$NON-NLS-1$
+	public static String KonsExtension_noteActionLabel;
+	public static String KonsExtension_noteActionXREFText;
+	public static String NotizInputDialog_noteDlgMessage;
+	public static String NotizInputDialog_noteDlgText;
+	public static String NotizInputDialog_noteDlgTitle;
+	public static String NotizInputDialog_notYourNoteTitle;
+	public static String NotizInputDialog_notYourNoteMessage;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+	
+	private Messages(){}
 }

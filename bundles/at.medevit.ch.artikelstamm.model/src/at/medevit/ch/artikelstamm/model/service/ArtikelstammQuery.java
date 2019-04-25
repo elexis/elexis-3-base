ecbe12617d038a70c3f0cd13d656a72b6ccd7d7f
thomashu <huster@medevit.at>
2019-04-25 20:17:41 +0200

    [14587] updated IQuery implementations due to core changes

--- bundles/at.medevit.ch.artikelstamm.model/src/at/medevit/ch/artikelstamm/model/service/ArtikelstammQuery.java
+++ bundles/at.medevit.ch.artikelstamm.model/src/at/medevit/ch/artikelstamm/model/service/ArtikelstammQuery.java
@@ -2,9 +2,7 @@ package at.medevit.ch.artikelstamm.model.service;
 
 import javax.persistence.EntityManager;
 
-import ch.elexis.core.jpa.entities.EntityWithDeleted;
 import ch.elexis.core.jpa.model.adapter.AbstractModelQuery;
-import ch.elexis.core.model.ModelPackage;
 import ch.elexis.core.services.IQuery;
 
 public class ArtikelstammQuery<T> extends AbstractModelQuery<T> implements IQuery<T> {
@@ -22,9 +20,5 @@ public class ArtikelstammQuery<T> extends AbstractModelQuery<T> implements IQuer
 		
 		criteriaQuery = criteriaBuilder.createQuery(entityClazz);
 		rootQuery = criteriaQuery.from(entityClazz);
-		
-		if (EntityWithDeleted.class.isAssignableFrom(entityClazz) && !includeDeleted) {
-			and(ModelPackage.Literals.DELETEABLE__DELETED, COMPARATOR.NOT_EQUALS, true);
-		}
 	}
 }

03c7e1edf4314f6d5d67e3870eec7060cc31d833
thomashu <huster@medevit.at>
2019-06-06 13:43:55 +0200

    [15244] [15230] added tests for previous changes

--- bundles/at.medevit.elexis.emediplan.core/src/at/medevit/elexis/emediplan/core/internal/EMediplanServiceImpl.java
+++ bundles/at.medevit.elexis.emediplan.core/src/at/medevit/elexis/emediplan/core/internal/EMediplanServiceImpl.java
@@ -270,12 +270,9 @@ public class EMediplanServiceImpl implements EMediplanService {
 		String json = getDecodedJsonString(chunk);
 		if (chunk.length() > 8) {
 			logger.debug("json version: " + chunk.substring(5, 8));
-			GsonBuilder gb = new GsonBuilder();
-			gb.registerTypeAdapter(Medication.class, new MedicationDeserializer());
-			Gson g = gb.create();
-			Medication m = g.fromJson(json, Medication.class);
-			m.chunk = chunk;
-			return m;
+			Medication ret = createModelFromJsonString(json);
+			ret.chunk = chunk;
+			return ret;
 		}
 		else {
 			logger.error("invalid json length - cannot parseable");
@@ -284,6 +281,14 @@ public class EMediplanServiceImpl implements EMediplanService {
 		return null;
 	}
 	
+	protected Medication createModelFromJsonString(String jsonString) {
+		GsonBuilder gb = new GsonBuilder();
+		gb.registerTypeAdapter(Medication.class, new MedicationDeserializer());
+		Gson g = gb.create();
+		Medication m = g.fromJson(jsonString, Medication.class);
+		return m;
+	}
+
 	public void addExistingArticlesToMedication(Medication medication){
 		if (medication != null) {
 			findPatientForMedication(medication);
--- tests/at.medevit.elexis.emediplan.core.test/src/at/medevit/elexis/emediplan/core/test/AllTests.java
+++ tests/at.medevit.elexis.emediplan.core.test/src/at/medevit/elexis/emediplan/core/test/AllTests.java
@@ -1,5 +1,11 @@
 package at.medevit.elexis.emediplan.core.test;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -7,8 +13,18 @@ import at.medevit.elexis.emediplan.core.internal.EMediplanServiceImplTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	EMediplanServiceImplTest.class
-})
+		EMediplanServiceImplTest.class })
 public class AllTests {
 	
+	public static File getAsFile(String string) throws IOException{
+		File ret = Files.createTempFile("test_", ".pdf").toFile();
+		try (FileOutputStream output = new FileOutputStream(ret)) {
+			IOUtils.copy(AllTests.class.getResourceAsStream(string), output);
+		}
+		return ret;
+	}
+	
+	public static String getAsString(String string) throws IOException{
+		return IOUtils.toString(AllTests.class.getResourceAsStream(string), "UTF-8");
+	}
 }

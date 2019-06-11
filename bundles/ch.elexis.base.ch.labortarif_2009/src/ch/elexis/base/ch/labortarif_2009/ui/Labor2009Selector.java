772fc0b07748761831028fd95d8f23175432846f
Marco Descher <marco@descher.at>
2019-06-11 11:05:40 +0200

    [15255] EAL2009 add to favorites action

--- bundles/ch.elexis.base.ch.labortarif_2009/src/ch/elexis/base/ch/labortarif_2009/ui/Labor2009Selector.java
+++ bundles/ch.elexis.base.ch.labortarif_2009/src/ch/elexis/base/ch/labortarif_2009/ui/Labor2009Selector.java
@@ -12,10 +12,17 @@
 
 package ch.elexis.base.ch.labortarif_2009.ui;
 
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 
 import ch.elexis.core.data.events.ElexisEvent;
 import ch.elexis.core.data.events.ElexisEventDispatcher;
+import ch.elexis.core.ui.actions.AddVerrechenbarToLeistungsblockAction;
+import ch.elexis.core.ui.actions.ToggleVerrechenbarFavoriteAction;
 import ch.elexis.core.ui.events.ElexisUiEventListenerImpl;
 import ch.elexis.core.ui.util.viewers.CommonViewer;
 import ch.elexis.core.ui.util.viewers.DefaultLabelProvider;
@@ -29,14 +36,34 @@ import ch.rgw.tools.TimeTool;
 public class Labor2009Selector extends CodeSelectorFactory {
 	CommonViewer cv;
 	
+	private ToggleVerrechenbarFavoriteAction tvfa = new ToggleVerrechenbarFavoriteAction();
+	private AddVerrechenbarToLeistungsblockAction atla =
+		new AddVerrechenbarToLeistungsblockAction(Labor2009Tarif.class);
+	
+	private ISelectionChangedListener selChange = new ISelectionChangedListener() {
+		@Override
+		public void selectionChanged(SelectionChangedEvent event){
+			TableViewer tv = (TableViewer) event.getSource();
+			StructuredSelection ss = (StructuredSelection) tv.getSelection();
+			tvfa.updateSelection(ss.isEmpty() ? null : ss.getFirstElement());
+		}
+	};
+	
 	@Override
 	public ViewerConfigurer createViewerConfigurer(CommonViewer cv){
 		this.cv = cv;
+		
+		cv.setSelectionChangedListener(selChange);
+		
+		MenuManager menu = new MenuManager();
+		menu.add(atla);
+		menu.add(tvfa);
+		cv.setContextMenu(menu);
+		
 		ViewerConfigurer vc =
 			new ViewerConfigurer(new Labor2009ContentProvider(), new DefaultLabelProvider(),
-				new Labor2009ControlFieldProvider(cv),
-				new ViewerConfigurer.DefaultButtonProvider(), new SimpleWidgetProvider(
-					SimpleWidgetProvider.TYPE_LAZYLIST, SWT.NONE, null));
+				new Labor2009ControlFieldProvider(cv), new ViewerConfigurer.DefaultButtonProvider(),
+				new SimpleWidgetProvider(SimpleWidgetProvider.TYPE_LAZYLIST, SWT.NONE, null));
 		
 		ElexisEventDispatcher.getInstance().addListeners(
 			new UpdateEventListener(cv, Labor2009Tarif.class, ElexisEvent.EVENT_RELOAD));

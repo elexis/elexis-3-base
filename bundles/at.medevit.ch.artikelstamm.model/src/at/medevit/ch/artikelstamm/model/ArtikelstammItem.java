52349ac58395a8419ae245c2f1cf84a18b3353c5
sazgin <sa@slogic.at>
2019-06-19 14:38:53 +0200

    [15156] ArtikelstammItem return value notnull

--- bundles/at.medevit.ch.artikelstamm.model/src/at/medevit/ch/artikelstamm/model/ArtikelstammItem.java
+++ bundles/at.medevit.ch.artikelstamm.model/src/at/medevit/ch/artikelstamm/model/ArtikelstammItem.java
@@ -12,6 +12,7 @@ import at.medevit.ch.artikelstamm.model.common.preference.MargePreference;
 import at.medevit.ch.artikelstamm.model.service.ArtikelstammModelServiceHolder;
 import at.medevit.ch.artikelstamm.model.service.CoreModelServiceHolder;
 import ch.elexis.core.constants.StringConstants;
+import ch.elexis.core.jdt.NonNull;
 import ch.elexis.core.jpa.model.adapter.AbstractIdDeleteModelAdapter;
 import ch.elexis.core.jpa.model.adapter.mixin.ExtInfoHandler;
 import ch.elexis.core.model.IArticle;
@@ -133,7 +134,7 @@ public class ArtikelstammItem
 	}
 	
 	@Override
-	public String getAtcCode(){
+	public @NonNull String getAtcCode(){
 		return StringUtils.defaultString(getEntity().getAtc());
 	}
 	

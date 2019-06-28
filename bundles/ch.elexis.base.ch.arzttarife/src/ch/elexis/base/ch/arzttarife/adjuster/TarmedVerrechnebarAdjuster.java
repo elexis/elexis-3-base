1ede778a465e5d0483a14248a9f4e93359ff8c2f
sazgin <sa@slogic.at>
2019-06-28 12:07:10 +0200

    [15429] TarmedLeistungen vor der Verrechnung für die Kons anpassen

--- bundles/ch.elexis.base.ch.arzttarife/src/ch/elexis/base/ch/arzttarife/adjuster/TarmedVerrechnebarAdjuster.java
+++ bundles/ch.elexis.base.ch.arzttarife/src/ch/elexis/base/ch/arzttarife/adjuster/TarmedVerrechnebarAdjuster.java
@@ -1,9 +1,15 @@
 package ch.elexis.base.ch.arzttarife.adjuster;
 
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+
 import ch.elexis.core.data.interfaces.IVerrechenbar;
 import ch.elexis.core.data.interfaces.IVerrechenbarAdjuster;
+import ch.elexis.data.Fall;
 import ch.elexis.data.Konsultation;
 import ch.elexis.data.TarmedLeistung;
+import ch.rgw.tools.TimeTool;
 
 public class TarmedVerrechnebarAdjuster implements IVerrechenbarAdjuster {
 	
@@ -11,8 +17,24 @@ public class TarmedVerrechnebarAdjuster implements IVerrechenbarAdjuster {
 	@Override
 	public IVerrechenbar adjust(IVerrechenbar verrechenbar, Konsultation kons){
 		if (verrechenbar instanceof TarmedLeistung) {
-			System.out.println("ADJUST " + verrechenbar.getCode() + " for " + kons.getLabel());
-			// TODO add code for validating and changing according to context of kons
+			TarmedLeistung leistung = (TarmedLeistung) verrechenbar;
+			String leistungLaw = leistung.get(TarmedLeistung.FLD_LAW);
+			// check if a law for a leistung is specified
+			if (StringUtils.isNotEmpty(leistungLaw)) {
+				Fall fall = kons.getFall();
+				String law = fall.getConfiguredBillingSystemLaw().name();
+				
+				// law is not compatible for this leistung law look for compatible one
+				if (!leistungLaw.equals(law)) {
+					verrechenbar = TarmedLeistung.getFromCode(leistung.getCode(),
+						new TimeTool(kons.getDateTime()), fall.getAbrechnungsSystem());
+				}
+				if (verrechenbar == null) {
+					MessageDialog.openWarning(Display.getDefault().getActiveShell(), "Warnung",
+						"Warnung folgende Leistung '" + leistung.getCode()
+							+ "' konnte im aktuellen Kontext (Fall, Konsultation, Gesetz) nicht verrechnet werden.");
+				}
+			}
 		}
 		return verrechenbar;
 	}

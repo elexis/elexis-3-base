6360c60740ef763d8f247093914f6c11ff76a43c
thomashu <huster@medevit.at>
2019-07-08 16:25:27 +0200

    [15488] added check for existing vaccinations to import wizard

--- bundles/at.medevit.elexis.ehc.ui.vacdoc/src/at/medevit/elexis/ehc/ui/vacdoc/wizard/ImportVaccinationsWizardPage1.java
+++ bundles/at.medevit.elexis.ehc.ui.vacdoc/src/at/medevit/elexis/ehc/ui/vacdoc/wizard/ImportVaccinationsWizardPage1.java
@@ -1,38 +1,51 @@
 package at.medevit.elexis.ehc.ui.vacdoc.wizard;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ColumnLabelProvider;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
-import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.ehealth_connector.cda.Consumable;
 import org.ehealth_connector.cda.ch.vacd.CdaChVacd;
 import org.ehealth_connector.cda.ch.vacd.Immunization;
+import org.ehealth_connector.common.Code;
 
 import at.medevit.elexis.ehc.core.EhcCoreMapper;
 import at.medevit.elexis.ehc.ui.vacdoc.service.VacdocServiceComponent;
+import at.medevit.elexis.impfplan.model.po.Vaccination;
+import ch.elexis.core.constants.StringConstants;
+import ch.elexis.core.ui.UiDesk;
 import ch.elexis.data.Patient;
+import ch.elexis.data.Query;
+import ch.rgw.tools.TimeTool;
 
 public class ImportVaccinationsWizardPage1 extends WizardPage {
 	private TableViewer contentViewer;
 	private CdaChVacd ehcDocument;
 	
+	private List<Vaccination> vaccinations;
+	
 	protected ImportVaccinationsWizardPage1(String pageName, CdaChVacd ehcDocument){
 		super(pageName);
 		setTitle(pageName);
 		this.ehcDocument = ehcDocument;
+		Patient elexisPatient = EhcCoreMapper.getElexisPatient(ehcDocument.getPatient());
+		vaccinations = getVaccinations(elexisPatient);
 	}
 	
 	@Override
@@ -49,7 +62,7 @@ public class ImportVaccinationsWizardPage1 extends WizardPage {
 		control.setLayoutData(gd);
 		
 		contentViewer.setContentProvider(new ArrayContentProvider());
-		contentViewer.setLabelProvider(new LabelProvider() {
+		contentViewer.setLabelProvider(new ColumnLabelProvider() {
 			@Override
 			public String getText(Object element){
 				if (element instanceof Immunization) {
@@ -59,6 +72,17 @@ public class ImportVaccinationsWizardPage1 extends WizardPage {
 						+ immunization.getAuthor().getCompleteName();
 				}
 				return super.getText(element);
+			}
+			
+			@Override
+			public Color getBackground(Object element){
+				if (element instanceof Immunization) {
+					Immunization immunization = (Immunization) element;
+					if (isExisting(immunization, vaccinations)) {
+						return UiDesk.getColorFromRGB("D3D3D3");
+					}
+				}
+				return super.getBackground(element);
 			}
 			
 			@Override
@@ -84,9 +108,31 @@ public class ImportVaccinationsWizardPage1 extends WizardPage {
 			if (ehcDocument != null) {
 				List<Immunization> immunizations = ehcDocument.getImmunizations();
 				contentViewer.setInput(immunizations);
-				contentViewer.setSelection(new StructuredSelection(immunizations), true);
 			}
 		}
+	}
+	
+	private List<Vaccination> getVaccinations(Patient elexisPatient){
+		Query<Vaccination> qbe = new Query<>(Vaccination.class);
+		qbe.add("ID", Query.NOT_EQUAL, StringConstants.VERSION_LITERAL);
+		qbe.add(Vaccination.FLD_PATIENT_ID, Query.EQUALS, elexisPatient.getId());
+		qbe.orderBy(false, Vaccination.FLD_DOA);
+		return qbe.execute();
+	}
+	
+	private boolean isExisting(Immunization immunization, List<Vaccination> vaccinations){
+		for (Vaccination vaccination : vaccinations) {
+			Date applyDate = immunization.getApplyDate();
+			TimeTool vaccDate = vaccination.getDateOfAdministration();
+			if (applyDate.equals(vaccDate.getTime())) {
+				Consumable consumable = immunization.getConsumable();
+				Code atcCode = consumable.getWhoAtcCode();
+				if (atcCode.getCode().equalsIgnoreCase(vaccination.getAtcCode())) {
+					return consumable.getLotNr().equalsIgnoreCase(vaccination.getLotNo());
+				}
+			}
+		}
+		return false;
 	}
 	
 	@Override
@@ -102,17 +148,18 @@ public class ImportVaccinationsWizardPage1 extends WizardPage {
 	@SuppressWarnings("unchecked")
 	private List<Immunization> getSelectedImmunizations(){
 		IStructuredSelection contentSelection = (IStructuredSelection) contentViewer.getSelection();
-		
 		if (!contentSelection.isEmpty()) {
-			return contentSelection.toList();
+			List<Immunization> immunizations = contentSelection.toList();
+			return immunizations.stream().filter(i -> !isExisting(i, vaccinations))
+				.collect(Collectors.toList());
 		}
 		return Collections.emptyList();
 	}
-
+	
 	public boolean finish(){
 		try {
 			Patient elexisPatient = EhcCoreMapper.getElexisPatient(ehcDocument.getPatient());
-
+			
 			VacdocServiceComponent.getService().importImmunizations(elexisPatient,
 				getSelectedImmunizations());
 		} catch (Exception e) {

dbaf17fee65b45dff6c5ddabda2c58d1ab099505
Daniel Rösch <dr@vality.ch>
2019-07-12 15:45:13 +0200

    Add license to source code files

--- bundles/ch.itmed.fop.printing/src/ch/itmed/fop/printing/handlers/PatientAddressLabelHandler.java
+++ bundles/ch.itmed.fop.printing/src/ch/itmed/fop/printing/handlers/PatientAddressLabelHandler.java
@@ -1,8 +1,13 @@
-/* Copyright (C) 2018 vality GmbH - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by vality GmbH <info@vality.ch>, 2018
- */
+/*******************************************************************************
+ * Copyright (c) 2019 IT-Med AG <info@it-med-ag.ch>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IT-Med AG <info@it-med-ag.ch> - initial implementation
+ ******************************************************************************/
 
 package ch.itmed.fop.printing.handlers;
 
--- bundles/ch.itmed.fop.printing/src/ch/itmed/fop/printing/xml/elements/ArticlesElement.java
+++ bundles/ch.itmed.fop.printing/src/ch/itmed/fop/printing/xml/elements/ArticlesElement.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2019 IT-Med AG <info@it-med-ag.ch>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IT-Med AG <info@it-med-ag.ch> - initial implementation
+ ******************************************************************************/
+
 package ch.itmed.fop.printing.xml.elements;
 
 import java.util.List;

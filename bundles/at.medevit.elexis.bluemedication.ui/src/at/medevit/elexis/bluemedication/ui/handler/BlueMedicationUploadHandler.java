0d408649a85d0e4f854e6e8453f4171746ffb5cb
thomashu <huster@medevit.at>
2019-09-09 15:58:25 +0200

    [15795] added upload progress dialog

--- bundles/at.medevit.elexis.bluemedication.ui/src/at/medevit/elexis/bluemedication/ui/handler/BlueMedicationUploadHandler.java
+++ bundles/at.medevit.elexis.bluemedication.ui/src/at/medevit/elexis/bluemedication/ui/handler/BlueMedicationUploadHandler.java
@@ -1,14 +1,21 @@
 package at.medevit.elexis.bluemedication.ui.handler;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.IHandler;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.slf4j.LoggerFactory;
 
 import at.medevit.elexis.bluemedication.core.BlueMedicationServiceHolder;
 import at.medevit.elexis.bluemedication.core.UploadResult;
@@ -27,19 +34,39 @@ public class BlueMedicationUploadHandler extends AbstractHandler implements IHan
 				DocHandle docHandle = (DocHandle) object;
 				if (docHandle.getMimeType().endsWith("pdf")
 					|| docHandle.getTitle().endsWith(".pdf")) {
-					Result<UploadResult> result = BlueMedicationServiceHolder.getService()
-						.uploadDocument(
-						docHandle.getPatient(),
-						docHandle.createTemporaryFile(docHandle.getTitle()));
-					if (result.isOK()) {
-						// open the uri of the result
-						Program.launch(result.get().getUrl());
-						BlueMedicationServiceHolder.getService().addPendingUploadResult(docHandle,
-							result.get());
-					} else {
-						MessageDialog.openError(Display.getDefault().getActiveShell(),
-							"BlueMedication",
-							"Beim hochladen der Datei ist ein Fehler aufgetreten.");
+					Shell activeshell = HandlerUtil.getActiveShell(event);
+					ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(activeshell);
+					try {
+						progressDialog.run(true, false, new IRunnableWithProgress() {
+							public void run(IProgressMonitor monitor)
+								throws InvocationTargetException, InterruptedException{
+								monitor.beginTask(
+									"BlueMedication Upload von " + docHandle.getLabel(),
+									IProgressMonitor.UNKNOWN);
+								Result<UploadResult> result = BlueMedicationServiceHolder
+									.getService().uploadDocument(docHandle.getPatient(),
+										docHandle.createTemporaryFile(docHandle.getTitle()));
+								if (result.isOK()) {
+									Display.getDefault().syncExec(() -> {
+										// open the uri of the result
+										Program.launch(result.get().getUrl());
+										BlueMedicationServiceHolder.getService()
+											.addPendingUploadResult(docHandle, result.get());
+									});
+								} else {
+									Display.getDefault().syncExec(() -> {
+										MessageDialog.openError(
+											Display.getDefault().getActiveShell(), "BlueMedication",
+											"Beim hochladen der Datei ist ein Fehler aufgetreten.\n\nBitte HIN client Konfiguration prüfen.");
+									});
+								}
+							}
+						});
+					} catch (InvocationTargetException | InterruptedException e) {
+						MessageDialog.openError(activeshell, "BlueMedication",
+							"Bluemedication Upload konnte nicht gestartet werden.");
+						LoggerFactory.getLogger(getClass())
+							.error("Error creating structured diagnosis", e);
 					}
 				}
 			}

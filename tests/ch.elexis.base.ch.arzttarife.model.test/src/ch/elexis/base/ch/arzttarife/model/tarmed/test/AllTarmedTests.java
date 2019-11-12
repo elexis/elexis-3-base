99a9c4d792db03848d0e44fd0857cf8a3e066db9
Marco Descher <marco@descher.at>
2019-11-12 13:57:05 +0100

    [17027] Fix ArzttarifeTest

--- tests/ch.elexis.base.ch.arzttarife.model.test/src/ch/elexis/base/ch/arzttarife/model/tarmed/test/AllTarmedTests.java
+++ tests/ch.elexis.base.ch.arzttarife.model.test/src/ch/elexis/base/ch/arzttarife/model/tarmed/test/AllTarmedTests.java
@@ -1,8 +1,15 @@
 package ch.elexis.base.ch.arzttarife.model.tarmed.test;
 
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
+
+import ch.elexis.base.ch.arzttarife.elexis.service.test.IEncounterServiceTest;
+import ch.elexis.core.l10n.Messages;
+import ch.elexis.core.model.ch.BillingLaw;
+import ch.elexis.core.services.IBillingSystemService;
+import ch.elexis.core.utils.OsgiServiceUtil;
 
 @RunWith(Suite.class)
 @SuiteClasses({
@@ -11,5 +18,24 @@ import org.junit.runners.Suite.SuiteClasses;
 	TarmedBillingTest.class
 })
 public class AllTarmedTests {
+	
+	public static final String KVG_NAME = Messages.Fall_KVG_Name;
+	public static final String UVG_NAME = Messages.Fall_UVG_Name;
+	public static final String MV_NAME = Messages.Fall_MV_Name;
+	public static final String IV_NAME = Messages.Fall_IV_Name;
+	private static final String KVG_REQUIREMENTS = Messages.Fall_KVGRequirements; //$NON-NLS-1$
+	public static final String UVG_REQUIREMENTS = Messages.Fall_UVGRequirements; //$NON-NLS-1$
+	public static final String CONST_TARMED_DRUCKER = Messages.Fall_TarmedPrinter; //$NON-NLS-1$
+	public static final String CONST_TARMED_LEISTUNG = Messages.Fall_TarmedLeistung; //$NON-NLS-1$
+	public static final String VVG_NAME = Messages.Fall_VVG_Name;
+	public static final String PRIVATE_NAME = Messages.Fall_Private_Name; //$NON-NLS-1$	
+	
+	@BeforeClass
+	public static void beforeClass() {
+		// initialize the required billing systems
+		IBillingSystemService billingSystemService = OsgiServiceUtil.getService(IBillingSystemService.class).get();
+		billingSystemService.addOrModifyBillingSystem(KVG_NAME, CONST_TARMED_LEISTUNG, CONST_TARMED_DRUCKER, KVG_REQUIREMENTS, BillingLaw.KVG);
+		billingSystemService.addOrModifyBillingSystem(UVG_NAME, CONST_TARMED_LEISTUNG, CONST_TARMED_DRUCKER, UVG_REQUIREMENTS, BillingLaw.UVG);
+	}
 	
 }

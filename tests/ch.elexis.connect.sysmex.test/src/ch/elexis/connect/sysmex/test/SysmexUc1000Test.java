9bb1fa1472440626520d7272d1b7fc14bd29b62d
thomashu <huster@medevit.at>
2020-03-17 17:50:23 +0100

    [14435] added UC1000Data results with info in properties and tests

--- tests/ch.elexis.connect.sysmex.test/src/ch/elexis/connect/sysmex/test/SysmexUc1000Test.java
+++ tests/ch.elexis.connect.sysmex.test/src/ch/elexis/connect/sysmex/test/SysmexUc1000Test.java
@@ -22,11 +22,57 @@ public class SysmexUc1000Test {
 		UC1000Data data = new UC1000Data();
 		assertEquals(data.getSize(), content.length());
 		data.parse(content);
+		
 		ResultInfo uroInfo = data.getResultInfo("uro");
 		assertNotNull(uroInfo);
 		assertTrue(uroInfo.isAnalyzed());
+		assertEquals("3+", uroInfo.getQualitativValue());
+		assertEquals("8.0", uroInfo.getSemiQualitativValue());
+		
+		ResultInfo bidInfo = data.getResultInfo("bid");
+		assertNotNull(bidInfo);
+		assertTrue(bidInfo.isAnalyzed());
+		assertEquals("-", bidInfo.getQualitativValue());
+		assertEquals("", bidInfo.getSemiQualitativValue());
+		
 		ResultInfo bilInfo = data.getResultInfo("bil");
 		assertNotNull(bilInfo);
 		assertTrue(bilInfo.isAnalyzed());
+		
+		ResultInfo ketInfo = data.getResultInfo("ket");
+		assertNotNull(ketInfo);
+		assertTrue(ketInfo.isAnalyzed());
+		
+		ResultInfo gluInfo = data.getResultInfo("glu");
+		assertNotNull(gluInfo);
+		assertTrue(gluInfo.isAnalyzed());
+		
+		ResultInfo proInfo = data.getResultInfo("pro");
+		assertNotNull(proInfo);
+		assertTrue(proInfo.isAnalyzed());
+		
+		ResultInfo phInfo = data.getResultInfo("ph");
+		assertNotNull(phInfo);
+		assertTrue(phInfo.isAnalyzed());
+		
+		ResultInfo nitInfo = data.getResultInfo("nit");
+		assertNotNull(nitInfo);
+		assertTrue(nitInfo.isAnalyzed());
+		
+		ResultInfo leuInfo = data.getResultInfo("leu");
+		assertNotNull(leuInfo);
+		assertTrue(leuInfo.isAnalyzed());
+		
+		ResultInfo sgInfo = data.getResultInfo("sg");
+		assertNotNull(sgInfo);
+		assertTrue(sgInfo.isAnalyzed());
+		
+		ResultInfo creInfo = data.getResultInfo("cre");
+		assertNotNull(creInfo);
+		assertTrue(creInfo.isAnalyzed());
+		
+		ResultInfo albInfo = data.getResultInfo("alb");
+		assertNotNull(albInfo);
+		assertTrue(albInfo.isAnalyzed());
 	}
 }

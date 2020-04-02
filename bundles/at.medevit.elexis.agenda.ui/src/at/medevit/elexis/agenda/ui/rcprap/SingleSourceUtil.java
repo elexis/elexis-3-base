ba7b5f560602976d042e3fece1212c459108da51
Marco Descher <marco@descher.at>
2020-04-02 14:06:24 +0200

    [18505] Refactor a.m.e.agenda.ui for rcp/rap usage

--- bundles/at.medevit.elexis.agenda.ui/src/at/medevit/elexis/agenda/ui/rcprap/SingleSourceUtil.java
+++ bundles/at.medevit.elexis.agenda.ui/src/at/medevit/elexis/agenda/ui/rcprap/SingleSourceUtil.java
@@ -10,62 +10,63 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SingleSourceUtil {
-
-	private static final String RAP_BASE_URL = "/agenda/static/";
+	
 	private static Logger log = LoggerFactory.getLogger(SingleSourceUtil.class);
-
+	
 	private static final boolean IS_RAP;
 	private static final String HTML_BASE_URL;
-
+	
 	static {
 		Class<?> clazz = null;
 		try {
 			clazz = Class.forName("org.eclipse.rap.rwt.RWT"); //$NON-NLS-1$
-		} catch (ClassNotFoundException cnfe) {
-		}
+		} catch (ClassNotFoundException cnfe) {}
 		IS_RAP = clazz != null;
-
+		
 		if (IS_RAP) {
-			HTML_BASE_URL = RAP_BASE_URL + "html/";
+			String baseUrl = System.getenv("BASE_URL");
+			if (baseUrl == null) {
+				baseUrl = "";
+			}
+			HTML_BASE_URL = baseUrl + "/agenda/static/html/"; //$NON-NLS-1$
 		} else {
 			String _baseUrl = null;
 			try {
-				URL url = FileLocator
-						.toFileURL(FrameworkUtil.getBundle(SingleSourceUtil.class).getResource("/rsc/html/")); //$NON-NLS-1$
+				URL url = FileLocator.toFileURL(
+					FrameworkUtil.getBundle(SingleSourceUtil.class).getResource("/rsc/html/")); //$NON-NLS-1$
 				_baseUrl = url.toString();
 			} catch (IOException e) {
 				log.error("Initialization error", e); //$NON-NLS-1$
 			}
 			HTML_BASE_URL = _baseUrl;
 		}
-
+		
 	}
-
-	public static boolean isRap() {
+	
+	public static boolean isRap(){
 		return IS_RAP;
 	}
-
+	
 	/**
-	 * Resolve the html base url, i.e. the location of the fullcalendar html
-	 * resources.
+	 * Resolve the html base url, i.e. the location of the fullcalendar html resources.
 	 * 
 	 * @param html
 	 * @return
 	 */
-	public static String resolve(String html) {
+	public static String resolve(String html){
 		return HTML_BASE_URL + html;
 	}
-
+	
 	/**
-	 * The RAP {@link Browser#execute(String)} might throw an
-	 * {@link IllegalStateException} if script execution is not synchronized. Hence
-	 * if we face a RAP environment we enforce synchronization.
+	 * The RAP {@link Browser#execute(String)} might throw an {@link IllegalStateException} if
+	 * script execution is not synchronized. Hence if we face a RAP environment we enforce
+	 * synchronization.
 	 * 
 	 * @param browser
 	 * @param script
 	 * @return
 	 */
-	public static boolean executeScript(final Browser browser, final String script) {
+	public static boolean executeScript(final Browser browser, final String script){
 		log.debug("executeScript: {}", script); //$NON-NLS-1$
 		if (IS_RAP) {
 			synchronized (BrowserLock.class) {
@@ -82,8 +83,7 @@ public class SingleSourceUtil {
 			return browser.execute(script);
 		}
 	}
-
-	private class BrowserLock {
-	}
-
+	
+	private class BrowserLock {}
+	
 }

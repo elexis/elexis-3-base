6fb4ff8c60d9b0415b047b93f07227e3c17232f6
thomashu <huster@medevit.at>
2020-04-09 21:34:06 +0200

    [18677] added new lg1 medapp web rest interface
    Conflicts:
    	bundles/ch.elexis.labororder.lg1/src/ch/elexis/labororder/lg1/order/LabOrderAction.java

--- bundles/ch.elexis.labororder.lg1/src/ch/elexis/labororder/lg1/order/LabOrderAction.java
+++ bundles/ch.elexis.labororder.lg1/src/ch/elexis/labororder/lg1/order/LabOrderAction.java
@@ -1,38 +1,30 @@
 package ch.elexis.labororder.lg1.order;
 
-import static ch.elexis.core.constants.XidConstants.DOMAIN_EAN;
-
-import java.io.File;
-import java.text.MessageFormat;
-import java.util.Date;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Vector;
-
+import java.util.Map;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.slf4j.LoggerFactory;
 
-import ch.elexis.core.data.activator.CoreHub;
 import ch.elexis.core.data.events.ElexisEventDispatcher;
-import ch.elexis.core.data.util.LocalLock;
-import ch.elexis.core.model.ch.BillingLaw;
-import ch.elexis.data.Anwender;
-import ch.elexis.data.Fall;
-import ch.elexis.data.Kontakt;
-import ch.elexis.data.Mandant;
 import ch.elexis.data.Patient;
-import ch.elexis.hl7.data.HL7Kontakt;
-import ch.elexis.hl7.data.HL7Kostentraeger;
-import ch.elexis.hl7.data.HL7Mandant;
-import ch.elexis.hl7.data.HL7Patient;
-import ch.elexis.hl7.v26.HL7_OML_O21;
 import ch.elexis.labororder.lg1.messages.Messages;
-import ch.elexis.labororder.lg1.preferences.LG1PreferencePage;
-import ch.elexis.tarmedprefs.TarmedRequirements;
-import ch.rgw.io.FileTool;
-import ch.rgw.tools.StringTool;
-import ch.rgw.tools.TimeTool;
+import com.google.gson.Gson;
 
 public class LabOrderAction extends Action {
 	
@@ -48,275 +40,49 @@ public class LabOrderAction extends Action {
 	@Override
 	public void run(){
 		Patient patient = ElexisEventDispatcher.getSelectedPatient();
-		Kontakt costBearer = null;
-		Kontakt rechnungsempfaenger = null;
-		Date beginDate = null;
-		String vnr = ""; //$NON-NLS-1$
-		String plan = ""; //$NON-NLS-1$
-		// Patient und Kostentraeger bestimmen
-		if (patient == null) {
-			MessageDialog.openError(Display.getDefault().getActiveShell(),
-				Messages.LabOrderAction_errorTitleNoPatientSelected,
-				Messages.LabOrderAction_errorMessageNoPatientSelected);
-		} else {
-			Fall fall = (Fall) ElexisEventDispatcher.getSelected(Fall.class);
-			// Selected könnte noch vom vorangehendem Patienten sein
-			if (fall != null && fall.getPatient() != null
-				&& !patient.getId().equals(fall.getPatient().getId())) {
-				fall = null;
+		if (patient != null) {
+			try {
+				sendPostRequest(patient);
+			} catch (IOException e) {
+				LoggerFactory.getLogger(getClass()).error("Error contacting LG1 web service", e);
+				MessageDialog.openError(Display.getDefault().getActiveShell(), "Fehler",
+					"Es ist ein Fehler beim LG1 Aufruf aufgetreten.\n\n"
+						+ e.getLocalizedMessage());
 			}
-			// Wenn nur 1 Fall offen, dann wird dieser verwendet
-			if (fall == null) {
-				List<Fall> offeneFaelleList = new Vector<Fall>();
-				for (Fall tmpFall : patient.getFaelle()) {
-					if (tmpFall.isOpen()) {
-						offeneFaelleList.add(tmpFall);
-					}
-				}
-				if (offeneFaelleList.size() == 1) {
-					fall = offeneFaelleList.get(0);
-				}
-			}
-			if (fall == null) {
-				MessageDialog.openError(Display.getDefault().getActiveShell(),
-					Messages.LabOrderAction_errorTitleNoFallSelected,
-					Messages.LabOrderAction_errorMessageNoFallSelected);
-			} else {
-				costBearer = fall.getCostBearer();
-				if (costBearer == null) {
-					costBearer = fall.getGarant();
-				}
-				rechnungsempfaenger = fall.getRequiredContact(Fall.FLD_EXT_RECHNUNGSEMPFAENGER);
-				if (rechnungsempfaenger == null) {
-					rechnungsempfaenger = fall.getGarant();
-				}
-				plan = fall.getAbrechnungsSystem();
-				beginDate = new TimeTool(fall.getBeginnDatum()).getTime();
-				vnr = getInsuranceOrCaseNumber(fall);
-			}
-		}
-		
-		boolean ok = false;
-		long orderNr = -1;
-		String filenamePath = "-"; //$NON-NLS-1$
-		if (patient != null && costBearer != null) {
-			orderNr = getNextOrderNr(patient);
-			filenamePath = writeHL7File(patient, rechnungsempfaenger, costBearer, plan, beginDate,
-				vnr, orderNr);
-			if (filenamePath != null) {
-				ok = true; // @deprecated openBrowser();
-			}
-		}
-		
-		if (ok) {
-			String patLabel = "-"; //$NON-NLS-1$
-			if (patient != null) {
-				patLabel = patient.getLabel();
-			}
-			String orderNrText = ""; //$NON-NLS-1$
-			if (orderNr >= 0) {
-				orderNrText = new Long(orderNr).toString();
-			}
-			MessageDialog.openInformation(Display.getDefault().getActiveShell(),
-				Messages.LabOrderAction_infoTitleLabOrderFinshed,
-				MessageFormat.format(Messages.LabOrderAction_infoMessageLabOrderFinshed,
-					orderNrText, patLabel, filenamePath));
 		}
 	}
 	
-	private String getInsuranceOrCaseNumber(final Fall fall){
-		String nummer = null;
-		BillingLaw gesetz = fall.getConfiguredBillingSystemLaw();
-		if (gesetz != null) {
-			if (gesetz == BillingLaw.IVG) {
-				nummer = fall.getRequiredString(TarmedRequirements.CASE_NUMBER);
-			} else if (gesetz == BillingLaw.UVG) {
-				nummer = fall.getRequiredString(TarmedRequirements.ACCIDENT_NUMBER);
-			} else {
-				nummer = fall.getRequiredString(TarmedRequirements.INSURANCE_NUMBER);
-			}
-		}
-		if (nummer == null) {
-			nummer = fall.getInfoString(TarmedRequirements.CASE_NUMBER);
-			if ("".equals(nummer)) { //$NON-NLS-1$
-				nummer = fall.getInfoString(TarmedRequirements.ACCIDENT_NUMBER);
-			}
-			if ("".equals(nummer)) { //$NON-NLS-1$
-				nummer = fall.getInfoString(TarmedRequirements.INSURANCE_NUMBER);
-			}
-		}
+	private void sendPostRequest(Patient patient) throws IOException{
+		HttpClient httpclient = HttpClients.createDefault();
+		HttpPost httppost = new HttpPost("https://ms2.medapp.ch/rest/lg1/v1/call-medapp/");
+
+		// Request parameters and other properties.
+		List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+		params.add(new BasicNameValuePair("appkey", "99987id76opkliuhj79joiplhji1doio987poku"));
+		params.add(new BasicNameValuePair("service", "orderentry"));
+		params.add(new BasicNameValuePair("data", getData(patient)));
+		httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
 		
-		return nummer;
+		//Execute and get the response.
+		HttpResponse response = httpclient.execute(httppost);
+		HttpEntity entity = response.getEntity();
+		
+		if (entity != null) {
+			try (InputStream instream = entity.getContent()) {
+				String responseText = IOUtils.toString(instream, "UTF-8");
+				ResponseDialog dialog =
+						new ResponseDialog(responseText, Display.getDefault().getActiveShell());
+				dialog.open();
+			}
+		}
 	}
 	
-	private void fillHL7Kontakt(final HL7Kontakt hl7Kontakt, final Kontakt kontakt){
-		
-		String name = kontakt.get(Kontakt.FLD_NAME1);
-		if (name == null) {
-			name = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setName(name.trim());
-		
-		String firstname = kontakt.get(Kontakt.FLD_NAME2);
-		if (firstname == null) {
-			firstname = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setFirstname(firstname.trim());
-		
-		String title = kontakt.get("Titel"); //$NON-NLS-1$
-		if (title == null) {
-			title = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setTitle(title.trim());
-		
-		String phone1 = kontakt.get(Kontakt.FLD_PHONE1);
-		if (phone1 == null) {
-			phone1 = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setPhone1(phone1.trim());
-		
-		String phone2 = kontakt.get(Kontakt.FLD_PHONE2);
-		if (phone2 == null) {
-			phone2 = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setPhone2(phone2.trim());
-		
-		String email = kontakt.get(Kontakt.FLD_E_MAIL);
-		if (email == null) {
-			email = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setEmail(email.trim());
-		
-		String fax = kontakt.get(Kontakt.FLD_FAX);
-		if (fax == null) {
-			fax = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setFax(fax.trim());
-		
-		String street = kontakt.get(Kontakt.FLD_STREET);
-		if (street == null) {
-			street = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setAddress1(street.trim());
-		
-		String other = kontakt.get(Patient.FLD_NAME3);
-		if (other == null) {
-			other = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setAddress2(other.trim());
-		
-		String city = kontakt.get(Patient.FLD_PLACE);
-		if (city == null) {
-			city = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setCity(city.trim());
-		
-		String zip = kontakt.get(Patient.FLD_ZIP);
-		if (zip == null) {
-			zip = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setZip(zip.trim());
-		
-		String country = kontakt.get(Patient.FLD_COUNTRY);
-		if (country == null) {
-			country = ""; //$NON-NLS-1$
-		}
-		hl7Kontakt.setCountry(country.trim());
-	}
-	
-	/**
-	 * Creates HL7 File (V2.6)
-	 * 
-	 * @return full filename path of the file. Null if error happens
-	 */
-	private String writeHL7File(final Patient patient, final Kontakt rechnungsempfaenger,
-		final Kontakt kostentraeger, final String plan, final Date beginDate, final String vnr,
-		final long orderNr){
-		
-		String uniqueMessageControlID = StringTool.unique("MessageControlID"); //$NON-NLS-1$
-		String uniqueProcessingID = StringTool.unique("ProcessingID"); //$NON-NLS-1$
-		
-		HL7Mandant mandant = new HL7Mandant();
-		Mandant actMandant = ElexisEventDispatcher.getSelectedMandator();
-		if (actMandant != null) {
-			mandant.setLabel(actMandant.get(Anwender.FLD_LABEL));
-			mandant.setEan(actMandant.getXid(DOMAIN_EAN));
-		}
-		
-		HL7_OML_O21 omlO21 =
-			new HL7_OML_O21("CHELEXIS", "PATDATA", Messages.LabOrderAction_receivingApplication, "",
-				Messages.LabOrderAction_receivingFacility, uniqueMessageControlID,
-				uniqueProcessingID, mandant);
-		
-		// Patient
-		HL7Patient hl7Patient = new HL7Patient();
-		fillHL7Kontakt(hl7Patient, patient);
-		String geschlecht = patient.getGeschlecht();
-		if (geschlecht != null && geschlecht.length() > 0) {
-			hl7Patient.setIsMale(
-				Patient.MALE.toUpperCase().equals(patient.getGeschlecht().toUpperCase()));
-		}
-		hl7Patient.setBirthdate(new TimeTool(patient.getGeburtsdatum()).getTime());
-		hl7Patient.setPatCode(patient.getPatCode());
-		
-		// Rechnungsempfaenger
-		HL7Kostentraeger hl7Rechnungsempfaenger = new HL7Kostentraeger();
-		fillHL7Kontakt(hl7Rechnungsempfaenger, rechnungsempfaenger);
-		hl7Rechnungsempfaenger.setEan(rechnungsempfaenger.getXid(DOMAIN_EAN));
-		
-		// Kostentraeger
-		HL7Kostentraeger hl7Kostentraeger = new HL7Kostentraeger();
-		fillHL7Kontakt(hl7Kostentraeger, kostentraeger);
-		hl7Kostentraeger.setEan(kostentraeger.getXid(DOMAIN_EAN));
-		
-		try {
-			String encodedMessage = omlO21.createText(hl7Patient, hl7Rechnungsempfaenger,
-				hl7Kostentraeger, plan, beginDate, vnr, orderNr);
-			
-			// write file
-			String filename =
-				new Long(orderNr).toString() + "_" + patient.get(Patient.FLD_PATID) + ".hl7"; //$NON-NLS-1$ //$NON-NLS-2$
-			File hl7File =
-				new File(LG1PreferencePage.getUploadDir() + File.separator + filename);
-			FileTool.writeFile(hl7File, encodedMessage.getBytes(TEXT_ENCODING));
-			
-			return hl7File.getPath();
-		} catch (Exception e) {
-			MessageDialog.openError(Display.getDefault().getActiveShell(), MessageFormat
-				.format(Messages.LabOrderAction_errorTitleCannotCreateHL7, omlO21.getVersion()),
-				e.getMessage());
-		}
-		return null;
-	}
-	
-	/**
-	 * Opens a browser in or outside of elexis
-	 * 
-	 * @return
-	 */
-	private long getNextOrderNr(final Patient patient) throws NumberFormatException{
-		LocalLock lock = new LocalLock("ch.elexis.labororder.lg1.nextOrderNr");
-		try {
-			while (!lock.tryLock()) {
-				// unlock if there is a hanging lock, if not changed for 2 sec.
-				long millis = lock.getLockCurrentMillis();
-				try {
-					Thread.sleep(2000);
-				} catch (Exception e) {
-					// ignore and carry on
-				}
-				if (millis == lock.getLockCurrentMillis()) {
-					lock.unlock();
-				}
-			}
-			
-			String value = CoreHub.globalCfg.get("ch.elexis.labororder.lg1.orderNr", "0");
-			Integer intValue = Integer.valueOf(value);
-			long ret = intValue++;
-			CoreHub.globalCfg.set("ch.elexis.labororder.lg1.orderNr", Integer.toString(intValue));
-			return ret;
-		} finally {
-			lock.unlock();
-		}
+	private String getData(Patient patient){
+		Gson gson = new Gson();
+		ch.elexis.labororder.lg1.order.model.Patient lg1Patient =
+			ch.elexis.labororder.lg1.order.model.Patient.of(patient);
+		Map<String, ch.elexis.labororder.lg1.order.model.Patient> map = new HashMap<>();
+		map.put("patient", lg1Patient);
+		return gson.toJson(map);
 	}
 }
---
+++ bundles/ch.elexis.labororder.lg1/src/ch/elexis/labororder/lg1/order/model/Patient.java
@@ -0,0 +1,139 @@
+package ch.elexis.labororder.lg1.order.model;
+
+import static ch.elexis.core.constants.XidConstants.DOMAIN_EAN;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
+
+import ch.elexis.core.data.events.ElexisEventDispatcher;
+import ch.elexis.core.model.ch.BillingLaw;
+import ch.elexis.core.types.Gender;
+import ch.elexis.data.Fall;
+import ch.elexis.data.Kontakt;
+import ch.elexis.labororder.lg1.messages.Messages;
+import ch.elexis.tarmedprefs.TarmedRequirements;
+import ch.rgw.tools.TimeTool;
+
+public class Patient {
+	String id = "";
+	String dateofbirth = "";
+	String gender = "";
+	String title = "";
+	String lastname = "";
+	String firstname = "";
+	String street = "";
+	String zip = "";
+	String city = "";
+	String country = "";
+	String insurancenumber = "";
+	String insurancename = "";
+	String insurancegln = "";
+	String billing = "";
+	
+	public static Patient of(ch.elexis.data.Patient patient){
+		Patient ret = new Patient();
+		ret.id = patient.getPatCode();
+		ret.dateofbirth = new TimeTool(patient.getGeburtsdatum()).toString(TimeTool.DATE_COMPACT);
+		ret.gender = patient.getGender() == Gender.FEMALE ? "female" : "male";
+		ret.title = patient.get(ch.elexis.data.Patient.TITLE);
+		ret.lastname = patient.getName();
+		ret.firstname = patient.getVorname();
+		ret.street = patient.getAnschrift().getStrasse();
+		ret.zip = patient.getAnschrift().getPlz();
+		ret.city = patient.getAnschrift().getOrt();
+		ret.country = patient.getAnschrift().getLand();
+		Fall fall = getFall(patient);
+		if (fall == null) {
+			MessageDialog.openError(Display.getDefault().getActiveShell(),
+				Messages.LabOrderAction_errorTitleNoFallSelected,
+				Messages.LabOrderAction_errorMessageNoFallSelected);
+		}
+		ret.insurancenumber = getInsuranceOrCaseNumber(fall);
+		ret.insurancename = getInsuranceName(fall);
+		ret.insurancegln = getInsuranceGln(fall);
+		ret.billing = getBilling(fall);
+		return ret;
+	}
+	
+	private static String getBilling(Fall fall){
+		Kontakt costBearer = fall.getCostBearer();
+		Kontakt guarantor = fall.getGarant();
+		if (costBearer == null) {
+			costBearer = fall.getGarant();
+		}
+		if (costBearer != null && costBearer.istOrganisation()) {
+			if (guarantor.equals(costBearer)) {
+				return "tp";
+			}
+		}
+		return "tg";
+	}
+	
+	private static String getInsuranceName(Fall fall){
+		Kontakt costBearer = fall.getCostBearer();
+		if (costBearer == null) {
+			costBearer = fall.getGarant();
+		}
+		return costBearer.getLabel(true);
+	}
+	
+	private static String getInsuranceGln(Fall fall){
+		Kontakt costBearer = fall.getCostBearer();
+		if (costBearer == null) {
+			costBearer = fall.getGarant();
+		}
+		return costBearer.getXid(DOMAIN_EAN);
+	}
+	
+	private static Fall getFall(ch.elexis.data.Patient patient){
+		Fall fall = (Fall) ElexisEventDispatcher.getSelected(Fall.class);
+		if (fall != null && fall.getPatient() != null
+			&& !patient.getId().equals(fall.getPatient().getId())) {
+			fall = null;
+		}
+		if (fall == null) {
+			List<Fall> offeneFaelleList = new ArrayList<>();
+			for (Fall tmpFall : patient.getFaelle()) {
+				if (tmpFall.isOpen()) {
+					offeneFaelleList.add(tmpFall);
+				}
+			}
+			if (offeneFaelleList.size() == 1) {
+				fall = offeneFaelleList.get(0);
+			}
+		}
+		return fall;
+	}
+	
+	private static String getInsuranceOrCaseNumber(final Fall fall){
+		String nummer = null;
+		BillingLaw gesetz = BillingLaw.KVG;
+		try {
+			gesetz = fall.getConfiguredBillingSystemLaw();
+		} catch (IllegalArgumentException e) {
+			// unknown billing system, lets try with KVG
+		}
+		if (gesetz != null) {
+			if (gesetz == BillingLaw.IVG) {
+				nummer = fall.getRequiredString(TarmedRequirements.CASE_NUMBER);
+			} else if (gesetz == BillingLaw.UVG) {
+				nummer = fall.getRequiredString(TarmedRequirements.ACCIDENT_NUMBER);
+			} else {
+				nummer = fall.getRequiredString(TarmedRequirements.INSURANCE_NUMBER);
+			}
+		}
+		if (nummer == null) {
+			nummer = fall.getInfoString(TarmedRequirements.CASE_NUMBER);
+			if ("".equals(nummer)) { //$NON-NLS-1$
+				nummer = fall.getInfoString(TarmedRequirements.ACCIDENT_NUMBER);
+			}
+			if ("".equals(nummer)) { //$NON-NLS-1$
+				nummer = fall.getInfoString(TarmedRequirements.INSURANCE_NUMBER);
+			}
+		}
+		return nummer;
+	}
+}

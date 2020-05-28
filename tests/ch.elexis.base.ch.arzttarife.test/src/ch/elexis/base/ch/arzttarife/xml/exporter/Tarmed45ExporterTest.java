633f5eeab820abb44bfdd64e39dfb2d5a27a2d6a
thomashu <huster@medevit.at>
2020-05-28 13:38:27 +0200

    [20056] fixed and add Tarmed45ExporterTest again

--- tests/ch.elexis.base.ch.arzttarife.test/src/ch/elexis/base/ch/arzttarife/xml/exporter/Tarmed45ExporterTest.java
+++ tests/ch.elexis.base.ch.arzttarife.test/src/ch/elexis/base/ch/arzttarife/xml/exporter/Tarmed45ExporterTest.java
@@ -9,6 +9,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 
@@ -17,6 +18,7 @@ import ch.elexis.base.ch.arzttarife.test.TestData;
 import ch.elexis.base.ch.arzttarife.test.TestData.TestSzenario;
 import ch.elexis.core.data.interfaces.IRnOutputter;
 import ch.elexis.core.model.IInvoice;
+import ch.elexis.data.Verrechnet;
 import ch.fd.invoice450.request.RequestType;
 import ch.fd.invoice450.request.VatRateType;
 import ch.fd.invoice450.request.VatType;
@@ -31,27 +33,29 @@ public class Tarmed45ExporterTest {
 		assertFalse(szenario.getInvoices().isEmpty());
 		Tarmed45Exporter exporter = new Tarmed45Exporter();
 		List<IInvoice> invoices = szenario.getInvoices();
-		RequestType nonNullRequest = null;
-		for (IInvoice invoice : invoices) {
-			ByteArrayOutputStream output = new ByteArrayOutputStream();
-			assertTrue(exporter.doExport(invoice, output, IRnOutputter.TYPE.ORIG));
-			RequestType request = TarmedJaxbUtil
+		Optional<IInvoice> vatInvoice = invoices.stream()
+			.filter(i -> i.getBilled().stream()
+				.filter(b -> b.getExtInfo(Verrechnet.VATSCALE) != null).findFirst().isPresent())
+			.findFirst();
+		assertTrue(vatInvoice.isPresent());
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+		assertTrue(exporter.doExport(vatInvoice.get(), output, IRnOutputter.TYPE.ORIG));
+		RequestType vatRequest =
+			TarmedJaxbUtil
 				.unmarshalInvoiceRequest450(new ByteArrayInputStream(output.toByteArray()));
-			// check if the vat is included
-			assertNotNull(request.getPayload().getBody().getTiersGarant());
-			VatType vat = request.getPayload().getBody().getTiersGarant().getBalance().getVat();
-			assertNotNull(vat);
-			for (VatRateType vatRate : vat.getVatRate()) {
-				if (vatRate.getVatRate() == 0.0) {
-					assertEquals(0.0, vatRate.getVat(), 0.01);
-				} else {
-					Double expectedVat = (vatRate.getAmount() / (100.0 + vatRate.getVatRate()))
-						* vatRate.getVatRate();
-					assertEquals(expectedVat, vatRate.getVat(), 0.01);
-					nonNullRequest = request;
-				}
+		// check vat values
+		assertNotNull(vatRequest.getPayload().getBody().getTiersGarant());
+		VatType vat = vatRequest.getPayload().getBody().getTiersGarant().getBalance().getVat();
+		assertNotNull(vat);
+		assertEquals(3, vat.getVatRate().size());
+		for (VatRateType vatRate : vat.getVatRate()) {
+			if (vatRate.getVatRate() == 0.0) {
+				assertEquals(0.0, vatRate.getVat(), 0.01);
+			} else {
+				Double expectedVat =
+					(vatRate.getAmount() / (100.0 + vatRate.getVatRate())) * vatRate.getVatRate();
+				assertEquals(expectedVat, vatRate.getVat(), 0.01);
 			}
 		}
-		assertNotNull(nonNullRequest);
 	}
 }

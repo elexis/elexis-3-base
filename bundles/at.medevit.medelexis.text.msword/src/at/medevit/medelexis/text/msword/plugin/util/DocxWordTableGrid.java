95cd0c92bc48961755ee0d53fbfbfb19e3f8324f
Niklaus Giger <niklaus.giger@member.fsf.org>
2020-06-08 10:51:21 +0200

    [20226] Medelexis OpenOffice and MsWord plugin add in OpenSource (version 3.8)

---
+++ bundles/at.medevit.medelexis.text.msword/src/at/medevit/medelexis/text/msword/plugin/util/DocxWordTableGrid.java
@@ -0,0 +1,13 @@
+package at.medevit.medelexis.text.msword.plugin.util;
+
+import org.w3c.dom.Node;
+
+public class DocxWordTableGrid {
+	
+	Node grid;
+
+	public DocxWordTableGrid(Node node){
+		this.grid = node;
+	}
+	
+}

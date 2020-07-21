fe3a0bdebcd2b8024c7419141bc6e036c15414d3
thomashu <huster@medevit.at>
2020-07-21 11:27:51 +0200

    [20684] add emediplan json with desc and event from ui on letter create

--- bundles/at.medevit.elexis.emediplan.ui/src/at/medevit/elexis/emediplan/ui/handler/SaveEMediplanUtil.java
+++ bundles/at.medevit.elexis.emediplan.ui/src/at/medevit/elexis/emediplan/ui/handler/SaveEMediplanUtil.java
@@ -6,8 +6,11 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import ch.elexis.core.model.IDocument;
+import ch.elexis.core.model.IDocumentLetter;
 import ch.elexis.core.model.IMandator;
 import ch.elexis.core.model.IPatient;
+import ch.elexis.core.services.holder.CoreModelServiceHolder;
 import ch.elexis.data.Brief;
 import ch.elexis.data.Mandant;
 import ch.elexis.data.Patient;
@@ -25,11 +28,13 @@ public class SaveEMediplanUtil {
 		return pdfFile.getAbsolutePath();
 	}
 	
-	public static void saveEMediplan(IPatient patient, IMandator mandant, byte[] content){
+	public static IDocument saveEMediplan(IPatient patient, IMandator mandant, byte[] content){
 		TimeTool now = new TimeTool();
 		Brief letter = new Brief("eMediplan " + now.toString(TimeTool.DATE_GER), now,
 			Mandant.load(mandant.getId()), null, null, Brief.UNKNOWN);
 		letter.setPatient(Patient.load(patient.getId()));
 		letter.save(content, "pdf");
+		return CoreModelServiceHolder.get().load(letter.getId(), IDocumentLetter.class)
+			.orElse(null);
 	}
 }

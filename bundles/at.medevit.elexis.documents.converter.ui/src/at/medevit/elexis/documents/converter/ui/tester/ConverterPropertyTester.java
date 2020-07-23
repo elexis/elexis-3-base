a72a9cae0dfc22dd45a25caf6cffe18e8730d1b7
thomashu <huster@medevit.at>
2020-07-23 12:53:59 +0200

    [20706] added jodconverter rest based documents converter incl. ui

---
+++ bundles/at.medevit.elexis.documents.converter.ui/src/at/medevit/elexis/documents/converter/ui/tester/ConverterPropertyTester.java
@@ -0,0 +1,27 @@
+package at.medevit.elexis.documents.converter.ui.tester;
+
+import javax.inject.Inject;
+
+import org.eclipse.core.expressions.PropertyTester;
+
+import ch.elexis.core.services.IDocumentConverter;
+import ch.elexis.core.ui.util.CoreUiUtil;
+
+public class ConverterPropertyTester extends PropertyTester {
+	
+	@Inject
+	private IDocumentConverter converter;
+	
+	public ConverterPropertyTester(){
+		CoreUiUtil.injectServices(this);
+	}
+	
+	@Override
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue){
+		if ("available".equals(property)) {
+			return converter.isAvailable();
+		}
+		return false;
+	}
+	
+}

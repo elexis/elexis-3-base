f44b4215225a48e2db6f1a637a58707be17dd312
Marco Descher <marco@descher.at>
2020-12-10 13:52:11 +0100

    [21599] DocumentDocHandle support VirtualFilesystemService

--- bundles/ch.elexis.omnivore.model/src/ch/elexis/omnivore/model/IDocumentHandle.java
+++ bundles/ch.elexis.omnivore.model/src/ch/elexis/omnivore/model/IDocumentHandle.java
@@ -1,10 +1,13 @@
 package ch.elexis.omnivore.model;
 
 import ch.elexis.core.model.IDocument;
+import ch.elexis.core.services.IVirtualFilesystemService.IVirtualFilesystemHandle;
 
 public interface IDocumentHandle extends IDocument {
 	
 	public boolean isCategory();
+	
+	public IVirtualFilesystemHandle getHandle();
 	
 	public boolean exportToFileSystem();
 }

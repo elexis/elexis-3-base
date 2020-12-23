085eb31b96495676d5bb84b19a4a0b4a2ded1bc9
thomas <thomas@thomas-hpelitebook840g5>
2020-12-23 15:33:40 +0100

    [21600] fix getting omnivore basepath, anre remove no protocol

--- bundles/ch.elexis.omnivore.model/src/ch/elexis/omnivore/model/internal/Preferences.java
+++ bundles/ch.elexis.omnivore.model/src/ch/elexis/omnivore/model/internal/Preferences.java
@@ -2,17 +2,30 @@ package ch.elexis.omnivore.model.internal;
 
 import static ch.elexis.omnivore.PreferenceConstants.BASEPATH;
 import static ch.elexis.omnivore.PreferenceConstants.STOREFS;
+import static ch.elexis.omnivore.PreferenceConstants.STOREFSGLOBAL;
 
 import ch.elexis.core.services.holder.ConfigServiceHolder;
 
 public class Preferences {
 
 	public static boolean storeInFilesystem() {
-		return ConfigServiceHolder.get().get(STOREFS, false);
+		if (ConfigServiceHolder.getGlobal(STOREFSGLOBAL, false)) {
+			return ConfigServiceHolder.getGlobal(STOREFS, false);
+		} else {
+			return ConfigServiceHolder.getLocal(STOREFS, false);
+		}
 	}
 
 	public static String getBasepath() {
-		return ConfigServiceHolder.get().get(BASEPATH, null);
+		String ret = "";
+		if (ConfigServiceHolder.getGlobal(STOREFSGLOBAL, false)) {
+			ret = ConfigServiceHolder.getGlobal(BASEPATH, null);
+		} else {
+			ret = ConfigServiceHolder.getLocal(BASEPATH, null);
+		}
+		if (ret.contains("no protocol: ")) {
+			ret = ret.replaceAll("no protocol: ", "");
+		}
+		return ret;
 	}
-
 }

78fd044958277e253e57d67b22a17372f4215a64
thomas <thomas@thomas-hpelitebook840g5>
2021-02-24 15:06:41 +0100

    [22080] add kapsch referral incl feature to build, rework to use browser

--- bundles/at.medevit.elexis.kapsch.referral.ui/src/at/medevit/elexis/kapsch/referral/internal/KapschReferralServiceImpl.java
+++ bundles/at.medevit.elexis.kapsch.referral.ui/src/at/medevit/elexis/kapsch/referral/internal/KapschReferralServiceImpl.java
@@ -12,19 +12,39 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import org.apache.commons.lang.StringUtils;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.LoggerFactory;
 
 import at.medevit.elexis.kapsch.referral.KapschReferralService;
+import ch.elexis.core.constants.XidConstants;
+import ch.elexis.core.data.events.ElexisEventDispatcher;
+import ch.elexis.core.model.ch.BillingLaw;
 import ch.elexis.core.services.holder.ConfigServiceHolder;
+import ch.elexis.core.types.Gender;
+import ch.elexis.data.Fall;
+import ch.elexis.data.Kontakt;
 import ch.elexis.data.Patient;
 
 @Component(service = KapschReferralService.class)
 public class KapschReferralServiceImpl implements KapschReferralService {
 	
+	public Optional<String> getPatientReferralUrl(Patient patient){
+		URL url = getUrl();
+		Map<String, Object> params = getPatientParameterMap(patient);
+		if (params != null) {
+			String getParams = getMapAsGetParams(params);
+			if (getParams != null) {
+				LoggerFactory.getLogger(getClass())
+					.info("Referral URL " + url.toString() + "?" + getParams);
+				return Optional.of(url.toString() + "?" + getParams);
+			}
+		}
+		return Optional.empty();
+	}
+	
 	@Override
 	public Optional<String> sendPatient(Patient patient){
-		
         URL url = getUrl();
 		Map<String, Object> params = getPatientParameterMap(patient);
 		if (params != null) {
@@ -70,8 +90,26 @@ public class KapschReferralServiceImpl implements KapschReferralService {
 		}
 	}
 
+	private String getMapAsGetParams(Map<String, Object> params){
+		try {
+			StringBuilder postData = new StringBuilder();
+			for (Map.Entry<String, Object> param : params.entrySet()) {
+				if (postData.length() != 0)
+					postData.append('&');
+				postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+				postData.append('=');
+				postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+			}
+			return postData.toString();
+		} catch (UnsupportedEncodingException e) {
+			LoggerFactory.getLogger(getClass()).error("Error getting get params", e);
+			return null;
+		}
+	}
+	
 	private Map<String, Object> getPatientParameterMap(Patient patient){
 		Map<String, Object> params = new LinkedHashMap<>();
+		// patient data
 		params.put("PID", patient.getPatCode());
 		params.put("Lastname", patient.getName());
 		params.put("Firstname", patient.getVorname());
@@ -79,9 +117,61 @@ public class KapschReferralServiceImpl implements KapschReferralService {
 		params.put("PostalCode", patient.getAnschrift().getPlz());
 		params.put("City", patient.getAnschrift().getOrt());
 		params.put("Birthday", patient.getGeburtsdatum());
-		params.put("Sex", patient.getGeschlecht());
+		params.put("Sex", getSex(patient));
+		params.put("SSN", getSsn(patient));
+		params.put("eMail", patient.get(Patient.FLD_E_MAIL));
+		params.put("Phone", patient.get(Patient.FLD_PHONE1));
+		params.put("Mobile", patient.get(Patient.FLD_MOBILEPHONE));
+		
+		Fall fall = (Fall) ElexisEventDispatcher.getSelected(Fall.class);
+		if (fall != null) {
+			Kontakt garant = fall.getGarant();
+			if (garant != null) {
+				params.put("GuarantorName", garant.get(Kontakt.FLD_NAME1));
+				params.put("GuarantorStreet", garant.getAnschrift().getStrasse());
+				params.put("GuarantorPostalCode", garant.getAnschrift().getPlz());
+				params.put("GuarantorCity", garant.getAnschrift().getOrt());
+				params.put("GuarantorPhone", garant.get(Kontakt.FLD_PHONE1));
+				params.put("GuarantorGln", getGln(garant));
+				if (StringUtils.isNotEmpty((String) fall.getExtInfoStoredObjectByKey("VEKANr"))) {
+					params.put("InsuranceCardNr", (String) fall.getExtInfoStoredObjectByKey("VEKANr"));
+				}
+				if (StringUtils
+					.isNotEmpty((String) fall.getExtInfoStoredObjectByKey("VEKAValid"))) {
+					params.put("InsuranceCardExpiryDate",
+						(String) fall.getExtInfoStoredObjectByKey("VEKAValid"));
+				}
+				params.put("EntryReason", fall.getConfiguredBillingSystemLaw().name());
+				if(fall.getConfiguredBillingSystemLaw() == BillingLaw.UVG) {
+					if(StringUtils.isNotEmpty(fall.getRequiredString("Unfallnummer"))) {
+						params.put("AccidentNr", fall.getRequiredString("Unfallnummer"));
+					}
+					if (StringUtils.isNotEmpty(fall.getInfoString("Unfalldatum"))) {
+						params.put("DateOfAccident", fall.getRequiredString("Unfalldatum"));
+					}
+				}
+			}
+		}
 		
 		return params;
+	}
+	
+	private String getGln(Kontakt contact){
+		return contact.getXid(XidConstants.DOMAIN_EAN);
+	}
+	
+	private String getSsn(Patient patient){
+		return patient.getXid(XidConstants.DOMAIN_AHV);
+	}
+	
+	private String getSex(Patient patient){
+		Gender gender = patient.getGender();
+		if (gender == Gender.FEMALE) {
+			return "W";
+		} else if (gender == Gender.MALE) {
+			return "M";
+		}
+		return "U";
 	}
 	
 	private URL getUrl(){
--- bundles/at.medevit.elexis.kapsch.referral.ui/src/at/medevit/elexis/kapsch/referral/ui/handler/SendHandler.java
+++ bundles/at.medevit.elexis.kapsch.referral.ui/src/at/medevit/elexis/kapsch/referral/ui/handler/SendHandler.java
@@ -6,33 +6,21 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 import at.medevit.elexis.kapsch.referral.internal.KapschReferralServiceHolder;
-import ch.elexis.core.model.Identifiable;
+import ch.elexis.core.data.events.ElexisEventDispatcher;
 import ch.elexis.data.Patient;
 
 public class SendHandler extends AbstractHandler {
 	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException{
-		Patient pat = null;
-		IWorkbenchWindow iWorkbenchWindow = HandlerUtil.getActiveWorkbenchWindow(event);
-		if (iWorkbenchWindow != null) {
-			ISelection selection = iWorkbenchWindow.getActivePage().getSelection();
-			if (selection instanceof IStructuredSelection) {
-				IStructuredSelection strucSelection = (IStructuredSelection) selection;
-				pat = getAsPatient(strucSelection);
-			}
-		}
+		Patient pat = ElexisEventDispatcher.getSelectedPatient();
 		if (pat != null) {
 			// start sending patient data
-			Optional<String> url = KapschReferralServiceHolder.get().sendPatient(pat);
+			Optional<String> url = KapschReferralServiceHolder.get().getPatientReferralUrl(pat);
 			if (url.isPresent()) {
 				Program.launch(url.get());
 			} else {
@@ -41,15 +29,5 @@ public class SendHandler extends AbstractHandler {
 			}
 		}
 		return null;
-	}
-	
-	private Patient getAsPatient(IStructuredSelection strucSelection){
-		Patient ret = null;
-		if (strucSelection.getFirstElement() instanceof Identifiable) {
-			ret = Patient.load(((Identifiable) strucSelection.getFirstElement()).getId());
-		} else {
-			ret = (Patient) strucSelection.getFirstElement();
-		}
-		return ret;
 	}
 }

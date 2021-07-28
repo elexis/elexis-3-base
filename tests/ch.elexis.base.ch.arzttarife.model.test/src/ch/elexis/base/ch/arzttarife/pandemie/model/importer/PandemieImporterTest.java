01ba91d8202026a520a9af38ed847e67d3ce19d4
thomas <thomas@thomas-hpelitebook840g5>
2021-07-28 12:16:36 +0200

    [22528] update PandemieImporter to remove title line breaks, incl test

--- tests/ch.elexis.base.ch.arzttarife.model.test/src/ch/elexis/base/ch/arzttarife/pandemie/model/importer/PandemieImporterTest.java
+++ tests/ch.elexis.base.ch.arzttarife.model.test/src/ch/elexis/base/ch/arzttarife/pandemie/model/importer/PandemieImporterTest.java
@@ -1,7 +1,9 @@
 package ch.elexis.base.ch.arzttarife.pandemie.model.importer;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
@@ -24,7 +26,7 @@ public class PandemieImporterTest {
 		Status retStatus =
 			(Status) importer.performImport(new NullProgressMonitor(),
 				PandemieImporterTest.class
-					.getResourceAsStream("/rsc/pandemie_201102.xlsx"),
+					.getResourceAsStream("/rsc/PT_20210701.xlsx"),
 				null);
 		assertEquals(IStatus.OK, retStatus.getCode());
 		
@@ -35,6 +37,14 @@ public class PandemieImporterTest {
 		assertNotNull(pauschale);
 		assertNotNull(pauschale.getText());
 		assertEquals(2250, ((IPandemieLeistung) pauschale).getCents());
+		
+		ICodeElement mulitline =
+			codeElementService.loadFromString("Pandemie", "01.01.1300", null).get();
+		assertNotNull(mulitline);
+		assertNotNull(mulitline.getText());
+		assertTrue(mulitline.getText().startsWith(
+			"Immunologische Analyse auf Sars-CoV-2 Antigene und Schnelltest zum direkten Nachweis von Sars-CoV-2"));
+		assertFalse(mulitline.getText().contains("\n"));
 		OsgiServiceUtil.ungetService(codeElementService);
 	}
 }

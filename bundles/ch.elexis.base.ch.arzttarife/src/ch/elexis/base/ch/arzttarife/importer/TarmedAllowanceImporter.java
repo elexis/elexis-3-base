b2bd53a0653a9de4e1a25760cafa104a03abb18d
thomas <thomas@thomas-hpelitebook840g5>
2021-08-24 11:05:59 +0200

    [22260] add tarmedallowance ui

---
+++ bundles/ch.elexis.base.ch.arzttarife/src/ch/elexis/base/ch/arzttarife/importer/TarmedAllowanceImporter.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2020, G. Weirich and Elexis
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    T. Huster - initial implementation
+ *    
+ *******************************************************************************/
+package ch.elexis.base.ch.arzttarife.importer;
+
+import java.io.FileInputStream;
+
+import javax.inject.Inject;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.swt.widgets.Composite;
+
+import ch.elexis.core.interfaces.IReferenceDataImporter;
+import ch.elexis.core.services.IReferenceDataImporterService;
+import ch.elexis.core.ui.util.CoreUiUtil;
+import ch.elexis.core.ui.util.ImporterPage;
+
+public class TarmedAllowanceImporter extends ImporterPage {
+	
+	@Inject
+	private IReferenceDataImporterService importerService;
+	
+	public TarmedAllowanceImporter(){
+		CoreUiUtil.injectServicesWithContext(this);
+	}
+	
+	@Override
+	public Composite createPage(Composite parent){
+		return new FileBasedImporter(parent, this);
+	}
+	
+	@Override
+	public IStatus doImport(IProgressMonitor monitor) throws Exception{
+		IReferenceDataImporter importer = importerService.getImporter("tarmedallowance")
+			.orElseThrow(() -> new IllegalStateException("No IReferenceDataImporter available."));
+		return importer.performImport(monitor, new FileInputStream(results[0]), null);
+	}
+	
+	@Override
+	public String getDescription(){
+		return "Tarmedpauschalen";
+	}
+	
+	@Override
+	public String getTitle(){
+		return "Tarmedpauschalen";
+	}
+}
---
+++ bundles/ch.elexis.base.ch.arzttarife/src/ch/elexis/views/TarmedAllowanceContentProvider.java
@@ -0,0 +1,57 @@
+package ch.elexis.views;
+
+import java.util.List;
+
+import ch.elexis.base.ch.arzttarife.service.ArzttarifeModelServiceHolder;
+import ch.elexis.base.ch.arzttarife.tarmedallowance.ITarmedAllowance;
+import ch.elexis.core.model.IEncounter;
+import ch.elexis.core.services.IQuery;
+import ch.elexis.core.services.IQuery.COMPARATOR;
+import ch.elexis.core.services.IQuery.ORDER;
+import ch.elexis.core.services.holder.ContextServiceHolder;
+import ch.elexis.core.ui.util.viewers.CommonViewer;
+import ch.elexis.core.ui.util.viewers.CommonViewerContentProvider;
+import ch.elexis.core.ui.util.viewers.ViewerConfigurer.ControlFieldProvider;
+
+public class TarmedAllowanceContentProvider extends CommonViewerContentProvider {
+	
+	
+	private ControlFieldProvider controlFieldProvider;
+	
+	public TarmedAllowanceContentProvider(CommonViewer commonViewer,
+		ControlFieldProvider controlFieldProvider){
+		super(commonViewer);
+		this.controlFieldProvider = controlFieldProvider;
+	}
+	
+	@Override
+	public Object[] getElements(Object arg0){
+		IQuery<?> query = getBaseQuery();
+		
+		java.util.Optional<IEncounter> encounter =
+			ContextServiceHolder.get().getTyped(IEncounter.class);
+		encounter.ifPresent(e -> {
+			query.and("validFrom", COMPARATOR.LESS_OR_EQUAL, e.getDate());
+			query.startGroup();
+			query.or("validTo", COMPARATOR.GREATER_OR_EQUAL, e.getDate());
+			query.or("validTo", COMPARATOR.EQUALS, null);
+			query.andJoinGroups();
+		});
+		
+		// apply filters from control field provider
+		controlFieldProvider.setQuery(query);
+		applyQueryFilters(query);
+		query.orderBy("code", ORDER.ASC);
+		List<?> elements = query.execute();
+		
+		return elements.toArray(new Object[elements.size()]);
+	}
+	
+	@Override
+	protected IQuery<?> getBaseQuery(){
+		IQuery<ITarmedAllowance> query =
+			ArzttarifeModelServiceHolder.get().getQuery(ITarmedAllowance.class);
+		query.and("id", COMPARATOR.NOT_EQUALS, "VERSION");
+		return query;
+	}
+}

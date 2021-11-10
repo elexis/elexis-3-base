0a93c377069f752efe38805dd3d4c54c11af0df2
thomas <huster@medevit.at>
2021-11-10 17:00:43 +0100

    [22410] add testDate to corona123 test request url parameters

--- bundles/at.medevit.elexis.corona123/src/at/medevit/elexis/corona123/handler/OpenPatientTestHandler.java
+++ bundles/at.medevit.elexis.corona123/src/at/medevit/elexis/corona123/handler/OpenPatientTestHandler.java
@@ -1,5 +1,10 @@
 package at.medevit.elexis.corona123.handler;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -9,6 +14,7 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.slf4j.LoggerFactory;
 
 import ch.elexis.core.model.IPatient;
 
@@ -32,8 +38,15 @@ public class OpenPatientTestHandler extends AbstractHandler implements IHandler 
 			String baseUrl = UrlBuilder.getTestBaseUrl();
 			
 			String patientParameters = UrlBuilder.getPatientParameters(patient);
-			
-			Program.launch(baseUrl + "?" + patientParameters);
+			try {
+				String testDateParameter = "testDate=" + URLEncoder.encode(
+					DateTimeFormatter.ofPattern("yyyy-MM-dd").format(LocalDate.now()), "UTF-8");
+				
+				Program.launch(baseUrl + "?" + testDateParameter + "&" + patientParameters);
+			} catch (UnsupportedEncodingException e) {
+				LoggerFactory.getLogger(UrlBuilder.class).error("Error getting testDate parameter",
+					e);
+			}
 		} else {
 			MessageDialog.openError(Display.getDefault().getActiveShell(), "Fehler",
 				"Es ist keine corona123 Organisations ID konfiguriert.");

d008744ff81cb8253718862d7a3a71e858eaaac1
thomas <huster@medevit.at>
2021-12-01 19:50:42 +0100

    [23459] set media print attribute media size with custom name

--- bundles/ch.itmed.fop.printing/src/ch/itmed/fop/printing/print/PrintProvider.java
+++ bundles/ch.itmed.fop.printing/src/ch/itmed/fop/printing/print/PrintProvider.java
@@ -11,16 +11,15 @@
 
 package ch.itmed.fop.printing.print;
 
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.print.Doc;
 import javax.print.DocFlavor;
@@ -29,8 +28,12 @@ import javax.print.PrintException;
 import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
 import javax.print.SimpleDoc;
-import javax.print.attribute.standard.MediaPrintableArea;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.Size2DSyntax;
 import javax.print.attribute.standard.MediaSize;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
@@ -38,29 +41,37 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.fop.apps.FOPException;
 import org.apache.fop.apps.FOUserAgent;
 import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.MimeConstants;
 import org.apache.fop.render.print.PageableRenderer;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.rendering.ImageType;
-import org.apache.pdfbox.rendering.PDFRenderer;
-import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
 
 import ch.elexis.core.utils.CoreUtil;
 import ch.itmed.fop.printing.xml.documents.FoTransformer;
 
 public final class PrintProvider {
 	private static Logger logger = LoggerFactory.getLogger(PrintProvider.class);
-
-	private static DocPrintJob createDocPrintJob(String printerName) {
-		PrintService[] services = PrintServiceLookup.lookupPrintServices(DocFlavor.SERVICE_FORMATTED.PAGEABLE, null);
-
+	
+	private static Map<String, CustomMediaSizeName> customMediaSizeMap = new HashMap<>();
+	private static Integer customMediaSizeNumber = 1000;
+	
+	private static DocPrintJob createDocPrintJob(String printerName){
+		PrintService[] services =
+			PrintServiceLookup.lookupPrintServices(DocFlavor.SERVICE_FORMATTED.PAGEABLE, null);
+		
 		for (PrintService printer : services) {
 			if (printer.getName().equals(printerName)) {
 				return printer.createPrintJob();
@@ -68,7 +79,7 @@ public final class PrintProvider {
 		}
 		return null;
 	}
-
+	
 	/**
 	 * Prints the documents.
 	 * 
@@ -80,57 +91,100 @@ public final class PrintProvider {
 	 * @throws PrintException
 	 */
 	public static void print(InputStream foStream, String printerName)
-			throws IOException, FOPException, TransformerException, PrintException {
+		throws IOException, FOPException, TransformerException, PrintException{
 		// always try to use printing via formats before using javax renderer
 		if (createDocFlavorPrintJob(printerName, DocFlavor.INPUT_STREAM.PDF) != null) {
 			logger.info("Using fo pdf printing with printer [" + printerName + "]");
 			printWithOutputFormatAndDocFlavor(foStream, printerName, MimeConstants.MIME_PDF,
-				DocFlavor.INPUT_STREAM.PDF);
+				DocFlavor.INPUT_STREAM.PDF, null);
 			return;
 		} else if (createDocFlavorPrintJob(printerName,
 			DocFlavor.INPUT_STREAM.POSTSCRIPT) != null) {
 			logger.info("Using fo postscript printing with printer [" + printerName + "]");
 			printWithOutputFormatAndDocFlavor(foStream, printerName, MimeConstants.MIME_POSTSCRIPT,
-				DocFlavor.INPUT_STREAM.POSTSCRIPT);
-			return;
-		} else if (createDocFlavorPrintJob(printerName, DocFlavor.INPUT_STREAM.PNG) != null) {
-			logger.info("Using fo png printing with printer [" + printerName + "]");
-			printWithOutputFormatAndDocFlavor(foStream, printerName, MimeConstants.MIME_PNG,
-				DocFlavor.INPUT_STREAM.PNG);
+				DocFlavor.INPUT_STREAM.POSTSCRIPT, null);
 			return;
 		}
 		logger.info("Using default fo javax printing with printer [" + printerName + "]");
+		CustomMediaSizeName mediaSize = getPageInformation(foStream);
+		PrintRequestAttributeSet printRequestAttributes = getPrintRequestAttributes(mediaSize);
 		
 		// Make sure that the position of the marker is not at the end of stream
 		foStream.reset();
-
+		
 		FopFactory fopFactory = FopFactory.newInstance(new File(".").toURI());
 		DocPrintJob printJob = createDocPrintJob(printerName);
-		logPrinterDebugInfo(printJob, DocFlavor.SERVICE_FORMATTED.PAGEABLE);
+		logPrinterDebugInfo(printJob);
 		FOUserAgent userAgent = fopFactory.newFOUserAgent();
 		PageableRenderer renderer = new PageableRenderer(userAgent);
-
+		
 		userAgent.setRendererOverride(renderer);
 		// Construct FOP with desired output format
 		Fop fop = fopFactory.newFop(userAgent);
-
+		
 		// Setup JAXP using identity transformer
 		TransformerFactory factory = TransformerFactory.newInstance();
 		// identity transformer
 		Transformer transformer = factory.newTransformer();
-
+		
 		// Setup input stream
 		Source src = new StreamSource(foStream);
-
+		
 		// Resulting SAX events (the generated FO) must be piped through to FOP
 		Result res = new SAXResult(fop.getDefaultHandler());
-
+		
 		// Start XSLT transformation and FOP processing
 		transformer.transform(src, res);
 		
 		Doc doc = new SimpleDoc(renderer, DocFlavor.SERVICE_FORMATTED.PAGEABLE, null);
-		printJob.print(doc, null);
+		printJob.print(doc, printRequestAttributes);
 		logger.info("Print job sent to printer: " + printerName);
+	}
+	
+	private static CustomMediaSizeName getPageInformation(InputStream foStream) throws IOException{
+		ByteArrayOutputStream copy = new ByteArrayOutputStream();
+		IOUtils.copy(foStream, copy);
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse(new ByteArrayInputStream(copy.toByteArray()));
+			XPathFactory xpathfactory = XPathFactory.newInstance();
+			XPath xpath = xpathfactory.newXPath();
+			XPathExpression expr = xpath.compile("//root/layout-master-set/simple-page-master");
+			Object result = expr.evaluate(doc, XPathConstants.NODESET);
+			NodeList nodes = (NodeList) result;
+			if (nodes.getLength() > 0) {
+				NamedNodeMap attributes = nodes.item(0).getAttributes();
+				if (attributes.getNamedItem("page-width") != null
+					&& attributes.getNamedItem("page-height") != null) {
+					Integer width = Integer.parseInt(attributes.getNamedItem("page-width")
+						.getNodeValue().replaceAll("[^0-9]", ""));
+					Integer height = Integer.parseInt(attributes.getNamedItem("page-height")
+						.getNodeValue().replaceAll("[^0-9]", ""));
+					if (customMediaSizeMap.get(width + "x" + height) == null) {
+						CustomMediaSizeName customMediaSize =
+							new CustomMediaSizeName(customMediaSizeNumber++);
+						customMediaSizeMap.put(width + "x" + height, customMediaSize);
+						new MediaSize(width, height, Size2DSyntax.MM, customMediaSize);
+					}
+					return customMediaSizeMap.get(width + "x" + height);
+				}
+			}
+		} catch (Exception e) {
+			logger.error("Error parsing page infomation", e);
+		}
+		return null;
+	}
+	
+	private static PrintRequestAttributeSet getPrintRequestAttributes(
+		CustomMediaSizeName mediaSize){
+		HashPrintRequestAttributeSet ret = new HashPrintRequestAttributeSet();
+		ret.add(mediaSize);
+		if (System.getProperty(FoTransformer.DEBUG_MODE) != null) {
+			logger.info("mediasize attribute [" + mediaSize + "] ["
+				+ MediaSize.getMediaSizeForName(mediaSize) + "]");
+		}
+		return ret;
 	}
 	
 	private static DocPrintJob createDocFlavorPrintJob(String printerName, DocFlavor docFlavor){
@@ -146,47 +200,18 @@ public final class PrintProvider {
 		return null;
 	}
 	
-	private static void logPrinterDebugInfo(DocPrintJob printJob, DocFlavor printingDocFlavor){
+	private static void logPrinterDebugInfo(DocPrintJob printJob){
 		if (System.getProperty(FoTransformer.DEBUG_MODE) != null) {
 			logger.info("Printer [" + printJob.getPrintService().getName() + "]");
 			DocFlavor[] supportedFlavors = printJob.getPrintService().getSupportedDocFlavors();
 			Arrays.asList(supportedFlavors).forEach(df -> logger.info("docflavor [" + df + "]"));
-			if (printingDocFlavor == DocFlavor.INPUT_STREAM.PNG) {
-				Object supported = printJob.getPrintService()
-					.getSupportedAttributeValues(MediaSize.class, printingDocFlavor, null);
-				if (supported != null) {
-					if (supported.getClass().isArray()) {
-						Object[] array = (Object[]) supported;
-						for (Object object : array) {
-							logger.info("attr mediasize [" + object + "]");
-						}
-					} else {
-						logger.info("attr mediasize [" + supported + "]");
-					}
-				}
-				supported = printJob.getPrintService()
-					.getSupportedAttributeValues(MediaPrintableArea.class, printingDocFlavor, null);
-				if (supported != null) {
-					if (supported.getClass().isArray()) {
-						Object[] array = (Object[]) supported;
-						for (Object object : array) {
-							logger.info("attr mediaprintablearea [" + object + "]");
-						}
-					} else {
-						logger.info("attr mediaprintablearea [" + supported + "]");
-					}
-				}
-			}
 		}
 	}
 	
 	private static void printWithOutputFormatAndDocFlavor(InputStream foStream, String printerName,
-		String outputFormat, DocFlavor printingDocFlavor)
+		String outputFormat, DocFlavor printingDocFlavor,
+		PrintRequestAttributeSet printRequestAttributes)
 		throws IOException, FOPException, TransformerException, PrintException{
-		boolean usePng = printingDocFlavor == DocFlavor.INPUT_STREAM.PNG;
-		if (usePng) {
-			outputFormat = MimeConstants.MIME_PDF;
-		}
 		// Make sure that the position of the marker is not at the end of stream
 		foStream.reset();
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -218,56 +243,23 @@ public final class PrintProvider {
 					.error("Could not write medi-print debug pdf", e);
 			}
 		}
-		if (usePng) {
-			List<ByteArrayOutputStream> outputStreams = convertPdfToPng(outputStream);
-			for (ByteArrayOutputStream os : outputStreams) {
-				print(os, printerName, printingDocFlavor);
-			}
-		} else {
-			print(outputStream, printerName, printingDocFlavor);
-		}
+		print(outputStream, printerName, printingDocFlavor, printRequestAttributes);
 	}
 	
 	private static void print(ByteArrayOutputStream outputStream, String printerName,
-		DocFlavor printingDocFlavor) throws PrintException{
+		DocFlavor printingDocFlavor, PrintRequestAttributeSet printRequestAttributes)
+		throws PrintException{
 		DocPrintJob printJob = createDocFlavorPrintJob(printerName, printingDocFlavor);
 		if (printJob != null) {
-			logPrinterDebugInfo(printJob, printingDocFlavor);
+			logPrinterDebugInfo(printJob);
 			if (printJob.getPrintService().isDocFlavorSupported(printingDocFlavor)) {
 				Doc doc = new SimpleDoc(new ByteArrayInputStream(outputStream.toByteArray()),
 					printingDocFlavor, null);
-				printJob.print(doc, null);
+				printJob.print(doc, printRequestAttributes);
 			} else {
 				logger.error(
 					"Could not print on [" + printerName + "] with [" + printingDocFlavor + "]");
 			}
 		}
-	}
-	
-	private static List<ByteArrayOutputStream> convertPdfToPng(ByteArrayOutputStream outputStream)
-		throws IOException{
-		List<ByteArrayOutputStream> ret = new ArrayList<>();
-		PDDocument document = PDDocument.load(new ByteArrayInputStream(outputStream.toByteArray()));
-		PDFRenderer pdfRenderer = new PDFRenderer(document);
-		for (int page = 0; page < document.getNumberOfPages(); ++page) {
-			BufferedImage renderedImage = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
-			
-			//			Image resultingImage = renderedImage.getScaledInstance(400, 400, Image.SCALE_DEFAULT);
-			//			BufferedImage outputImage = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB);
-			//			outputImage.getGraphics().drawImage(resultingImage, 0, 0, null);
-			
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			ImageIOUtil.writeImage(renderedImage, "PNG", out, 300);
-			if (System.getProperty(FoTransformer.DEBUG_MODE) != null) {
-				File userDir = CoreUtil.getWritableUserDir();
-				File pngOut = new File(userDir, "medi-print_debug.png");
-				try (FileOutputStream fo = new FileOutputStream(pngOut)) {
-					ImageIOUtil.writeImage(renderedImage, "PNG", fo, 300);
-				}
-			}
-			ret.add(out);
-		}
-		document.close();
-		return ret;
 	}
 }

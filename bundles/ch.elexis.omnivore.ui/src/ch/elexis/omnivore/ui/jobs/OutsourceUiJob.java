8e9ca5624e2d3d463237856df828b9130e3e4bb9
thomas <huster@medevit.at>
2022-05-12 09:37:15 +0200

    [24106] use query cursor for omnivore OutsourceUiJob

--- bundles/ch.elexis.omnivore.ui/src/ch/elexis/omnivore/ui/jobs/OutsourceUiJob.java
+++ bundles/ch.elexis.omnivore.ui/src/ch/elexis/omnivore/ui/jobs/OutsourceUiJob.java
@@ -1,7 +1,6 @@
 package ch.elexis.omnivore.ui.jobs;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
@@ -9,6 +8,7 @@ import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.widgets.Shell;
 
 import ch.elexis.core.services.IQuery;
+import ch.elexis.core.services.IQueryCursor;
 import ch.elexis.core.ui.util.SWTHelper;
 import ch.elexis.omnivore.data.Messages;
 import ch.elexis.omnivore.model.IDocumentHandle;
@@ -25,21 +25,23 @@ public class OutsourceUiJob {
 
 				@Override
 				public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-
 					IQuery<IDocumentHandle> qDoc = OmnivoreModelServiceHolder.get().getQuery(IDocumentHandle.class);
-					List<IDocumentHandle> lDocs = qDoc.execute();
 
-					monitor.beginTask("Dateien werden ausgelagert...", lDocs.size());
+					try (IQueryCursor<IDocumentHandle> docs = qDoc.executeAsCursor()) {
+						monitor.beginTask("Dateien werden ausgelagert...", docs.size());
 
-					for (IDocumentHandle docHandle : lDocs) {
-						if (monitor.isCanceled())
-							return;
-						monitor.subTask("Datei: " + docHandle.getTitle());
-						if (!docHandle.exportToFileSystem()) {
-							SWTHelper.showError(Messages.DocHandle_writeErrorCaption2,
-									Messages.DocHandle_writeErrorCaption2, "Fehlerdetails siehe Logdatei");
+						while (docs.hasNext()) {
+							IDocumentHandle docHandle = docs.next();
+
+							if (monitor.isCanceled())
+								return;
+							monitor.subTask("Datei: " + docHandle.getTitle());
+							if (!docHandle.exportToFileSystem()) {
+								SWTHelper.showError(Messages.DocHandle_writeErrorCaption2,
+										Messages.DocHandle_writeErrorCaption2, "Fehlerdetails siehe Logdatei");
+							}
+							monitor.worked(1);
 						}
-						monitor.worked(1);
 					}
 					monitor.done();
 				}

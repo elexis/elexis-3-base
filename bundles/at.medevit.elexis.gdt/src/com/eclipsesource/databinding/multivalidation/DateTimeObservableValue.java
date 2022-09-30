d053e936bfb533c6bc9b26fd815fde5d84a59821
Thomas Huster <huster@medevit.at>
2022-09-30 11:32:54 +0200

    [24582] fix setting date of DateTime widget in gdt custom observable

--- bundles/at.medevit.elexis.gdt/src/com/eclipsesource/databinding/multivalidation/DateTimeObservableValue.java
+++ bundles/at.medevit.elexis.gdt/src/com/eclipsesource/databinding/multivalidation/DateTimeObservableValue.java
@@ -73,9 +73,8 @@ public class DateTimeObservableValue extends AbstractObservableValue {
 		if (!this.dateTime.isDisposed()) {
 			Calendar cal = Calendar.getInstance();
 			cal.setTime(date);
-			this.dateTime.setYear(cal.get(Calendar.YEAR));
-			this.dateTime.setMonth(cal.get(Calendar.MONTH));
-			this.dateTime.setDay(cal.get(Calendar.DAY_OF_MONTH));
+			this.dateTime.setDate(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+
 			this.dateTime.setHours(cal.get(Calendar.HOUR_OF_DAY));
 			this.dateTime.setMinutes(cal.get(Calendar.MINUTE));
 			this.dateTime.setSeconds(cal.get(Calendar.SECOND));

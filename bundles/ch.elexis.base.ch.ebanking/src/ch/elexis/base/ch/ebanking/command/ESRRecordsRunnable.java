d4a670e756d956326c502490e57e6e75635d7099
Thomas Huster <huster@medevit.at>
2022-11-03 16:44:53 +0100

    [24079] read esr files from zip, update esr reading progress

---
+++ bundles/ch.elexis.base.ch.ebanking/src/ch/elexis/base/ch/ebanking/command/ESRRecordsRunnable.java
@@ -0,0 +1,115 @@
+package ch.elexis.base.ch.ebanking.command;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.elexis.base.ch.ebanking.esr.ESR;
+import ch.elexis.base.ch.ebanking.esr.ESRRecord;
+import ch.elexis.base.ch.ebanking.esr.Messages;
+import ch.elexis.core.ui.util.SWTHelper;
+import ch.elexis.data.AccountTransaction;
+import ch.elexis.data.Rechnung;
+import ch.elexis.data.RnStatus;
+import ch.elexis.data.Zahlung;
+import ch.rgw.tools.Money;
+import ch.rgw.tools.TimeTool;
+
+public class ESRRecordsRunnable implements Runnable {
+
+	private IProgressMonitor monitor;
+	private List<ESRRecord> records;
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	public ESRRecordsRunnable(IProgressMonitor monitor, List<ESRRecord> records) {
+		if (monitor == null) {
+			this.monitor = new NullProgressMonitor();
+		} else {
+			this.monitor = monitor;
+		}
+		this.records = records;
+	}
+
+	@Override
+	public void run() {
+		boolean skipPaidAll = false;
+		boolean bookPaidAll = false;
+		for (ESRRecord rec : records) {
+			monitor.worked(1);
+			if (rec.getRejectCode().equals(ESRRecord.REJECT.OK)) {
+				if (rec.getTyp().equals(ESRRecord.MODE.Summenrecord)) {
+					log.info(Messages.ESRView_ESR_finished + rec.getBetrag());
+				} else if ((rec.getTyp().equals(ESRRecord.MODE.Storno_edv))
+						|| (rec.getTyp().equals(ESRRecord.MODE.Storno_Schalter))) {
+					Rechnung rn = rec.getRechnung();
+					Money zahlung = rec.getBetrag().negate();
+					Zahlung zahlungsObj = rn.addZahlung(zahlung, Messages.ESRView_storno_for + rn.getNr() + " / " //$NON-NLS-1$
+							+ rec.getPatient().getPatCode(), new TimeTool(rec.getValuta()));
+					if (zahlungsObj != null && ESR.getAccount() != null) {
+						AccountTransaction transaction = zahlungsObj.getTransaction();
+						transaction.setAccount(ESR.getAccount());
+					}
+					rec.setGebucht(null);
+				} else {
+					Rechnung rn = rec.getRechnung();
+					if (rn.getStatus() == RnStatus.BEZAHLT) {
+						if (skipPaidAll) {
+							continue;
+						}
+						if (!bookPaidAll) {
+							int ret = SWTHelper.ask(Messages.ESRView_paid,
+									Messages.ESRView_rechnung + rn.getNr() + Messages.ESRView_ispaid,
+									IDialogConstants.OK_LABEL, IDialogConstants.CANCEL_LABEL,
+									IDialogConstants.OK_LABEL + " für Alle",
+									IDialogConstants.CANCEL_LABEL + " für Alle");
+							if (ret == 3) {
+								skipPaidAll = true;
+							} else if (ret == 2) {
+								bookPaidAll = true;
+							}
+							if (ret == 1 || skipPaidAll) {
+								continue;
+							}
+						}
+					}
+					if (rn.getStatus() == RnStatus.IN_BETREIBUNG) {
+						if (SWTHelper.askYesNo(Messages.ESRView_compulsoryExecution, Messages.ESRView_rechnung
+								+ rn.getNr() + Messages.ESRView_isInCompulsoryExecution) == false) {
+							continue;
+						}
+					}
+
+					Money zahlung = rec.getBetrag();
+					Money offen = rn.getOffenerBetrag();
+					if (zahlung.isMoreThan(offen) && (zahlung.doubleValue() - offen.doubleValue() > 0.03)) {
+						if (SWTHelper.askYesNo(Messages.ESRView_toohigh,
+								Messages.ESRView_paymentfor + rn.getNr() + Messages.ESRView_morethan) == false) {
+							continue;
+						}
+					}
+
+					Zahlung zahlungsObj = rn.addZahlung(zahlung, Messages.ESRView_vesrfor + rn.getNr() + " / " //$NON-NLS-1$
+							+ rec.getPatient().getPatCode(), new TimeTool(rec.getValuta()));
+					if (zahlungsObj != null && ESR.getAccount() != null) {
+						AccountTransaction transaction = zahlungsObj.getTransaction();
+						transaction.setAccount(ESR.getAccount());
+					}
+					rec.setGebucht(null);
+				}
+			} else if (rec.getRejectCode().equals(ESRRecord.REJECT.RN_NUMMER)) {
+				TimeTool valutaDate = new TimeTool(rec.getValuta());
+				AccountTransaction transaction = new AccountTransaction(rec.getPatient(), null, rec.getBetrag(),
+						valutaDate.toString(TimeTool.DATE_GER), Messages.LoadESRFileHandler_notAssignable);
+				if (ESR.getAccount() != null) {
+					transaction.setAccount(ESR.getAccount());
+				}
+			}
+		}
+		monitor.done();
+	}
+}
--- bundles/ch.elexis.base.ch.ebanking/src/ch/elexis/base/ch/ebanking/command/LoadESRFileHandler.java
+++ bundles/ch.elexis.base.ch.ebanking/src/ch/elexis/base/ch/ebanking/command/LoadESRFileHandler.java
@@ -1,15 +1,20 @@
 package ch.elexis.base.ch.ebanking.command;
 
 import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
+import org.apache.commons.io.FilenameUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
@@ -19,11 +24,8 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.commands.IElementUpdater;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.menus.UIElement;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import ch.elexis.base.ch.ebanking.ESRView;
-import ch.elexis.base.ch.ebanking.esr.ESR;
 import ch.elexis.base.ch.ebanking.esr.ESRFile;
 import ch.elexis.base.ch.ebanking.esr.ESRRecord;
 import ch.elexis.base.ch.ebanking.esr.Messages;
@@ -31,124 +33,77 @@ import ch.elexis.core.data.util.ResultAdapter;
 import ch.elexis.core.ui.UiDesk;
 import ch.elexis.core.ui.icons.Images;
 import ch.elexis.core.ui.util.SWTHelper;
-import ch.elexis.data.AccountTransaction;
-import ch.elexis.data.Rechnung;
-import ch.elexis.data.RnStatus;
-import ch.elexis.data.Zahlung;
+import ch.elexis.core.utils.CoreUtil;
+import ch.rgw.io.FileTool;
 import ch.rgw.tools.ExHandler;
-import ch.rgw.tools.Money;
 import ch.rgw.tools.Result;
-import ch.rgw.tools.TimeTool;
 
 public class LoadESRFileHandler extends AbstractHandler implements IElementUpdater {
 
 	public static final String COMMAND_ID = "ch.elexis.ebanking_ch.command.loadESRFile";
 
-	private Logger log = LoggerFactory.getLogger(this.getClass());
-
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		FileDialog fld = new FileDialog(PlatformUI.getWorkbench().getDisplay().getActiveShell(), SWT.OPEN);
 		fld.setText(Messages.ESRView_selectESR);
+		fld.setFilterExtensions(new String[] { "*.xml;*.zip" });
 		final String filename = fld.open();
 		if (filename != null) {
-			final ESRFile esrf = new ESRFile();
 			final File file = new File(filename);
 			try {
 				PlatformUI.getWorkbench().getProgressService().busyCursorWhile(new IRunnableWithProgress() {
 
 					public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-						monitor.beginTask(Messages.ESRView_reading_ESR, (int) (file.length() / 25));
-						Result<List<ESRRecord>> result = esrf.read(file, monitor);
-						if (result.isOK()) {
-							boolean skipPaidAll = false;
-							boolean bookPaidAll = false;
-							for (ESRRecord rec : result.get()) {
-								monitor.worked(1);
-								if (rec.getRejectCode().equals(ESRRecord.REJECT.OK)) {
-									if (rec.getTyp().equals(ESRRecord.MODE.Summenrecord)) {
-										log.info(Messages.ESRView_ESR_finished + rec.getBetrag());
-									} else if ((rec.getTyp().equals(ESRRecord.MODE.Storno_edv))
-											|| (rec.getTyp().equals(ESRRecord.MODE.Storno_Schalter))) {
-										Rechnung rn = rec.getRechnung();
-										Money zahlung = rec.getBetrag().negate();
-										Zahlung zahlungsObj = rn.addZahlung(zahlung,
-												Messages.ESRView_storno_for + rn.getNr() + " / " //$NON-NLS-1$
-														+ rec.getPatient().getPatCode(),
-												new TimeTool(rec.getValuta()));
-										if (zahlungsObj != null && ESR.getAccount() != null) {
-											AccountTransaction transaction = zahlungsObj.getTransaction();
-											transaction.setAccount(ESR.getAccount());
-										}
-										rec.setGebucht(null);
-									} else {
-										Rechnung rn = rec.getRechnung();
-										if (rn.getStatus() == RnStatus.BEZAHLT) {
-											if (skipPaidAll) {
-												continue;
-											}
-											if (!bookPaidAll) {
-												int ret = SWTHelper.ask(Messages.ESRView_paid,
-														Messages.ESRView_rechnung + rn.getNr()
-																+ Messages.ESRView_ispaid,
-														IDialogConstants.OK_LABEL, IDialogConstants.CANCEL_LABEL,
-														IDialogConstants.OK_LABEL + " für Alle",
-														IDialogConstants.CANCEL_LABEL + " für Alle");
-												if (ret == 3) {
-													skipPaidAll = true;
-												} else if (ret == 2) {
-													bookPaidAll = true;
+						File tmpDir = null;
+						List<File> esrFiles = new ArrayList<>();
+						try {
+							String extension = FilenameUtils.getExtension(file.getName());
+							if ("zip".equalsIgnoreCase(extension)) {
+								tmpDir = new File(CoreUtil.getTempDir(), "esr_zip_" + System.currentTimeMillis());
+								if (tmpDir.mkdirs()) {
+									try {
+										FileTool.unzip(file, tmpDir);
+										esrFiles.addAll(Arrays.asList(tmpDir.listFiles(new FilenameFilter() {
+											@Override
+											public boolean accept(File dir, String name) {
+												if (file.isFile()) {
+													String extension = FilenameUtils.getExtension(name);
+													return "xml".equalsIgnoreCase(extension);
 												}
-												if (ret == 1 || skipPaidAll) {
-													continue;
-												}
+												return false;
 											}
-										}
-										if (rn.getStatus() == RnStatus.IN_BETREIBUNG) {
-											if (SWTHelper.askYesNo(Messages.ESRView_compulsoryExecution,
-													Messages.ESRView_rechnung + rn.getNr()
-															+ Messages.ESRView_isInCompulsoryExecution) == false) {
-												continue;
-											}
-										}
-
-										Money zahlung = rec.getBetrag();
-										Money offen = rn.getOffenerBetrag();
-										if (zahlung.isMoreThan(offen)
-												&& (zahlung.doubleValue() - offen.doubleValue() > 0.03)) {
-											if (SWTHelper.askYesNo(Messages.ESRView_toohigh, Messages.ESRView_paymentfor
-													+ rn.getNr() + Messages.ESRView_morethan) == false) {
-												continue;
-											}
-										}
-
-										Zahlung zahlungsObj = rn.addZahlung(zahlung,
-												Messages.ESRView_vesrfor + rn.getNr() + " / " //$NON-NLS-1$
-														+ rec.getPatient().getPatCode(),
-												new TimeTool(rec.getValuta()));
-										if (zahlungsObj != null && ESR.getAccount() != null) {
-											AccountTransaction transaction = zahlungsObj.getTransaction();
-											transaction.setAccount(ESR.getAccount());
-										}
-										rec.setGebucht(null);
-									}
-								} else if (rec.getRejectCode().equals(ESRRecord.REJECT.RN_NUMMER)) {
-									TimeTool valutaDate = new TimeTool(rec.getValuta());
-									AccountTransaction transaction = new AccountTransaction(rec.getPatient(), null,
-											rec.getBetrag(), valutaDate.toString(TimeTool.DATE_GER),
-											Messages.LoadESRFileHandler_notAssignable);
-									if (ESR.getAccount() != null) {
-										transaction.setAccount(ESR.getAccount());
+										})));
+									} catch (IOException e) {
+										ExHandler.handle(e);
 									}
 								}
+							} else if ("xml".equalsIgnoreCase(extension)) {
+								esrFiles.add(file);
 							}
-							monitor.done();
-							updateEsrView(event);
-						} else {
-							ResultAdapter.displayResult(result, Messages.ESRView_errorESR);
+							for (File esrFile : esrFiles) {
+								monitor.beginTask(Messages.ESRView_reading_ESR + " " + esrFile.getName(), //$NON-NLS-1$
+										(int) (esrFile.length() / 25));
+								ESRFile esrf = new ESRFile();
+								Result<List<ESRRecord>> result = esrf.read(esrFile, monitor);
+								if (result.isOK()) {
+									CompletableFuture.runAsync(new ESRRecordsRunnable(monitor, result.get()))
+											.thenRunAsync(() -> updateEsrView(event));
+									if (monitor.isCanceled()) {
+										break;
+									}
+								} else {
+									ResultAdapter.displayResult(result, Messages.ESRView_errorESR);
+								}
+							}
+						} finally {
+							if (tmpDir != null) {
+								for (File file : tmpDir.listFiles()) {
+									file.delete();
+								}
+								tmpDir.delete();
+							}
 						}
 					}
-
 				});
 			} catch (InvocationTargetException e) {
 				ExHandler.handle(e);

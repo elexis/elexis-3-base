d043b91adee0093416f1eca07cec6501cd9bc921
alexandradenk <132701971+alexandradenk@users.noreply.github.com>
2023-08-08 15:10:07 +0200

    [25464] JodConverter service, set default URL (#271)

--- bundles/at.medevit.elexis.documents.converter/src/at/medevit/elexis/documents/converter/JodRestDocumentConverter.java
+++ bundles/at.medevit.elexis.documents.converter/src/at/medevit/elexis/documents/converter/JodRestDocumentConverter.java
@@ -90,7 +90,7 @@ public class JodRestDocumentConverter implements IDocumentConverter {
 	}
 
 	private String getAppBasePath() {
-		return configService.get("jodrestconverter/basepath", StringUtils.EMPTY);
+		return configService.get("jodrestconverter/basepath", "https://office.medelexis.ch/jodconverter/");
 	}
 
 	@Override

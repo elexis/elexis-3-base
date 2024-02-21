836f0bb564f6a5a534a358cf23c58777d8552c44
thuster <huster@medevit.at>
2024-02-21 12:09:43 +0100

    [26187] add old hl7 medics order, make new web order optional via pref

--- bundles/ch.elexis.laborimport.medics.v2/src/ch/elexis/labor/medics/v2/order/LabOrderAction.java
+++ bundles/ch.elexis.laborimport.medics.v2/src/ch/elexis/labor/medics/v2/order/LabOrderAction.java
@@ -2,38 +2,49 @@ package ch.elexis.labor.medics.v2.order;
 
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Shell;
 
 import ch.elexis.core.model.IPatient;
+import ch.elexis.core.services.holder.ConfigServiceHolder;
 import ch.elexis.core.services.holder.ContextServiceHolder;
 import ch.elexis.labor.medics.v2.MedicsActivator;
+import ch.elexis.labor.medics.v2.MedicsPreferencePage;
 import ch.elexis.labor.medics.v2.Messages;
 
 public class LabOrderAction extends Action {
+
+	private LabOrderActionHl7 labOrderHl7;
 
 	public LabOrderAction() {
 		setId("laborder"); //$NON-NLS-1$
 		setImageDescriptor(MedicsActivator.getImageDescriptor("rsc/medics16.png")); //$NON-NLS-1$
 		setText(Messages.LabOrderAction_nameAction);
+
+		labOrderHl7 = new LabOrderActionHl7();
 	}
 
 	@Override
 	public void run() {
-		Optional<IPatient> patient = ContextServiceHolder.get().getActivePatient();
-		if (patient.isPresent()) {
-			WebAis webAis = new WebAis();
-			String response = webAis.createPatientAndOrder(patient.get());
-			if (isUrl(response)) {
-				Program.launch(response);
-			} else {
-				MessageDialog.openError(new Shell(), Messages.LabOrderAction_errorTitleCannotShowURL, response);
-			}
+		if (StringUtils.isEmpty(ConfigServiceHolder.get().get(MedicsPreferencePage.CFG_MEDICS_ORDER_API, ""))) {
+			labOrderHl7.run();
 		} else {
-			MessageDialog.openError(new Shell(), Messages.LabOrderAction_errorTitleNoPatientSelected,
-					Messages.LabOrderAction_errorMessageNoPatientSelected);
+			Optional<IPatient> patient = ContextServiceHolder.get().getActivePatient();
+			if (patient.isPresent()) {
+				WebAis webAis = new WebAis();
+				String response = webAis.createPatientAndOrder(patient.get());
+				if (isUrl(response)) {
+					Program.launch(response);
+				} else {
+					MessageDialog.openError(new Shell(), Messages.LabOrderAction_errorTitleCannotShowURL, response);
+				}
+			} else {
+				MessageDialog.openError(new Shell(), Messages.LabOrderAction_errorTitleNoPatientSelected,
+						Messages.LabOrderAction_errorMessageNoPatientSelected);
+			}
 		}
 	}
 

26443a61aeab9cfc92e7b2cd67d91f79496c3ac1
Maximilian Kucher <maximilian.kucher@medicalvalues.de>
2024-03-07 09:52:47 +0100

    more formatting

--- bundles/ch.elexis.labororder.lg1_medicalvalues/src/ch/elexis/labororder/lg1_medicalvalues/order/LabOrderAction.java
+++ bundles/ch.elexis.labororder.lg1_medicalvalues/src/ch/elexis/labororder/lg1_medicalvalues/order/LabOrderAction.java
@@ -31,7 +31,7 @@ public class LabOrderAction extends Action {
 	public LabOrderAction() {
         setId("ch.elexis.laborder.lg1_medicalvalues.laborder"); //$NON-NLS-1$
 		setImageDescriptor(AbstractUIPlugin.imageDescriptorFromPlugin("ch.elexis.labororder.lg1_medicalvalues", //$NON-NLS-1$
-				"rsc/lg1_logo.png"));
+		        "rsc/lg1_logo.png"));
 		setText(Messages.LabOrderAction_nameAction);
 	}
 
@@ -45,24 +45,24 @@ public class LabOrderAction extends Action {
 			} catch (IOException e) {
 				LoggerFactory.getLogger(getClass()).error("Error contacting LG1 web service", e);
 				MessageDialog.openError(Display.getDefault().getActiveShell(), "Fehler",
-						"Es ist ein Fehler beim LG1 Aufruf aufgetreten.\n\n" + e.getLocalizedMessage());
+				        "Es ist ein Fehler beim LG1 Aufruf aufgetreten.\n\n" + e.getLocalizedMessage());
 			} catch (IllegalArgumentException e) {
 				LoggerFactory.getLogger(getClass()).error("Error calling medicalvalues order creation API", e);
 				MessageDialog.openError(Display.getDefault().getActiveShell(), "Fehler",
-						"Es fehlen zur Auftragserstellung benötigte Patientendaten.\n\n" + e.getLocalizedMessage());
+				        "Es fehlen zur Auftragserstellung benötigte Patientendaten.\n\n" + e.getLocalizedMessage());
 			} catch (URISyntaxException e) {
-                LoggerFactory.getLogger(getClass()).error("Error building medicalvalues order creation API URI", e);
-                MessageDialog.openError(Display.getDefault().getActiveShell(), "Fehler",
-                        "Es ist ein Fehler beim Erstellen der URL für die Auftragserstellung aufgetreten.\n\n" + e.getLocalizedMessage());
-            }
+                                LoggerFactory.getLogger(getClass()).error("Error building medicalvalues order creation API URI", e);
+                                MessageDialog.openError(Display.getDefault().getActiveShell(), "Fehler",
+                                        "Es ist ein Fehler beim Erstellen der URL für die Auftragserstellung aufgetreten.\n\n" + e.getLocalizedMessage());
+                        }
 		}
 	}
 
 	private URI buildOrderCreationUri(Patient patient) throws URISyntaxException {
 		URIBuilder builder = new URIBuilder("https://oe.lg1.lan/mdi/orders/importPatientAndCreateOrder");
 
-        ch.elexis.labororder.lg1_medicalvalues.order.model.Patient lg1Patient = ch.elexis.labororder.lg1_medicalvalues.order.model.Patient.of(patient);
-        lg1Patient.toMedicalvaluesOrderCreationAPIQueryParams(builder);
+                ch.elexis.labororder.lg1_medicalvalues.order.model.Patient lg1Patient = ch.elexis.labororder.lg1_medicalvalues.order.model.Patient.of(patient);
+                lg1Patient.toMedicalvaluesOrderCreationAPIQueryParams(builder);
 
 		return builder.build();
 	}

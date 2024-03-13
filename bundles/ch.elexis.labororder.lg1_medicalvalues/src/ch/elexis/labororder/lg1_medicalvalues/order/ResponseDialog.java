af7ab7c9a30c56d54b19f3c901e68806037b81d6
MKmedicalvalues <104839052+MKmedicalvalues@users.noreply.github.com>
2024-03-13 14:20:20 +0100

    [26233] LG1 laboratory order creation via the medicalvalues api (#317)

---
+++ bundles/ch.elexis.labororder.lg1_medicalvalues/src/ch/elexis/labororder/lg1_medicalvalues/order/ResponseDialog.java
@@ -0,0 +1,70 @@
+package ch.elexis.labororder.lg1_medicalvalues.order;
+
+import java.net.URI;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
+import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
+import org.slf4j.LoggerFactory;
+
+public class ResponseDialog extends Dialog {
+
+	private String responseText;
+	private Label title;
+	private Browser browser;
+
+	public ResponseDialog(String text, Shell shell) {
+		super(shell);
+		this.responseText = text;
+	}
+
+	@Override
+	protected Control createDialogArea(Composite container) {
+		Composite parent = (Composite) super.createDialogArea(container);
+
+		title = new Label(parent, SWT.NONE);
+		title.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+		if (StringUtils.isNotBlank(responseText)) {
+			title.setText("Es ist folgendes Problem aufgetreten");
+			browser = new Browser(parent, SWT.BORDER);
+			GridData gd = new GridData(SWT.FILL, SWT.FILL, true, true);
+			gd.widthHint = 600;
+			gd.heightHint = 600;
+			browser.setLayoutData(gd);
+			browser.setText(responseText);
+		}
+		return parent;
+	}
+
+	@Override
+	protected Control createButtonBar(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		GridData data = new GridData(GridData.HORIZONTAL_ALIGN_END | GridData.VERTICAL_ALIGN_CENTER);
+		data.exclude = true;
+		composite.setLayoutData(data);
+		return composite;
+	}
+
+	public static void openMedapp(String location) {
+		Display.getDefault().asyncExec(() -> {
+			try {
+				IWorkbenchBrowserSupport browserSupport = PlatformUI.getWorkbench().getBrowserSupport();
+				IWebBrowser externalBrowser = browserSupport.getExternalBrowser();
+				externalBrowser.openURL(new URI(location).toURL());
+			} catch (Exception ex) {
+				LoggerFactory.getLogger(ResponseDialog.class).error("Error open medapp url", ex);
+			}
+		});
+	}
+}

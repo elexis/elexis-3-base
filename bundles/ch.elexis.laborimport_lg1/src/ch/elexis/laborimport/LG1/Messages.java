87e9e21fff96849701fd7826d960e3c0c0b90b33
Victoria <Victoria@LAPTOP-LMK2LJMB>
2024-03-21 07:50:30 +0100

    [26226] setted the messages back to the ones from v3.8

--- bundles/ch.elexis.laborimport_lg1/src/ch/elexis/laborimport/LG1/Messages.java
+++ bundles/ch.elexis.laborimport_lg1/src/ch/elexis/laborimport/LG1/Messages.java
@@ -1,11 +1,34 @@
-// Copyright 2010 (c) Niklaus Giger <niklaus.giger@member.fsf.org>
-package ch.elexis.laborimport.LG1;
+/**********************************************************************
+ * Copyright (c) 2005 IBM Corporation and others. All rights reserved.   This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * IBM - Initial API and implementation
+ **********************************************************************/package ch.elexis.laborimport.LG1;
 
 import org.eclipse.osgi.util.NLS;
 
-public class Messages {
-	public static String PreferencePage_DownloadDir = ch.elexis.core.l10n.Messages.Core_Download_Directory;
-	public static String PreferencePage_JMedTrasferJar = ch.elexis.core.l10n.Messages.PreferencePage_JMedTrasferJar;
-	public static String PreferencePage_JMedTrasferJni = ch.elexis.core.l10n.Messages.PreferencePage_JMedTrasferJni;
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "ch.elexis.labororder.lg1.messages.messages";
+	public static String PreferencePage_JMedTrasferJni;
+	public static String PreferencePage_JMedTrasferJar;
+    public static String LabOrderAction_errorMessageNoFallSelected;
+    public static String LabOrderAction_errorMessageNoPatientSelected;
+    public static String LabOrderAction_errorTitleCannotCreateHL7;
+    public static String LabOrderAction_errorTitleCannotShowURL;
+    public static String LabOrderAction_errorTitleNoFallSelected;
+    public static String LabOrderAction_errorTitleNoPatientSelected;
+    public static String LabOrderAction_infoMessageLabOrderFinshed;
+    public static String LabOrderAction_infoTitleLabOrderFinshed;
+    public static String LabOrderAction_nameAction;
+    public static String LabOrderAction_receivingApplication;
+    public static String LabOrderAction_receivingFacility;
+	public static String Lg1PreferencePage_labelUploadDir;
+	public static String PreferencePage_DownloadDir;
 
+    static { // load message values from bundle file
+    NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+  }
 }

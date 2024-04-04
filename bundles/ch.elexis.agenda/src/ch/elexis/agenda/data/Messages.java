affc5a4ceb8ef5ffc8da1b2164e7d6a307ef5e59
Marco Descher <marco@descher.at>
2024-04-04 09:12:05 +0200

    [25802] ImporterPages remove Import_Agenda

--- bundles/ch.elexis.agenda/src/ch/elexis/agenda/data/Messages.java
+++ bundles/ch.elexis.agenda/src/ch/elexis/agenda/data/Messages.java
@@ -12,13 +12,6 @@
 package ch.elexis.agenda.data;
 
 public class Messages {
-	public static String Import_Agenda_cancelled = ch.elexis.core.l10n.Messages.Import_Agenda_cancelled;
-	public static String Import_Agenda_creatingTables = ch.elexis.core.l10n.Messages.Import_Agenda_creatingTables;
-	public static String Import_Agenda_errorsDuringImport = ch.elexis.core.l10n.Messages.Core_Error_while_importing;
-	public static String Import_Agenda_importFromJavaAgenda = ch.elexis.core.l10n.Messages.Import_Agenda_importFromJavaAgenda;
-	public static String Import_Agenda_importingAgenda = ch.elexis.core.l10n.Messages.Import_Agenda_importingAgenda;
-	public static String Import_Agenda_importingApps = ch.elexis.core.l10n.Messages.Import_Agenda_importingApps;
-	public static String Import_Agenda_importWasCancelled = ch.elexis.core.l10n.Messages.Import_Agenda_importWasCancelled;
 	public static String TagesNachricht_29 = ch.elexis.core.l10n.Messages.TagesNachricht_29;
 
 }

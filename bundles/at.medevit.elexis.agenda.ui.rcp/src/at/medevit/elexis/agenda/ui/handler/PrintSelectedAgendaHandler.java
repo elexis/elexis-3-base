51c71faf3629a969b0dce932834713360350da7d
Dalibor <Dalibor@192.168.1.110>
2024-06-03 15:22:24 +0200

    [26380] AgendaWeb PDF Druck in Agenda Farbe

--- bundles/at.medevit.elexis.agenda.ui.rcp/src/at/medevit/elexis/agenda/ui/handler/PrintSelectedAgendaHandler.java
+++ bundles/at.medevit.elexis.agenda.ui.rcp/src/at/medevit/elexis/agenda/ui/handler/PrintSelectedAgendaHandler.java
@@ -1,11 +1,8 @@
 package at.medevit.elexis.agenda.ui.handler;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -21,101 +18,104 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.ServiceReference;
 import org.slf4j.LoggerFactory;
 
 import at.medevit.elexis.agenda.ui.function.LoadEventsFunction;
+import at.medevit.elexis.agenda.ui.utils.PdfUtils;
 import at.medevit.elexis.agenda.ui.view.AgendaView;
 import at.medevit.elexis.agenda.ui.xml.AreaPeriodsLetter;
+import ch.elexis.agenda.preferences.PreferenceConstants;
 import ch.elexis.core.model.IAppointment;
 import ch.elexis.core.model.IPeriod;
-import ch.elexis.core.services.IFormattedOutput;
-import ch.elexis.core.services.IFormattedOutputFactory;
-import ch.elexis.core.services.IFormattedOutputFactory.ObjectType;
-import ch.elexis.core.services.IFormattedOutputFactory.OutputType;
+import ch.elexis.core.services.holder.ConfigServiceHolder;
+
 
 public class PrintSelectedAgendaHandler {
 
-	@Execute
-	public Object execute(MPart part, @Named(IServiceConstants.ACTIVE_SHELL) Shell shell) {
-		if (part.getObject() instanceof AgendaView) {
-			AgendaView agendaView = (AgendaView) part.getObject();
-			LoadEventsFunction loadEventsFunction = agendaView.getLoadEventsFunction();
+    @Execute
+    public Object execute(MPart part, @Named(IServiceConstants.ACTIVE_SHELL) Shell shell) {
+        if (part.getObject() instanceof AgendaView) {
+            AgendaView agendaView = (AgendaView) part.getObject();
+            LoadEventsFunction loadEventsFunction = agendaView.getLoadEventsFunction();
 
-			List<IPeriod> periods = loadEventsFunction.getCurrentPeriods();
-			Map<String, List<IPeriod>> areaPeriodMap = getAreaPeriodMap(periods);
+            List<IPeriod> periods = loadEventsFunction.getCurrentPeriods();
+            Map<String, List<IPeriod>> areaPeriodMap = getAreaPeriodMap(periods);
 
-			for (String area : areaPeriodMap.keySet()) {
-				AreaPeriodsLetter letter = AreaPeriodsLetter.of(area, areaPeriodMap.get(area));
-				if (letter != null) {
-					BundleContext bundleContext = FrameworkUtil.getBundle(getClass()).getBundleContext();
-					ServiceReference<IFormattedOutputFactory> serviceRef = bundleContext
-							.getServiceReference(IFormattedOutputFactory.class);
-					if (serviceRef != null) {
-						IFormattedOutputFactory service = bundleContext.getService(serviceRef);
-						IFormattedOutput outputter = service.getFormattedOutputImplementation(ObjectType.JAXB,
-								OutputType.PDF);
-						ByteArrayOutputStream pdf = new ByteArrayOutputStream();
-						Map<String, String> parameters = new HashMap<>();
-						parameters.put("current-date", //$NON-NLS-1$
-								LocalDate.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))); //$NON-NLS-1$
-
-						outputter.transform(letter, getClass().getResourceAsStream("/rsc/xslt/areaperiods2fo.xslt"), //$NON-NLS-1$
-								pdf, parameters);
-						bundleContext.ungetService(serviceRef);
-						// save and open the file ...
-						File file = null;
-						FileOutputStream fout = null;
-						try {
-							file = File.createTempFile(letter.getArea() + "_", ".pdf"); //$NON-NLS-1$ //$NON-NLS-2$
-							fout = new FileOutputStream(file);
-							fout.write(pdf.toByteArray());
-						} catch (IOException e) {
-							Display.getDefault().syncExec(() -> {
-								MessageDialog.openError(shell, "Fehler", "Fehler beim PDF anlegen.\n" + e.getMessage());
-							});
-							LoggerFactory.getLogger(getClass()).error("Error creating PDF", e); //$NON-NLS-1$
-						} finally {
-							if (fout != null) {
-								try {
-									fout.close();
-								} catch (IOException e) {
-									// ignore
-								}
-							}
-						}
-						if (file != null) {
-							Program.launch(file.getAbsolutePath());
-						}
-					} else {
-						LoggerFactory.getLogger(getClass()).warn("No formatted output factory available."); //$NON-NLS-1$
-					}
-				}
+            for (String area : areaPeriodMap.keySet()) {
+                AreaPeriodsLetter letter = AreaPeriodsLetter.of(area, areaPeriodMap.get(area));
+                List<Map<String, String>> appointments = new ArrayList<>();
+                Map<String, String> colors = new HashMap<>();
+                for (IPeriod period : areaPeriodMap.get(area)) {
+                    if (period instanceof IAppointment) {
+                        IAppointment appointment = (IAppointment) period;
+                        String type = appointment.getType();
+                        String typeColor = getColorForType(type);
+                        if ("gesperrt".equals(type)) {
+                            continue;
+                        }
+                        Map<String, String> appointmentData = new HashMap<>();
+                        appointmentData.put("Area", letter.getArea());
+                        appointmentData.put("ID", appointment.getId());
+                        appointmentData.put("Von", appointment.getStartTime().toLocalTime().toString());
+                        appointmentData.put("Bis", appointment.getEndTime().toLocalTime().toString());
+                        appointmentData.put("Personalien", appointment.getSubjectOrPatient());
+                        appointmentData.put("Grund", appointment.getReason());
+                        appointments.add(appointmentData);
+						colors.put(appointment.getId(), typeColor != null ? typeColor : "FFFFFFFF");
+                    }
+                }
+				System.out.println("test 99 " + appointments.size());
+                FileOutputStream fout = null;
+                File file = null;
+                try {
+					file = File.createTempFile(letter.getArea().replaceAll("\\s+", "_") + "_", ".pdf"); //$NON-NLS-1$ //$NON-NLS-2$
+                    fout = new FileOutputStream(file);
+					PdfUtils.saveFile(fout, appointments, colors);
+                } catch (IOException e) {
+                    Display.getDefault().syncExec(() -> {
+                        MessageDialog.openError(shell, "Fehler", "Fehler beim PDF anlegen.\n" + e.getMessage());
+                    });
+                    LoggerFactory.getLogger(getClass()).error("Error creating PDF", e); //$NON-NLS-1$
+                } finally {
+                    if (fout != null) {
+                        try {
+                            fout.close();
+                        } catch (IOException e) {
+                            // ignore
+                        }
+                    }
+                }
+                if (file != null) {
+                    Program.launch(file.getAbsolutePath());
+                }
 			}
-		}
+        }
 		return null;
+    }
+
+	private String getColorForType(String type) {
+		String colorDesc = ConfigServiceHolder.getUser(PreferenceConstants.AG_TYPCOLOR_PREFIX + type, "FFFFFFFF");
+		return colorDesc;
 	}
 
-	private Map<String, List<IPeriod>> getAreaPeriodMap(List<IPeriod> periods) {
+    private Map<String, List<IPeriod>> getAreaPeriodMap(List<IPeriod> periods) {
 		if (periods != null && !periods.isEmpty()) {
-			if (periods.get(0) instanceof IAppointment) {
-				Map<String, List<IPeriod>> ret = new HashMap<>();
-				for (IPeriod iPeriod : periods) {
-					String area = ((IAppointment) iPeriod).getSchedule();
-					List<IPeriod> list = ret.get(area);
-					if (list == null) {
-						list = new ArrayList<>();
-					}
-					list.add(iPeriod);
-					ret.put(area, list);
-				}
-				return ret;
-			} else {
-				throw new IllegalStateException("Can not determine area of period " + periods.get(0)); //$NON-NLS-1$
-			}
-		}
-		return Collections.emptyMap();
-	}
+            if (periods.get(0) instanceof IAppointment) {
+                Map<String, List<IPeriod>> ret = new HashMap<>();
+                for (IPeriod iPeriod : periods) {
+                    String area = ((IAppointment) iPeriod).getSchedule();
+                    List<IPeriod> list = ret.get(area);
+                    if (list == null) {
+                        list = new ArrayList<>();
+                    }
+                    list.add(iPeriod);
+                    ret.put(area, list);
+                }
+                return ret;
+            } else {
+                throw new IllegalStateException("Can not determine area of period " + periods.get(0));
+            }
+        }
+        return Collections.emptyMap();
+    }
 }
---
+++ bundles/at.medevit.elexis.agenda.ui.rcp/src/at/medevit/elexis/agenda/ui/utils/PdfUtils.java
@@ -0,0 +1,175 @@
+package at.medevit.elexis.agenda.ui.utils;
+
+import java.awt.Color;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+
+import be.quodlibet.boxable.BaseTable;
+import be.quodlibet.boxable.Cell;
+import be.quodlibet.boxable.HorizontalAlignment;
+import be.quodlibet.boxable.Row;
+import be.quodlibet.boxable.line.LineStyle;
+
+public class PdfUtils {
+
+	public static void saveFile(FileOutputStream fout, List<Map<String, String>> appointments,
+			Map<String, String> colors) throws IOException {
+		appointments.sort(Comparator.comparing(a -> a.get("Von")));
+		try (PDDocument document = new PDDocument()) {
+			PDPage page = new PDPage(new PDRectangle(PDRectangle.A4.getHeight(), PDRectangle.A4.getWidth()));
+			document.addPage(page);
+			PDPageContentStream contentStream = new PDPageContentStream(document, page);
+			float yPosDescriptionEnd = writeHeader(contentStream, page, appointments);
+			contentStream.close();
+			createDataTable(document, appointments, colors, yPosDescriptionEnd);
+			addPageNumbers(document);
+			document.save(fout);
+		}
+	}
+
+	private static void createDataTable(PDDocument doc, List<Map<String, String>> appointments,
+			Map<String, String> colors, float startY) throws IOException {
+		PDPage page = doc.getPage(0);
+		float margin = 50;
+		float tableWidth = page.getMediaBox().getWidth() - (2 * margin);
+		float yPosition = startY;
+		BaseTable table = new BaseTable(yPosition, startY, margin, tableWidth, margin, doc, page, true, true);
+		addTableHeader(table);
+		for (Map<String, String> appointment : appointments) {
+			float rowHeight = calculateRowHeight(appointment, tableWidth);
+			Row<PDPage> dataRow = table.createRow(rowHeight);
+			for (String key : new String[] { "Von", "Bis", "Personalien", "Grund" }) {
+				String text = appointment.get(key);
+				if (text != null) {
+					text = text.replaceAll("[\\r\\n]", " ");
+				}
+				Cell<PDPage> cell = dataRow.createCell(columnWidth(key), text != null ? text : "");
+				cell.setFont(PDType1Font.HELVETICA);
+				cell.setFontSize(10);
+				String id = appointment.get("ID");
+				if (id != null && colors.containsKey(id)) {
+					cell.setFillColor(getColorWithAlpha(colors.get(id)));
+				} else {
+					cell.setFillColor(Color.WHITE);
+				}
+				cell.setAlign(HorizontalAlignment.LEFT);
+				cell.setBorderStyle(new LineStyle(Color.WHITE, 0));
+				cell.setTopPadding(2f);
+				cell.setBottomPadding(2f);
+			}
+			yPosition -= rowHeight;
+		}
+		table.draw();
+		int pageCount = doc.getNumberOfPages();
+		for (int i = 0; i < pageCount; i++) {
+			PDPage tablePage = doc.getPage(i);
+			PDPageContentStream tableContentStream = new PDPageContentStream(doc, tablePage,
+					PDPageContentStream.AppendMode.APPEND, true);
+			writeHeader(tableContentStream, tablePage, appointments);
+			tableContentStream.close();
+		}
+	}
+
+	private static float calculateRowHeight(Map<String, String> appointment, float tableWidth) {
+		float defaultRowHeight = 10f;
+		float estimatedHeight = defaultRowHeight;
+		for (String key : appointment.keySet()) {
+			String text = appointment.get(key);
+			if (text != null) {
+				int lines = text.split("\n").length;
+				estimatedHeight = Math.max(estimatedHeight, defaultRowHeight * lines);
+			}
+		}
+		return estimatedHeight;
+	}
+
+	private static float columnWidth(String columnName) {
+		switch (columnName) {
+		case "Von":
+		case "Bis":
+			return 5f;
+		case "Personalien":
+			return 30f;
+		case "Grund":
+			return 60f;
+		default:
+			return 10f;
+		}
+	}
+
+	private static void addTableHeader(BaseTable table) throws IOException {
+		Row<PDPage> headerRow = table.createRow(15f);
+		for (String header : new String[] { "Von", "Bis", "Personalien", "Grund" }) {
+			Cell<PDPage> cell = headerRow.createCell(columnWidth(header), header);
+			cell.setFont(PDType1Font.HELVETICA_BOLD);
+			cell.setFontSize(10);
+			cell.setAlign(HorizontalAlignment.LEFT);
+			cell.setBorderStyle(new LineStyle(Color.WHITE, 0));
+			cell.setTopPadding(2f);
+			cell.setBottomPadding(2f);
+		}
+		table.addHeaderRow(headerRow);
+	}
+
+	private static Color getColorWithAlpha(String colorStr) {
+		int r = Integer.parseInt(colorStr.substring(0, 2), 16);
+		int g = Integer.parseInt(colorStr.substring(2, 4), 16);
+		int b = Integer.parseInt(colorStr.substring(4, 6), 16);
+		double alpha = 0.6;
+		r = (int) (r + (255 - r) * alpha);
+		g = (int) (g + (255 - g) * alpha);
+		b = (int) (b + (255 - b) * alpha);
+		return new Color(r, g, b);
+	}
+
+	private static float writeHeader(PDPageContentStream contentStream, PDPage page,
+			List<Map<String, String>> appointments) throws IOException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+		contentStream.beginText();
+		contentStream.setFont(PDType1Font.HELVETICA, 8);
+		String area = appointments.isEmpty() ? "Unbekannt" : appointments.get(0).get("Area");
+		float yPos = page.getMediaBox().getHeight() - 50;
+		contentStream.newLineAtOffset(page.getMediaBox().getWidth() - 255, yPos);
+		contentStream.showText("Agenda Bereich " + area + ", " + dateFormat.format(Calendar.getInstance().getTime())
+				+ " erstellt am " + dateFormat.format(Calendar.getInstance().getTime()));
+		contentStream.endText();
+		contentStream.setStrokingColor(Color.BLACK);
+		contentStream.setLineWidth((float) 0.5);
+		contentStream.moveTo(50, yPos - 3);
+		contentStream.lineTo(page.getMediaBox().getWidth() - 50, yPos - 3);
+		contentStream.stroke();
+		return yPos - 10;
+	}
+
+	private static void addPageNumbers(PDDocument document) throws IOException {
+		int totalPages = document.getNumberOfPages();
+		for (int i = 0; i < totalPages; i++) {
+			PDPage page = document.getPage(i);
+			PDPageContentStream footerContentStream = new PDPageContentStream(document, page,
+					PDPageContentStream.AppendMode.APPEND, true);
+			float lineYPos = 40;
+			footerContentStream.setStrokingColor(Color.BLACK);
+			footerContentStream.setLineWidth((float) 0.5);
+			footerContentStream.moveTo(50, lineYPos);
+			footerContentStream.lineTo(page.getMediaBox().getWidth() - 50, lineYPos);
+			footerContentStream.stroke();
+			footerContentStream.beginText();
+			footerContentStream.setFont(PDType1Font.HELVETICA, 8);
+			footerContentStream.newLineAtOffset(page.getMediaBox().getWidth() - 100, 30);
+			footerContentStream.showText("Seite " + (i + 1) + " von " + totalPages);
+			footerContentStream.endText();
+			footerContentStream.close();
+		}
+	}
+}

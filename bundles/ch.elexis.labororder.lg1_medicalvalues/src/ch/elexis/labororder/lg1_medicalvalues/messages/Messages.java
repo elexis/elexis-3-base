1cf82dc2fd3fd3f17233ae8ca2eeeb793b4a3f0c
pdenzler <patrick.denzlers@gmail.com>
2024-07-26 14:34:56 +0200

    [26497] add New Message for LG1 order (#357)

--- bundles/ch.elexis.labororder.lg1_medicalvalues/src/ch/elexis/labororder/lg1_medicalvalues/messages/Messages.java
+++ bundles/ch.elexis.labororder.lg1_medicalvalues/src/ch/elexis/labororder/lg1_medicalvalues/messages/Messages.java
@@ -20,7 +20,7 @@ public class Messages {
         public static String LabOrderAction_errorTitleNoPatientSelected = ch.elexis.core.l10n.Messages.Core_No_patient_selected_exlamation;
         public static String LabOrderAction_infoMessageLabOrderFinshed = ch.elexis.core.l10n.Messages.LabOrderAction_infoMessageLabOrderFinshed;
         public static String LabOrderAction_infoTitleLabOrderFinshed = ch.elexis.core.l10n.Messages.LabOrderAction_infoTitleLabOrderFinshed;
-        public static String LabOrderAction_nameAction = ch.elexis.core.l10n.Messages.LabOrderAction_nameAction;
+		public static String LaborOrderAction_LG1NameAction = ch.elexis.core.l10n.Messages.LaborOrderAction_LG1NameAction;
         public static String LabOrderAction_receivingApplication = ch.elexis.core.l10n.Messages.LabOrderAction_receivingApplication;
         public static String LabOrderAction_receivingFacility = ch.elexis.core.l10n.Messages.LabOrderAction_receivingFacility;
         public static String Lg1PreferencePage_labelUploadDir = ch.elexis.core.l10n.Messages.Core_Upload_Directory;

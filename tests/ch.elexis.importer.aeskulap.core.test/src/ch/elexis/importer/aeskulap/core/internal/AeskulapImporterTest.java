42ced179396eef7a52cca2c27f1f47874d90b1b3
thuster <huster@medevit.at>
2024-11-02 15:23:01 +0100

    [26655] add removing duplicate patients for aeskulap import incl test

--- tests/ch.elexis.importer.aeskulap.core.test/src/ch/elexis/importer/aeskulap/core/internal/AeskulapImporterTest.java
+++ tests/ch.elexis.importer.aeskulap.core.test/src/ch/elexis/importer/aeskulap/core/internal/AeskulapImporterTest.java
@@ -6,16 +6,17 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.SubMonitor;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import ch.elexis.core.data.service.ContextServiceHolder;
-import ch.elexis.core.data.service.CoreModelServiceHolder;
+import ch.elexis.core.constants.XidConstants;
 import ch.elexis.core.model.IDocument;
 import ch.elexis.core.model.ILabResult;
 import ch.elexis.core.model.IMandator;
@@ -26,13 +27,19 @@ import ch.elexis.core.model.ModelPackage;
 import ch.elexis.core.model.builder.IContactBuilder;
 import ch.elexis.core.model.builder.IUserBuilder;
 import ch.elexis.core.services.IDocumentStore;
+import ch.elexis.core.services.IModelService;
+import ch.elexis.core.services.IQuery;
 import ch.elexis.core.services.IQuery.COMPARATOR;
 import ch.elexis.core.services.IXidService;
+import ch.elexis.core.services.holder.ContextServiceHolder;
+import ch.elexis.core.services.holder.CoreModelServiceHolder;
+import ch.elexis.core.services.holder.XidServiceHolder;
 import ch.elexis.core.types.Gender;
 import ch.elexis.core.utils.OsgiServiceUtil;
 import ch.elexis.importer.aeskulap.core.IAeskulapImportFile;
 import ch.elexis.importer.aeskulap.core.IAeskulapImporter;
 import ch.elexis.importer.aeskulap.core.test.AllTests;
+import ch.elexis.omnivore.model.IDocumentHandle;
 
 public class AeskulapImporterTest {
 
@@ -79,5 +86,71 @@ public class AeskulapImporterTest {
 		List<IDocument> importedDocuments = omnivoreDocumentStore.getDocuments(importedPatient.getId(), null, null,
 				null);
 		assertFalse(importedDocuments.isEmpty());
+	}
+
+	@Test
+	public void removePatientDuplicates() {
+		// create existing patients
+		IPatient existingMatchNoAhv = new IContactBuilder.PatientBuilder(CoreModelServiceHolder.get(), "Vanessa",
+				"Test", LocalDate.of(1976, 3, 7), Gender.FEMALE).buildAndSave();
+
+		IPatient existingMatchAhv = new IContactBuilder.PatientBuilder(CoreModelServiceHolder.get(), "Martina",
+				"Test Doppelname",
+				LocalDate.of(1980, 8, 29), Gender.FEMALE).buildAndSave();
+		existingMatchAhv.addXid(XidConstants.DOMAIN_AHV, "7561234567897", true);
+
+		IPatient existingMatchWithData = new IContactBuilder.PatientBuilder(CoreModelServiceHolder.get(), "Nathalie",
+				"Test", LocalDate.of(1984, 3, 28), Gender.FEMALE).buildAndSave();
+		// run import
+		List<IAeskulapImportFile> files = importer.setImportDirectory(AllTests.getTestDirectory());
+		assertFalse(files.isEmpty());
+		SubMonitor subMonitor = SubMonitor.convert(new NullProgressMonitor(), files.size());
+		List<IAeskulapImportFile> imported = importer.importFiles(files, false, subMonitor);
+		assertTrue(imported.isEmpty());
+		assertEquals(2, queryPatients("Vanessa", "Test", null, false).size());
+		assertEquals(2, queryPatients(null, null, "7561234567897", false).size());
+		assertEquals(2, queryPatients("Nathalie", "Test", null, false).size());
+
+		IPatient importedWithData = queryPatients("Nathalie", "Test", null, false).stream()
+				.filter(p -> !getDocuments(p).isEmpty()).findFirst().get();
+		int documentsSize = getDocuments(importedWithData).size();
+		assertTrue(documentsSize > 0);
+
+		// run remove duplicates
+		importer.removePatientDuplicates(new NullProgressMonitor());
+
+		assertEquals(1, queryPatients("Vanessa", "Test", null, false).size());
+		assertEquals(1, queryPatients(null, null, "7561234567897", false).size());
+		assertEquals(1, queryPatients("Nathalie", "Test", null, false).size());
+
+		assertEquals(documentsSize, getDocuments(existingMatchWithData).size());
+	}
+
+	private IModelService omnivoreModelService;
+
+	private List<IDocumentHandle> getDocuments(IPatient patient) {
+		if (omnivoreModelService == null) {
+			omnivoreModelService = OsgiServiceUtil.getService(IModelService.class,
+					"(" + IModelService.SERVICEMODELNAME + "=ch.elexis.omnivore.data.model)").get();
+		}
+		IQuery<IDocumentHandle> documentQuery = omnivoreModelService.getQuery(IDocumentHandle.class);
+		documentQuery.and("kontakt", COMPARATOR.EQUALS, patient);
+		return documentQuery.execute();
+	}
+
+	private List<IPatient> queryPatients(String firstname, String lastname, String ahv, boolean includedeleted) {
+		if (StringUtils.isNotBlank(ahv)) {
+			List<IPatient> found = XidServiceHolder.get().findObjects(XidConstants.DOMAIN_AHV, ahv, IPatient.class);
+			if (!includedeleted) {
+				found = found.stream().filter(p -> !p.isDeleted()).toList();
+			}
+			return found;
+		} else if (StringUtils.isNotBlank(firstname) && StringUtils.isNotBlank(lastname)) {
+			IQuery<IPatient> query = CoreModelServiceHolder.get().getQuery(IPatient.class, includedeleted);
+			query.and(ModelPackage.Literals.IPERSON__FIRST_NAME, COMPARATOR.EQUALS, firstname);
+			query.and(ModelPackage.Literals.IPERSON__LAST_NAME, COMPARATOR.EQUALS, lastname);
+			return query.execute();
+		}
+		return Collections.emptyList();
 	}
 }

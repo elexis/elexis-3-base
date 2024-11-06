d38b6431dd9fe91d178d712b0d2e8270a89cca76
thuster <huster@medevit.at>
2024-11-06 14:48:47 +0100

    [26714] add select properties, delete after import ObservationMigrator

--- bundles/com.hilotec.elexis.messwerte.v2/src/com/hilotec/elexis/messwerte/v2/views/ObservationMigrator.java
+++ bundles/com.hilotec.elexis.messwerte.v2/src/com/hilotec/elexis/messwerte/v2/views/ObservationMigrator.java
@@ -1,6 +1,7 @@
 package com.hilotec.elexis.messwerte.v2.views;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -16,6 +17,7 @@ import javax.inject.Inject;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.e4.core.di.extensions.Service;
 import org.slf4j.LoggerFactory;
 
 import com.hilotec.elexis.messwerte.v2.data.Messung;
@@ -23,7 +25,6 @@ import com.hilotec.elexis.messwerte.v2.data.MessungKonfiguration;
 import com.hilotec.elexis.messwerte.v2.data.Messwert;
 
 import ch.elexis.core.exceptions.ElexisException;
-import ch.elexis.core.findings.IFindingsService;
 import ch.elexis.core.findings.IObservation;
 import ch.elexis.core.findings.IObservation.ObservationType;
 import ch.elexis.core.findings.ObservationComponent;
@@ -35,6 +36,7 @@ import ch.elexis.core.findings.templates.service.IFindingsTemplateService;
 import ch.elexis.core.findings.util.ModelUtil;
 import ch.elexis.core.findings.util.commands.UpdateFindingTextCommand;
 import ch.elexis.core.findings.util.model.TransientCoding;
+import ch.elexis.core.services.IModelService;
 import ch.elexis.core.ui.e4.util.CoreUiUtil;
 import ch.elexis.core.utils.CoreUtil;
 import ch.elexis.data.Patient;
@@ -44,7 +46,8 @@ import ch.rgw.tools.TimeTool;
 public class ObservationMigrator {
 
 	@Inject
-	private IFindingsService findingsService;
+	@Service(filterExpression = "(" + IModelService.SERVICEMODELNAME + "=ch.elexis.core.findings.model)")
+	private IModelService findingsModelService;
 
 	@Inject
 	private IFindingsTemplateService findigsTemplateService;
@@ -69,6 +72,14 @@ public class ObservationMigrator {
 		mappingProperties = new Properties();
 		try {
 			mappingProperties.load(getClass().getResourceAsStream("/rsc/migration/mapping.properties"));
+		} catch (IOException e) {
+			LoggerFactory.getLogger(getClass()).error("Could not load mapping properties.", e);
+		}
+	}
+
+	public void loadProperties(String filename) {
+		try (FileInputStream fis = new FileInputStream(filename)) {
+			mappingProperties.load(fis);
 		} catch (IOException e) {
 			LoggerFactory.getLogger(getClass()).error("Could not load mapping properties.", e);
 		}
@@ -130,12 +141,11 @@ public class ObservationMigrator {
 
 								observation.setEffectiveTime(messungDatum.toLocalDateTime());
 								observation.setOriginUri(UriType.DB.toString(messwert.storeToString()));
-
+								messwert.delete();
 								try {
 									new UpdateFindingTextCommand(observation).execute();
 								} catch (ElexisException e) {
-									LoggerFactory.getLogger(getClass())
-											.warn("Updating finding text failed"); //$NON-NLS-1$
+									LoggerFactory.getLogger(getClass()).warn("Updating finding text failed"); //$NON-NLS-1$
 								}
 							} catch (ElexisException e) {
 								LoggerFactory.getLogger(getClass()).error("Error creating finding for messwert '{}'", //$NON-NLS-1$

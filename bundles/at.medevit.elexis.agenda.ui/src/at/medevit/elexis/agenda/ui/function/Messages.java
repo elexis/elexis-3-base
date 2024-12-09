d95776e82f7556d46dfd41c8bb35cc7310a75d05
Dalibor <Dalibor@LAPTOP-QRU70GCF>
2024-12-09 16:11:08 +0100

    [26787] Screenshot logger hinzugefügt

--- bundles/at.medevit.elexis.agenda.ui/src/at/medevit/elexis/agenda/ui/function/Messages.java
+++ bundles/at.medevit.elexis.agenda.ui/src/at/medevit/elexis/agenda/ui/function/Messages.java
@@ -1,9 +1,10 @@
 package at.medevit.elexis.agenda.ui.function;
 
-import org.eclipse.osgi.util.NLS;
-
 public class Messages {
 	public static String AgendaUI_DayClick_info = ch.elexis.core.l10n.Messages.AgendaUI_DayClick_info;
 	public static String AgendaUI_DayClick_no_resource_selected = ch.elexis.core.l10n.Messages.AgendaUI_DayClick_no_resource_selected;
+	public static String ScreenshotFunction_PreviewShell_Text = ch.elexis.core.l10n.Messages.ScreenshotFunction_PreviewShell_Text;
+	public static String PrintChartAction_printTitle = ch.elexis.core.l10n.Messages.PrintChartAction_printTitle;
+	public static String ScreenshotFunction_PrintJob_Title = ch.elexis.core.l10n.Messages.ScreenshotFunction_PrintJob_Title;
 
 }
--- bundles/at.medevit.elexis.agenda.ui/src/at/medevit/elexis/agenda/ui/function/ScreenshotFunction.java
+++ bundles/at.medevit.elexis.agenda.ui/src/at/medevit/elexis/agenda/ui/function/ScreenshotFunction.java
@@ -1,10 +1,10 @@
 package at.medevit.elexis.agenda.ui.function;
 
-import javax.inject.Inject;
 import java.io.ByteArrayInputStream;
 import java.util.Base64;
 
-import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import javax.inject.Inject;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
@@ -18,13 +18,14 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.equo.chromium.swt.Browser;
 import com.equo.chromium.swt.BrowserFunction;
 
 public class ScreenshotFunction extends BrowserFunction {
-
-
+	private static final Logger logger = LoggerFactory.getLogger(ScreenshotFunction.class);
 
 	@Inject
 	public ScreenshotFunction(Browser browser, String name) {
@@ -43,7 +44,6 @@ public class ScreenshotFunction extends BrowserFunction {
 	private void showPreviewAndPrint(String base64Image) {
 		Display.getDefault().asyncExec(() -> {
 			try {
-				// Decodieren des Bildes
 				String base64Data = base64Image.split(",")[1];
 				byte[] imageBytes = Base64.getDecoder().decode(base64Data);
 
@@ -51,16 +51,13 @@ public class ScreenshotFunction extends BrowserFunction {
 				ImageData imageData = new ImageData(inputStream);
 				Image image = new Image(Display.getDefault(), imageData);
 
-				// Erstellen eines Vorschaufensters
 				Shell previewShell = new Shell(Display.getDefault());
-				previewShell.setText("Bildvorschau");
+				previewShell.setText(Messages.ScreenshotFunction_PreviewShell_Text);
 				previewShell.setLayout(new GridLayout(1, false));
 
-				// Canvas für die Bildanzeige
 				Canvas canvas = new Canvas(previewShell, SWT.BORDER);
 				canvas.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 				canvas.addPaintListener(e -> {
-					// Bild in Canvas einpassen
 					int canvasWidth = canvas.getSize().x;
 					int canvasHeight = canvas.getSize().y;
 					float aspectRatio = (float) imageData.width / imageData.height;
@@ -72,43 +69,29 @@ public class ScreenshotFunction extends BrowserFunction {
 						scaledHeight = canvasHeight;
 						scaledWidth = (int) (canvasHeight * aspectRatio);
 					}
-
 					int xOffset = (canvasWidth - scaledWidth) / 2;
 					int yOffset = (canvasHeight - scaledHeight) / 2;
-
 					e.gc.drawImage(image, 0, 0, imageData.width, imageData.height, xOffset, yOffset, scaledWidth,
 							scaledHeight);
 				});
-
-				// Drucken-Button
 				Button printButton = new Button(previewShell, SWT.PUSH);
-				printButton.setText("Drucken");
+				printButton.setText(Messages.PrintChartAction_printTitle);
 				printButton.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false));
-
-				// Event für den Druck-Button
 				printButton.addListener(SWT.Selection, event -> {
-					// Vorschaufenster schließen
 					previewShell.close();
-					// Druckdialog öffnen und Bild drucken
 					openPrintDialog(imageData, image);
 				});
-
-				// Vorschaufenster anzeigen
-				previewShell.setSize(800, 600); // Fenstergröße einstellen
+				previewShell.setSize(800, 600);
 				previewShell.open();
-
-				// Event-Loop für das Vorschaufenster
 				Display display = Display.getDefault();
 				while (!previewShell.isDisposed()) {
 					if (!display.readAndDispatch()) {
 						display.sleep();
 					}
 				}
-
-				image.dispose(); // Bildressourcen freigeben
-
+				image.dispose();
 			} catch (Exception e) {
-				e.printStackTrace();
+				logger.error("An error occurred while processing the screenshot:", e);
 			}
 		});
 	}
@@ -117,39 +100,29 @@ public class ScreenshotFunction extends BrowserFunction {
 		Shell shell = Display.getDefault().getActiveShell();
 		PrintDialog printDialog = new PrintDialog(shell, SWT.NONE);
 		PrinterData printerData = printDialog.open();
-
 		if (printerData != null) {
 			Printer printer = new Printer(printerData);
-
-			if (printer.startJob("Screenshot Print Job")) {
-				GC gc = new GC(printer); // Graphics Context für den Drucker erstellen
+			if (printer.startJob(Messages.ScreenshotFunction_PrintJob_Title)) {
+				GC gc = new GC(printer);
 				printer.startPage();
-
-				// Berechnung der Skalierung und Positionierung für den Druck
 				int pageWidth = printer.getClientArea().width;
 				int pageHeight = printer.getClientArea().height;
-
 				float aspectRatio = (float) imageData.width / imageData.height;
 				int scaledWidth = pageWidth;
 				int scaledHeight = (int) (pageWidth / aspectRatio);
-
 				if (scaledHeight > pageHeight) {
 					scaledHeight = pageHeight;
 					scaledWidth = (int) (pageHeight * aspectRatio);
 				}
-
 				int xOffset = (pageWidth - scaledWidth) / 2;
 				int yOffset = (pageHeight - scaledHeight) / 2;
-
 				gc.drawImage(image, 0, 0, imageData.width, imageData.height, xOffset, yOffset, scaledWidth,
 						scaledHeight);
-
-				gc.dispose(); // Ressourcen freigeben
+				gc.dispose();
 				printer.endPage();
 				printer.endJob();
 			}
-
-			printer.dispose(); // Druckerressourcen freigeben
+			printer.dispose();
 		}
 	}
 }

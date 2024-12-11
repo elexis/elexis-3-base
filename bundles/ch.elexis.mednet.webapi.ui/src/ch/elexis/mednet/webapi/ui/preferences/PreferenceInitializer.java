869b60069ab8f977dd8450eea95fa0eee2f6bc9e
Dalibor Aksic <93644806+Daksic28@users.noreply.github.com>
2024-12-11 13:36:54 +0100

    [26810] PreferenceInitializer angepasst, dass er den Namen nicht (#408)

--- bundles/ch.elexis.mednet.webapi.ui/src/ch/elexis/mednet/webapi/ui/preferences/PreferenceInitializer.java
+++ bundles/ch.elexis.mednet.webapi.ui/src/ch/elexis/mednet/webapi/ui/preferences/PreferenceInitializer.java
@@ -1,42 +1,45 @@
 package ch.elexis.mednet.webapi.ui.preferences;
 
+import java.util.Optional;
+
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ch.elexis.core.services.IConfigService;
+import ch.elexis.core.utils.OsgiServiceUtil;
 import ch.elexis.mednet.webapi.core.constants.PreferenceConstants;
 
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
-	private IConfigService configService;
 	private static final Logger logger = LoggerFactory.getLogger(PreferenceInitializer.class);
+	private IConfigService configService;
+
 	public PreferenceInitializer() {
-
-		BundleContext context = FrameworkUtil.getBundle(this.getClass()).getBundleContext();
-		if (context != null) {
-			ServiceReference<IConfigService> serviceReference = context.getServiceReference(IConfigService.class);
-			if (serviceReference != null) {
-				configService = context.getService(serviceReference);
-			}
-		}
-
-		if (configService != null) {
+		Optional<IConfigService> optionalService = OsgiServiceUtil.getServiceWait(IConfigService.class, 1000);
+		if (optionalService.isPresent()) {
+			this.configService = optionalService.get();
 			initializeDefaultPreferences();
 		} else {
-			logger.error("PreferenceInitializer: ConfigService could not be retrieved.");
+			logger.error("PreferenceInitializer: Could not retrieve IConfigService.");
 		}
     }
 
-    public void initializeDefaultPreferences() {
-		if (configService != null) {
-			configService.setActiveUserContact(PreferenceConstants.MEDNET_DOWNLOAD_PATH, "");
-			configService.setActiveUserContact(PreferenceConstants.MEDNET_USER_STRING, "");
-		} else {
-			logger.error("initializeDefaultPreferences: ConfigService is null.");
+	@Override
+	public void initializeDefaultPreferences() {
+		if (configService == null) {
+			logger.error("initializeDefaultPreferences: ConfigService is null, cannot set defaults.");
+			return;
+		}
+
+		setDefaultIfEmpty(PreferenceConstants.MEDNET_DOWNLOAD_PATH);
+		setDefaultIfEmpty(PreferenceConstants.MEDNET_USER_STRING);
+	}
+
+	private void setDefaultIfEmpty(String key) {
+		String currentVal = configService.getActiveUserContact(key, null);
+		if (currentVal == null || currentVal.isEmpty()) {
+			configService.setActiveUserContact(key, "");
 		}
     }
 }

5787d838ac03b6171701d55ec59a4cc7c09480e9
Dalibor Aksic <93644806+Daksic28@users.noreply.github.com>
2025-09-02 09:38:57 +0200

    [27303] onContextDateSelected angepasst, um richtigen Termin zu (#456)

--- bundles/ch.elexis.agenda/src/ch/elexis/agenda/composite/AppointmentDetailComposite.java
+++ bundles/ch.elexis.agenda/src/ch/elexis/agenda/composite/AppointmentDetailComposite.java
@@ -371,7 +371,6 @@ public class AppointmentDetailComposite extends Composite {
 	private void onContextDateSelected() {
 		txtDateFrom.setSelection(getDateFromCalendar());
 		setCompTimeToModel();
-		loadCompTimeFromModel();
 		dayBar.refresh();
 	}
 

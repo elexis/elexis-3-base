9d4aee47da3f86147fbb454dc114fbe0ea893ce0
thuster <huster@medevit.at>
2025-09-09 09:26:24 +0200

    [27332] add collision test to recurring appointment

--- bundles/ch.elexis.agenda/src/ch/elexis/agenda/series/command/AddReccuringDateHandler.java
+++ bundles/ch.elexis.agenda/src/ch/elexis/agenda/series/command/AddReccuringDateHandler.java
@@ -4,6 +4,8 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
+import ch.elexis.agenda.externalaccess.AgendaStatus;
+import ch.elexis.core.model.IAppointmentSeries;
 import ch.elexis.core.services.holder.AppointmentServiceHolder;
 import ch.elexis.dialogs.RecurringAppointmentDialog;
 
@@ -11,8 +13,10 @@ public class AddReccuringDateHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IAppointmentSeries series = AppointmentServiceHolder.get().createAppointmentSeries();
+		series.setSchedule(AgendaStatus.getSelectedBereich());
 		RecurringAppointmentDialog dlg = new RecurringAppointmentDialog(
-				AppointmentServiceHolder.get().createAppointmentSeries());
+				series);
 		dlg.open();
 		return null;
 	}

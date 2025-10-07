a23fbd616ff8ab8cac5b533f3b45f40b522ffc60
thuster <huster@medevit.at>
2025-10-07 13:00:04 +0200

    [27233] fix tardoc billing increment amount, add date validation

--- bundles/ch.elexis.base.ch.arzttarife.model/src/ch/elexis/base/ch/arzttarife/tardoc/model/TardocVerifier.java
+++ bundles/ch.elexis.base.ch.arzttarife.model/src/ch/elexis/base/ch/arzttarife/tardoc/model/TardocVerifier.java
@@ -1,21 +1,57 @@
 package ch.elexis.base.ch.arzttarife.tardoc.model;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.lang3.StringUtils;
+
+import ch.elexis.arzttarife_schweiz.Messages;
 import ch.elexis.base.ch.arzttarife.tardoc.ITardocGroup;
+import ch.elexis.base.ch.arzttarife.tarmed.model.TarmedOptifier;
 import ch.elexis.core.model.IBillable;
 import ch.elexis.core.model.IBillableVerifier;
 import ch.elexis.core.model.IBilled;
 import ch.elexis.core.model.IEncounter;
 import ch.rgw.tools.Result;
+import ch.rgw.tools.TimeTool;
 
 public class TardocVerifier implements IBillableVerifier {
 
 	@Override
 	public Result<IBillable> verifyAdd(IBillable billable, IEncounter encounter, double amount) {
-		// TODO currently no add verification, verification is performed by optifier
-		return Result.OK();
+		Result<IBillable> ret = Result.OK();
+		if (billable instanceof TardocLeistung) {
+			Result<IBilled> validDateResult = checkValidDate(encounter, (TardocLeistung) billable, null);
+			if (!validDateResult.isOK()) {
+				return new Result<IBillable>(validDateResult.getSeverity(), validDateResult.getCode(),
+						validDateResult.getMessages().get(0).getText(), billable, false);
+			}
+		}
+		return ret;
+	}
+
+	public Result<IBilled> checkValidDate(IEncounter kons, TardocLeistung tardocLeistung, IBilled newVerrechnet) {
+		TimeTool date = new TimeTool(kons.getDate());
+		LocalDate dVon = tardocLeistung.getValidFrom();
+		if (dVon != null) {
+			TimeTool tVon = new TimeTool(dVon);
+			if (date.isBefore(tVon)) {
+				return new Result<IBilled>(Result.SEVERITY.WARNING, TarmedOptifier.NOTYETVALID,
+						tardocLeistung.getCode() + StringUtils.SPACE + Messages.TarmedOptifier_NotYetValid,
+						newVerrechnet, false);
+			}
+		}
+		LocalDate dBis = tardocLeistung.getValidTo();
+		if (dBis != null) {
+			TimeTool tBis = new TimeTool(dBis);
+			if (date.isAfter(tBis)) {
+				return new Result<IBilled>(Result.SEVERITY.WARNING, TarmedOptifier.NOMOREVALID,
+						tardocLeistung.getCode() + StringUtils.SPACE + Messages.TarmedOptifier_NoMoreValid,
+						newVerrechnet, false);
+			}
+		}
+		return new Result<IBilled>(newVerrechnet);
 	}
 
 	@Override
@@ -25,6 +61,10 @@ public class TardocVerifier implements IBillableVerifier {
 			IBillable iv = vv.getBillable();
 			if (iv instanceof TardocLeistung) {
 				TardocLeistung tardocLeistung = (TardocLeistung) iv;
+				Result<IBilled> validDateResult = checkValidDate(encounter, tardocLeistung, vv);
+				if (!validDateResult.isOK()) {
+					ret.add(validDateResult);
+				}
 				Result<IBilled> limitResult = checkLimitations(encounter, tardocLeistung, vv);
 				if (!limitResult.isOK()) {
 					ret.add(limitResult);

8382c9d175747afdfb1d1b212f4e22fc5506b287
thuster <huster@medevit.at>
2026-02-02 17:22:41 +0100

    [27942] add aerztkasse base bundles, no os client id secret no feature

---
+++ bundles/at.medevit.elexis.aerztekasse.core/src/at/medevit/elexis/aerztekasse/core/IAerztekasseService.java
@@ -0,0 +1,126 @@
+package at.medevit.elexis.aerztekasse.core;
+
+import java.io.File;
+import java.util.Optional;
+
+import ch.elexis.core.model.IMandator;
+import ch.rgw.tools.Result;
+
+public interface IAerztekasseService {
+
+	public static final String AERZTEKASSE_SNAPSHOT_MODE = "aerztekasseSnapshotMode";
+
+	public static final String cfgBase = "net/medshare/connector/finance/aerztekasse"; //$NON-NLS-1$
+
+	// Globale Einstellungen
+	public static final String cfgUsername = cfgBase + "/username"; //$NON-NLS-1$
+	public static final String cfgPassword = cfgBase + "/password"; //$NON-NLS-1$
+	public static final String cfgArchiveDir = cfgBase + "/archivedir"; //$NON-NLS-1$
+	public static final String cfgErrorDir = cfgBase + "/errordir"; //$NON-NLS-1$
+	public static final String cfgAccount = cfgBase + "/account"; //$NON-NLS-1$
+	public static final String cfgDeploymentLevel = cfgBase + "/deploymentlevel"; //$NON-NLS-1$
+
+	/**
+	 * Send all invoice xml files of the provided sendDirectory to the Aerztekasse
+	 * REST service. After sending the files are moved to archive directory inside
+	 * the send directory.
+	 * 
+	 * @param invoiceFilename
+	 * @return
+	 */
+	public Result<Object> sendFiles(File sendDirectory);
+
+	/**
+	 * Send a single invoice xml file to the Aerztekasse REST service.
+	 * 
+	 * @param invoiceFile
+	 * @return
+	 */
+	public Result<Object> sendFile(File invoiceFile);
+
+	/**
+	 * Test if there is a client id available. Such an id should be provided by a
+	 * fragment.
+	 * 
+	 * @return
+	 */
+	public boolean hasClientId();
+
+	/**
+	 * Test if user credentials are available.
+	 * 
+	 * @return
+	 */
+	public boolean hasCredentials();
+
+	/**
+	 * Set the global username
+	 * 
+	 * @param string
+	 */
+	public void setUsername(String string);
+
+	/**
+	 * Get the global username
+	 * 
+	 * @return
+	 */
+	public Optional<String> getUsername();
+
+	/**
+	 * Get the global password
+	 * 
+	 * @param string
+	 */
+	public void setPassword(String string);
+
+	/**
+	 * Get the global password.
+	 * 
+	 * @return
+	 */
+	public Optional<String> getPassword();
+
+
+	/**
+	 * Ablehnung -> Rechnung fehler, und schreiben Info rein wenn möglich. bei
+	 * fehler in globaler error verzeichnis
+	 */
+
+	/**
+	 * Set the global archive directory
+	 * 
+	 * @param archiveDir
+	 */
+	public void setGlobalArchiveDir(String archiveDir);
+
+	/**
+	 * Get the global archive directory
+	 * 
+	 * @param archiveDir
+	 * @return
+	 */
+	public Optional<String> getGlobalArchiveDir();
+
+	/**
+	 * Set the global error directory
+	 * 
+	 * @param errorDir
+	 */
+	public void setGlobalErrorDir(String errorDir);
+
+	/**
+	 * Set the account string for the {@link IMandator}.
+	 * 
+	 * @param mandator
+	 * @param account
+	 */
+	public void setAccount(IMandator mandator, String account);
+
+	/**
+	 * Get the configured account String for the provided {@link IMandator}
+	 * 
+	 * @param mandator
+	 */
+	public Optional<String> getAccount(IMandator mandator);
+}
---
+++ bundles/at.medevit.elexis.aerztekasse.ui/src/at/medevit/elexis/aerztekasse/ui/preferences/AerztekassePreferences.java
@@ -0,0 +1,103 @@
+package at.medevit.elexis.aerztekasse.ui.preferences;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jface.preference.DirectoryFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+import at.medevit.elexis.aerztekasse.core.IAerztekasseService;
+import ch.elexis.core.model.IContact;
+import ch.elexis.core.model.IMandator;
+import ch.elexis.core.services.holder.CoreModelServiceHolder;
+import ch.elexis.core.ui.preferences.ConfigServicePreferenceStore;
+import ch.elexis.core.ui.preferences.ConfigServicePreferenceStore.Scope;
+import ch.elexis.core.ui.preferences.inputs.ComboFieldEditor;
+import ch.elexis.core.ui.preferences.inputs.PasswordFieldEditor;
+
+public class AerztekassePreferences extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+	public static final String CFG_OUTPUTDIR = "aerztekasse/outputdir";
+
+	public static final String ID = "at.medevit.elexis.aerztekasse.ui.preferences";
+
+	private String[] deploymentLevels;
+
+	@Override
+	public void init(IWorkbench workbench) {
+		setPreferenceStore(new ConfigServicePreferenceStore(Scope.GLOBAL));
+
+		// only provide deployment level selection if running snapshot mode
+		boolean snapshotMode = System.getProperty(IAerztekasseService.AERZTEKASSE_SNAPSHOT_MODE) != null;
+		if (snapshotMode) {
+			deploymentLevels = new String[] { "PREPROD", "PROD" };
+		}
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		// archiv directory
+		addField(new DirectoryFieldEditor(IAerztekasseService.cfgArchiveDir,
+				ch.elexis.core.l10n.Messages.Core_ArchiveDir, getFieldEditorParent()));
+
+		// receive directory
+		addField(new DirectoryFieldEditor(IAerztekasseService.cfgErrorDir,
+				ch.elexis.core.l10n.Messages.Core_Error_Directory, getFieldEditorParent()));
+
+		// username
+		addField(new StringFieldEditor(IAerztekasseService.cfgUsername, ch.elexis.core.l10n.Messages.Core_Username,
+				getFieldEditorParent()));
+		// password
+		addField(new PasswordFieldEditor(IAerztekasseService.cfgPassword,
+				ch.elexis.core.l10n.Messages.Core_Password, getFieldEditorParent()));
+
+		Set<IContact> billerSet = getBillerSet();
+		ArrayList<IContact> sortedBillerList = new ArrayList<IContact>(billerSet);
+		Collections.sort(sortedBillerList, new Comparator<IContact>() {
+			@Override
+			public int compare(IContact o1, IContact o2) {
+				IContact r1 = o1;
+				IContact r2 = o2;
+				return getLabel(r1).compareToIgnoreCase(getLabel(r2));
+			}
+
+		});
+
+		for (IContact biller : sortedBillerList) {
+			// contractor
+			addField(new StringFieldEditor(IAerztekasseService.cfgAccount + "/" + biller.getId(),
+					ch.elexis.core.l10n.Messages.Prefs_Contractor + " " + getLabel(biller), getFieldEditorParent()));
+		}
+
+		// add deployment level selection if running in snapshot mode
+		if (deploymentLevels != null) {
+			ComboFieldEditor cmbDeployLevel = new ComboFieldEditor(IAerztekasseService.cfgDeploymentLevel,
+					ch.elexis.core.l10n.Messages.Prefs_DeplymentLevel, deploymentLevels, getFieldEditorParent());
+			addField(cmbDeployLevel);
+		}
+	}
+
+	private String getLabel(IContact contact) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(contact.getDescription1()).append(StringUtils.SPACE)
+				.append(StringUtils.defaultString(contact.getDescription2()));
+		if (!StringUtils.isBlank(contact.getDescription3())) {
+			sb.append("(").append(contact.getDescription3()).append(")"); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		return sb.toString();
+	}
+
+	private Set<IContact> getBillerSet() {
+		HashSet<IContact> ret = new HashSet<>();
+		CoreModelServiceHolder.get().getQuery(IMandator.class).execute().stream().filter(m -> m.isActive())
+				.map(m -> m.getBiller()).forEach(c -> ret.add(c));
+		return ret;
+	}
+}

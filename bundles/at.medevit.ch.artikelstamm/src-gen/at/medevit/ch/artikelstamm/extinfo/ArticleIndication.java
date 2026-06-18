75a4e145ba123dade6db7d44a61813495702a352
thuster <huster@medevit.at>
2026-06-18 09:31:27 +0200

    [28132] update to artikelstamm v6 including ARTSL with indication info

---
+++ bundles/at.medevit.ch.artikelstamm/src-gen/at/medevit/ch/artikelstamm/extinfo/ArticleIndication.java
@@ -0,0 +1,60 @@
+package at.medevit.ch.artikelstamm.extinfo;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import at.medevit.ch.artikelstamm.ARTIKELSTAMM.ITEMS.ITEM.ARTSL.ARTLIMS.ARTLIM;
+import at.medevit.ch.artikelstamm.ARTIKELSTAMM.LIMITATIONS.LIMITATION;
+
+public class ArticleIndication {
+
+	private String code;
+	private String limcode;
+
+	private String validFrom;
+	private String validTo;
+
+	private String limText;
+	private String limTextF;
+
+	public static ArticleIndication of(ARTLIM al, Map<String, LIMITATION> limitations) {
+		ArticleIndication ret = new ArticleIndication();
+		ret.code = al.getINDCD();
+		ret.limcode = al.getLIMCD();
+		if (al.getVDAT() != null) {
+			ret.validFrom = getAsLocalDate(al.getVDAT()).toString();
+		}
+		if (al.getVTDAT() != null) {
+			ret.validTo = getAsLocalDate(al.getVTDAT()).toString();
+		}
+		LIMITATION limitation = limitations.get(al.getLIMCD());
+		if (limitation != null) {
+			ret.limText = limitation.getDSCR();
+			ret.limTextF = limitation.getDSCRF();
+		}
+		return ret;
+	}
+
+	private static LocalDate getAsLocalDate(XMLGregorianCalendar xmlGregorianCalendar) {
+		return LocalDate.of(xmlGregorianCalendar.getYear(), xmlGregorianCalendar.getMonth(),
+				xmlGregorianCalendar.getDay());
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getLimcode() {
+		return limcode;
+	}
+
+	public String getLimText() {
+		return limText;
+	}
+
+	public String getLimTextF() {
+		return limTextF;
+	}
+}

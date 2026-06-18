66d62e809f82f2dd493a9a5e1d2665abf21bf046
Dalibor Aksic <dalibor.aksic@medelexis.ch>
2026-06-18 11:00:12 +0200

    [28277] Mehrfachauswahl implementiert

--- bundles/at.medevit.elexis.inbox.ui/src/at/medevit/elexis/inbox/ui/command/IgnoreCommand.java
+++ bundles/at.medevit.elexis.inbox.ui/src/at/medevit/elexis/inbox/ui/command/IgnoreCommand.java
@@ -1,6 +1,8 @@
 package at.medevit.elexis.inbox.ui.command;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -11,6 +13,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchPart;
@@ -29,8 +32,16 @@ public class IgnoreCommand extends AbstractHandler implements IHandler {
 		IWorkbenchPart part = HandlerUtil.getActivePart(event);
 		if (part instanceof InboxView) {
 			InboxView view = (InboxView) part;
+
+			final List<IInboxElement> selectedElements = getSelectedInboxElements(view);
+			if (selectedElements.isEmpty()) {
+				MessageDialog.openInformation(HandlerUtil.getActiveShell(event), "Als gelesen markieren",
+						"Bitte wählen Sie zuerst die zu markierenden Inbox Einträge aus.");
+				return null;
+			}
+
 			if (MessageDialog.openQuestion(HandlerUtil.getActiveShell(event), "Als gelesen markieren",
-					"Wollen Sie alle Inbox Einträge wirklich als gelesen markieren?")) {
+					"Wollen Sie die ausgewählten Inbox Einträge wirklich als gelesen markieren?")) {
 
 				Shell activeshell = HandlerUtil.getActiveShell(event);
 				ProgressMonitorDialog progressDialog = new ProgressMonitorDialog(activeshell);
@@ -39,11 +50,10 @@ public class IgnoreCommand extends AbstractHandler implements IHandler {
 
 						@Override
 						public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-							// check all open elements
-							List<IInboxElement> openElements = view.getOpenInboxElements();
-							monitor.beginTask("Inbox Einträge als gelesen markieren", openElements.size());
+							// only mark the selected elements
+							monitor.beginTask("Inbox Einträge als gelesen markieren", selectedElements.size());
 
-							for (IInboxElement ie : openElements) {
+							for (IInboxElement ie : selectedElements) {
 								ie.setState(State.SEEN);
 								InboxModelServiceHolder.get().save(ie);
 								monitor.worked(1);
@@ -68,5 +78,28 @@ public class IgnoreCommand extends AbstractHandler implements IHandler {
 			}
 		}
 		return null;
+	}
+
+	/**
+	 * Collect the {@link IInboxElement}s from the current viewer selection.
+	 *
+	 * @param view
+	 * @return the selected inbox elements (never <code>null</code>)
+	 */
+	private List<IInboxElement> getSelectedInboxElements(InboxView view) {
+		List<IInboxElement> result = new ArrayList<>();
+		if (view.getViewer() != null && view.getViewer().getSelection() instanceof IStructuredSelection) {
+			IStructuredSelection selection = (IStructuredSelection) view.getViewer().getSelection();
+			for (Iterator<?> it = selection.iterator(); it.hasNext();) {
+				Object selected = it.next();
+				if (selected instanceof IInboxElement) {
+					IInboxElement element = (IInboxElement) selected;
+					if (!result.contains(element)) {
+						result.add(element);
+					}
+				}
+			}
+		}
+		return result;
 	}
 }

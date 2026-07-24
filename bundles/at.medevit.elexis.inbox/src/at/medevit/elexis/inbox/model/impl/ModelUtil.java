02e4389e221e130af3a6114795f8e78ff016622f
kappisj <julian.kappis@medelexis.ch>
2026-07-24 07:53:45 +0200

    Use Guava for inbox model cache

--- bundles/at.medevit.elexis.inbox/src/at/medevit/elexis/inbox/model/impl/ModelUtil.java
+++ bundles/at.medevit.elexis.inbox/src/at/medevit/elexis/inbox/model/impl/ModelUtil.java
@@ -1,12 +1,10 @@
 package at.medevit.elexis.inbox.model.impl;
 
-import java.lang.ref.WeakReference;
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 import ch.elexis.core.jpa.entities.EntityWithId;
 import ch.elexis.core.services.IModelService;
@@ -16,15 +14,8 @@ public class ModelUtil {
 
 	private static IModelService modelService;
 	private static final int MODEL_CACHE_SIZE = 2048;
-	private static final Map<String, WeakReference<Object>> modelCache = Collections
-			.synchronizedMap(new LinkedHashMap<String, WeakReference<Object>>(MODEL_CACHE_SIZE, 0.75f, true) {
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				protected boolean removeEldestEntry(Map.Entry<String, WeakReference<Object>> eldest) {
-					return size() > MODEL_CACHE_SIZE;
-				}
-			});
+	private static final Cache<String, Object> modelCache = CacheBuilder.newBuilder().maximumSize(MODEL_CACHE_SIZE)
+			.weakValues().build();
 
 	@Reference(target = "(" + IModelService.SERVICEMODELNAME + "=ch.elexis.core.model)")
 	public void setModelService(IModelService modelService) {
@@ -34,8 +25,7 @@ public class ModelUtil {
 	public static <T> T loadCoreModel(EntityWithId entity, Class<T> clazz) {
 		if (entity != null) {
 			String key = clazz.getName() + ':' + entity.getId();
-			WeakReference<Object> reference = modelCache.get(key);
-			Object cached = reference != null ? reference.get() : null;
+			Object cached = modelCache.getIfPresent(key);
 			if (cached != null) {
 				return clazz.cast(cached);
 			}

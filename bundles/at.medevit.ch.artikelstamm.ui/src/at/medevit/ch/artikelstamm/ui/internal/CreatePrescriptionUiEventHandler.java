f0d14fff861d91addc927b3b5da9b656c61680fb
thuster <huster@medevit.at>
2026-08-03 14:02:54 +0200

    [28132] add MedicationIndicationContribution to modify indication

--- bundles/at.medevit.ch.artikelstamm.ui/src/at/medevit/ch/artikelstamm/ui/internal/CreatePrescriptionUiEventHandler.java
+++ bundles/at.medevit.ch.artikelstamm.ui/src/at/medevit/ch/artikelstamm/ui/internal/CreatePrescriptionUiEventHandler.java
@@ -1,9 +1,6 @@
 package at.medevit.ch.artikelstamm.ui.internal;
 
 import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
@@ -17,13 +14,10 @@ import org.osgi.service.event.EventHandler;
 import at.medevit.ch.artikelstamm.IArtikelstammItem;
 import at.medevit.ch.artikelstamm.extinfo.ArticleIndicationInfo;
 import ch.elexis.core.common.ElexisEventTopics;
-import ch.elexis.core.model.IPatient;
 import ch.elexis.core.model.IPrescription;
 import ch.elexis.core.model.prescription.Constants;
 import ch.elexis.core.model.prescription.EntryType;
-import ch.elexis.core.services.INativeQuery;
 import ch.elexis.core.services.holder.CoreModelServiceHolder;
-import ch.elexis.core.services.holder.StoreToStringServiceHolder;
 
 @Component(property = { EventConstants.EVENT_TOPIC + "=" + ElexisEventTopics.PERSISTENCE_EVENT_COMPATIBILITY_CREATE,
 		EventConstants.EVENT_TOPIC + "=" + ElexisEventTopics.EVENT_CREATE })
@@ -46,7 +40,7 @@ public class CreatePrescriptionUiEventHandler implements EventHandler {
 				if (item.isPm()) {
 					Optional<ArticleIndicationInfo> indicationInfo = item.getIndicationInfo();
 					if (indicationInfo.isPresent() && !indicationInfo.get().getIndications().isEmpty()) {
-						Optional<String> indicationCodeHistory = getLastIndicationCode(item,
+						Optional<String> indicationCodeHistory = IndicationCodeUtil.getLastIndicationCode(item,
 									prescription.getPatient(), Collections.emptyList());
 
 						Display.getDefault().syncExec(() -> {
@@ -67,35 +61,5 @@ public class CreatePrescriptionUiEventHandler implements EventHandler {
 				}
 			}
 		}
-	}
-
-	// @formatter:off
-	private static final String PRESCRIPTION_BYPATIENT_ANDARTIKEL = "SELECT id FROM patient_artikel_joint"
-	+ " WHERE deleted = '0'"
-	+ " AND PatientID = ?1"
-	+ " AND Artikel = ?2"
-	+ " ORDER BY DateFrom DESC";
-	// @formatter:on
-
-	private Optional<String> getLastIndicationCode(IArtikelstammItem item, IPatient patient,
-			List<EntryType> filterType) {
-		INativeQuery nativeQuery = CoreModelServiceHolder.get().getNativeQuery(PRESCRIPTION_BYPATIENT_ANDARTIKEL);
-		Map<Integer, Object> parameterMap = nativeQuery.getIndexedParameterMap(Integer.valueOf(1), patient.getId(),
-				Integer.valueOf(2), StoreToStringServiceHolder.getStoreToString(item));
-		Iterator<?> result = nativeQuery.executeWithParameters(parameterMap).iterator();
-		while (result.hasNext()) {
-			String next = result.next().toString();
-			IPrescription precription = CoreModelServiceHolder.get().load(next, IPrescription.class).get();
-			if (filterType != null && !filterType.isEmpty()) {
-				if (!filterType.contains(precription.getEntryType())) {
-					continue;
-				}
-			}
-			if (precription.getExtInfo(Constants.FLD_EXT_INDICATIONCODE) instanceof String indicationCode
-					&& StringUtils.isNotBlank(indicationCode)) {
-				return Optional.of(indicationCode);
-			}
-		}
-		return Optional.empty();
 	}
 }

15808b5da781df5d2c02374c6cb9a8b095a877fb
thuster <huster@medevit.at>
2026-08-04 11:31:52 +0200

    [28132] add indicaton code selection context, use presc info for billing

--- bundles/at.medevit.ch.artikelstamm.ui/src/at/medevit/ch/artikelstamm/ui/internal/GenericTypeOriginalAdjuster.java
+++ bundles/at.medevit.ch.artikelstamm.ui/src/at/medevit/ch/artikelstamm/ui/internal/GenericTypeOriginalAdjuster.java
@@ -1,12 +1,9 @@
 package at.medevit.ch.artikelstamm.ui.internal;
 
-import java.util.Iterator;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Optional;
 
-import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
@@ -17,10 +14,9 @@ import at.medevit.ch.artikelstamm.extinfo.ArticleIndicationInfo;
 import at.medevit.ch.artikelstamm.model.common.preference.PreferenceConstants;
 import ch.elexis.core.model.IBillable;
 import ch.elexis.core.model.IBilled;
-import ch.elexis.core.model.IPatient;
+import ch.elexis.core.model.prescription.EntryType;
 import ch.elexis.core.model.verrechnet.Constants;
 import ch.elexis.core.services.IBilledAdjuster;
-import ch.elexis.core.services.INativeQuery;
 import ch.elexis.core.services.holder.BillingServiceHolder;
 import ch.elexis.core.services.holder.ConfigServiceHolder;
 import ch.elexis.core.services.holder.CoreModelServiceHolder;
@@ -58,10 +54,19 @@ public class GenericTypeOriginalAdjuster implements IBilledAdjuster {
 			if (item.isPm()) {
 				Optional<ArticleIndicationInfo> indicationInfo = item.getIndicationInfo();
 				if (indicationInfo.isPresent() && !indicationInfo.get().getIndications().isEmpty()) {
+					// lookup in prescriptions, and use that value without user interaction
+					Optional<String> indicationCodeHistory = IndicationCodeUtil.getLastIndicationCode(item,
+							billed.getEncounter().getPatient(), Arrays.asList(EntryType.FIXED_MEDICATION,
+									EntryType.RESERVE_MEDICATION, EntryType.SYMPTOMATIC_MEDICATION));
+					if (indicationCodeHistory.isPresent()) {
+						billed.setExtInfo(Constants.FLD_EXT_INDICATIONCODE, indicationCodeHistory.get());
+						CoreModelServiceHolder.get().save(billed);
+						return;
+					}
 					IndicationCodeSelectionDialog dialog = new IndicationCodeSelectionDialog(item,
 							Display.getDefault().getActiveShell());
 
-					Optional<String> indicationCodeHistory = getLastIndicationCode(item,
+					indicationCodeHistory = IndicationCodeUtil.getLastIndicationCode(item,
 							billed.getEncounter().getPatient());
 					indicationCodeHistory.ifPresent(code -> {
 						dialog.setSelectedCode(code);
@@ -70,39 +75,13 @@ public class GenericTypeOriginalAdjuster implements IBilledAdjuster {
 						if (dialog.getSelectedCode() instanceof String selectedCode) {
 							billed.setExtInfo(Constants.FLD_EXT_INDICATIONCODE, selectedCode);
 							CoreModelServiceHolder.get().save(billed);
+							// set selection for possible following prescription creation
+							IndicationCodeUtil.setIndicationCodeSelection(billed.getEncounter().getPatient(), item,
+									selectedCode);
 						}
 					}
 				}
 			}
 		}
-	}
-
-	// @formatter:off
-	private static final String VERRECHNET_BYPATIENT_ANDCODE = "SELECT leistungen.ID FROM leistungen, behandlungen, faelle"
-	+ " WHERE leistungen.deleted = '0'"
-	+ " AND leistungen.deleted = behandlungen.deleted"
-	+ " AND leistungen.BEHANDLUNG = behandlungen.ID"
-	+ " AND leistungen.KLASSE = 'ch.artikelstamm.elexis.common.ArtikelstammItem'"
-	+ " AND faelle.ID = behandlungen.fallID"
-	+ " AND faelle.PatientID = ?1"
-	+ " AND leistungen.LEISTG_CODE like ?2"
-	+ " ORDER BY behandlungen.Datum DESC";
-	// @formatter:on
-
-	private Optional<String> getLastIndicationCode(IArtikelstammItem item, IPatient patient) {
-		INativeQuery nativeQuery = CoreModelServiceHolder.get().getNativeQuery(VERRECHNET_BYPATIENT_ANDCODE);
-		String itemIdStart = item.getId().substring(0, item.getId().indexOf(item.getCode()) + item.getCode().length());
-		Map<Integer, Object> parameterMap = nativeQuery.getIndexedParameterMap(Integer.valueOf(1), patient.getId(),
-				Integer.valueOf(2), itemIdStart + "%");
-		Iterator<?> result = nativeQuery.executeWithParameters(parameterMap).iterator();
-		while (result.hasNext()) {
-			String next = result.next().toString();
-			IBilled billed = CoreModelServiceHolder.get().load(next, IBilled.class).get();
-			if (billed.getExtInfo(Constants.FLD_EXT_INDICATIONCODE) instanceof String indicationCode
-					&& StringUtils.isNotBlank(indicationCode)) {
-				return Optional.of(indicationCode);
-			}
-		}
-		return Optional.empty();
 	}
 }

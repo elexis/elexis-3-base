5809157776cdb7f8044286cc5c404e2b6b489ecd
Dalibor Aksic <dalibor.aksic@medelexis.ch>
2026-08-31 13:55:02 +0200

    [27622] Notizen: Pfad-Dialog mit OS-Auswahl

--- bundles/ch.elexis.notes/src/ch/elexis/notes/Preferences.java
+++ bundles/ch.elexis.notes/src/ch/elexis/notes/Preferences.java
@@ -11,13 +11,21 @@
  *******************************************************************************/
 package ch.elexis.notes;
 
-import org.eclipse.jface.preference.DirectoryFieldEditor;
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
-import ch.elexis.core.data.activator.CoreHub;
-import ch.elexis.core.ui.preferences.SettingsPreferenceStore;
+import ch.elexis.core.services.holder.ConfigServiceHolder;
+import ch.elexis.core.ui.e4.jface.preference.URIFieldEditorComposite;
+import ch.elexis.core.ui.preferences.ConfigServicePreferenceStore;
+import ch.elexis.core.ui.preferences.ConfigServicePreferenceStore.Scope;
 
 /**
  * Settings for the notes-Plugin
@@ -26,16 +34,62 @@ import ch.elexis.core.ui.preferences.SettingsPreferenceStore;
  *
  */
 public class Preferences extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
-	public static final String CFGTREE = "notes/basedir"; //$NON-NLS-1$
+	public static final String CFGTREE = NotesSettings.BASEDIR;
+
+	private BooleanFieldEditor bStoreGlobal;
+
+	private URIFieldEditorComposite baseDirEditor;
 
 	public Preferences() {
 		super(GRID);
-		setPreferenceStore(new SettingsPreferenceStore(CoreHub.localCfg));
+		setPreferenceStore(new ConfigServicePreferenceStore(Scope.LOCAL));
 	}
 
 	@Override
 	protected void createFieldEditors() {
-		addField(new DirectoryFieldEditor(CFGTREE, Messages.Preferences_basedir, getFieldEditorParent()));
+		bStoreGlobal = new BooleanFieldEditor(NotesSettings.CFG_PATHS_GLOBAL,
+				ch.elexis.core.l10n.Messages.PreferencesServer_storeFSGlobal, getFieldEditorParent()) {
+			@Override
+			protected void fireValueChanged(String property, Object oldValue, Object newValue) {
+				super.fireValueChanged(property, oldValue, newValue);
+				if (FieldEditor.VALUE.equals(property)) {
+					boolean global = Boolean.TRUE.equals(newValue);
+					ConfigServiceHolder.setGlobal(NotesSettings.CFG_PATHS_GLOBAL, global);
+					updatePathStore(global);
+				}
+			}
+		};
+		addField(bStoreGlobal);
+
+		baseDirEditor = new URIFieldEditorComposite(NotesSettings.BASEDIR, Messages.Preferences_basedir,
+				getFieldEditorParent(), SWT.NONE);
+		baseDirEditor.setEmptyStringAllowed(true);
+	}
+
+	@Override
+	protected Control createContents(Composite parent) {
+		Control control = super.createContents(parent);
+		bStoreGlobal.setPreferenceStore(new ConfigServicePreferenceStore(Scope.GLOBAL));
+		bStoreGlobal.load();
+		updatePathStore(NotesSettings.isStoreGlobal());
+		return control;
+	}
+
+	private void updatePathStore(boolean global) {
+		if (baseDirEditor == null || baseDirEditor.isDisposed()) {
+			return;
+		}
+		baseDirEditor.setPreferenceStore(new ConfigServicePreferenceStore(global ? Scope.GLOBAL : Scope.LOCAL));
+	}
+
+	@Override
+	protected void adjustGridLayout() {
+		super.adjustGridLayout();
+		if (getFieldEditorParent().getLayout() instanceof GridLayout
+				&& baseDirEditor.getLayoutData() instanceof GridData) {
+			((GridData) baseDirEditor.getLayoutData()).horizontalSpan = ((GridLayout) getFieldEditorParent()
+					.getLayout()).numColumns;
+		}
 	}
 
 	public void init(IWorkbench workbench) {
